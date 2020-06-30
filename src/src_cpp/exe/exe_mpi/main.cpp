d7acbba19a39ae0f2f3e9deecb6aba23187a6f4b
Aziz <aziz.jegham@eurodecision.com>
2020-06-30 16:54:50 +0200

    [cpp][json] json output for mpi not tested

--- src/src_cpp/exe/exe_mpi/main.cpp
+++ src/src_cpp/exe/exe_mpi/main.cpp
@@ -5,11 +5,14 @@
 #include "Benders.h"
 #include "BendersMPI.h"
 #include "launcher.h"
+#include "JsonWriter.h"
 
 int main(int argc, char** argv)
 {
 	mpi::environment env(argc, argv);
 	mpi::communicator world;
+
+	JsonWriter jsonWriter_l;
 
 	if (world.rank() == 0)
 		usage(argc);
@@ -20,9 +23,14 @@ int main(int argc, char** argv)
 		exit(0);
 	}
 	if (world.rank() == 0) {
+		jsonWriter_l.write(options);
 		options.print(std::cout);
+		jsonWriter_l.updateBeginTime();
 	}
+
+	world.barrier();//@FIXME here to wait for all processes to respect the beginTime ==> All Processes will start after the initialised beginTime
 	if (world.size() == 1) {
+		std::cout << "Sequential launch" << std::endl;
 		sequential_launch(options);
 	}
 	else {
@@ -34,12 +42,21 @@ int main(int argc, char** argv)
 		BendersMpi bendersMpi(env, world, options);
 		bendersMpi.load(input, env, world);
 		world.barrier();
+
 		bendersMpi.run(env, world, std::cout);
 		world.barrier();
+
+		if (world.rank() == 0) {
+			jsonWriter_l.updateEndTime();
+			jsonWriter_l.write(bendersMpi._trace, bendersMpi._data);
+			jsonWriter_l.dump("out.json");
+		}
 		bendersMpi.free(env, world);
 		world.barrier();
+
 		if (world.rank() == 0) {
 			std::cout << "Problem ran in " << timer.elapsed() << " seconds" << std::endl;
+			jsonWriter_l.updateEndTime();
 		}
 	}
 	return(0);
