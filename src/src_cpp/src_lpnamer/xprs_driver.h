7383d29dec8fc274a96aedacc1a89c73aede7fdb
Aziz <aziz.jegham@eurodecision.com>
2020-07-08 10:38:54 +0200

    [lpnamer] delete xpress files

--- src/src_cpp/src_lpnamer/xprs_driver.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <xprs.h>
-#include "common_lpnamer.h"
-
-void XPRS_CC optimizermsg(XPRSprob prob, void* data, const char *sMsg, int nLen, int nMsgLvl);
-void errormsg(const char *sSubName, int nLineNo, int nErrorCode);
