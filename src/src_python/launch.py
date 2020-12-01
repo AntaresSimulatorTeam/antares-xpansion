a124442e8852a9c0dce68ffd1c9e4609d5de26a2
jmkerloch <53606373+jmkerloch@users.noreply.github.com>
2020-12-01 18:08:08 +0100

    Add config.yaml file support for xpansion launcher configuration (#18)

--- src/src_python/launch.py
+++ src/src_python/launch.py
@@ -1,17 +1,12 @@
 """
     launches the execution of the antares xpansion c++ module
 """
-
+from pathlib import Path
 from antares_xpansion.xpansionConfig import XpansionConfig
 from antares_xpansion.driver import XpansionDriver
 
-CONFIG = XpansionConfig()
+CONFIG = XpansionConfig(Path.cwd() / "config.yaml")
 DRIVER = XpansionDriver(CONFIG)
 
 DRIVER.launch()
 
-#MY_LP_PATH = DRIVER.generate_mps_files()
-# my_lp_path = 'D:\\repo\\these-blanchot-lp-namer\\test_case\\output\\20200214-1622eco\\lp'
-# DRIVER.set_options('D:\\repo\\these-blanchot-lp-namer\\test_case\\output\\20200214-1622eco')
-# DRIVER.launch_optimization(MY_LP_PATH, CONFIG.BENDERS_SEQUENTIAL)
-# DRIVER.launch_optimization(MY_LP_PATH, CONFIG.MERGE_MPS)
