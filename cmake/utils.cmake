c22d679a9f263804cd67de2ed3bf2f134900724b
KERLOCH Jean-Marie <jmkerloch@silicom.fr>
2020-12-02 09:12:30 +0100

    update antares-deps commit for correct use copy_dependency update

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,7 +1,7 @@
 macro(copy_dependency deps target)
 
     if( MSVC )
-        if("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
+        if("${CMAKE_BUILD_TYPE}" STREQUAL "release")
 
             get_target_property( DEP_SHARED_LIB_PATH ${deps} IMPORTED_LOCATION_RELEASE )
         else()
