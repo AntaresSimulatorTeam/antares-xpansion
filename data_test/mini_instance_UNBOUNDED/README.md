af5a536b17b9e1cc16810dfba94fee2e9c8e1443
Blaxav <36446861+Blaxav@users.noreply.github.com>
2021-04-12 17:47:08 +0200

    Develop (#91)
    * add test resources
    
    * Correction bug JSON operational_cost
    
    * Adding end to end tests for benders
    
    End to end tests for executables benderssequential, bendersmpi and merge_mps. 
    Features :
     - Modifying structure of file containing subproblems weights, adding a line WEIGHT_SUM
     - Applying the options OUTPUTROOT to every output of optimization
     - Dumping an a-priori out.json file with a status ERROR before optimization begins.
    Bug Correction :
     - merge_mps : merge_mps had a bug when a file is given to set subproblems weights, bug corrected.
     - merge_mps : the operationnal cost and overall costs where not dumped in out.json in merge_mps. bug corrected.
    
    * Modification of REAMDE's format
    
    * Add options.OUTPUTROOT to outputs
    
    * Add options.OUTPUTROOT to bendersMpi Google Log
    
    * No JsonValue in IF condition
    
    * --
    
    * Correction bug JsonValue Compilation Ubuntu
    
    * Delete last call to print_solution
    
    * PR correction + write error probs in outputroot
    
    Adding headers in project
    BendersEndToEnd tests on np.arrays
    Writing error probs in outputroot directory
    
    * Deleting redundant Json solution output "optimality" 
    
    Two Json outputs in section "solution" said the same thing : boolean "optimality" and string "problem_status". We keep only the string as it can be more precise if we catch different status such as time_limit, iteration_limit, problem_unbounded, problem_infeasible ...

---
+++ data_test/mini_instance_UNBOUNDED/README.md
@@ -0,0 +1,22 @@
+# Data test mini_instance_UNBOUNDED
+
+## Brief
+
+This test case is an unbounded instance. 
+It is used to test unboundedness detection at optimization level.
+This test case is used by bendersEndToEnd test.
+
+## Problem formulation
+
+min -10 x + z1 + z2
+
+sc.
+
+    x + z1 >= 1.5
+
+    x + z2 >= 2.5
+
+    0 <= z1
+
+    0 <= z2
+    
--- src/src_cpp/src_sequential/launcher.cpp
+++ src/src_cpp/src_sequential/launcher.cpp
@@ -84,6 +84,9 @@ void sequential_launch(BendersOptions const & options) {
 	build_input(options, input);
 
 	JsonWriter jsonWriter_l;
+	jsonWriter_l.write_failure();
+	jsonWriter_l.dump(options.OUTPUTROOT + PATH_SEPARATOR + options.JSON_NAME + ".json");
+
 	jsonWriter_l.write(options);
 	jsonWriter_l.updateBeginTime();
 	LOG(INFO) << "Constructing workers..." << std::endl;
@@ -96,7 +99,7 @@ void sequential_launch(BendersOptions const & options) {
 	best_solution_log(benders._data, benders._trace, options.GAP);
 	jsonWriter_l.updateEndTime();
 	jsonWriter_l.write(input.size(), benders._trace, benders._data);
-	jsonWriter_l.dump(options.JSON_NAME+".json");
+	jsonWriter_l.dump(options.OUTPUTROOT + PATH_SEPARATOR + options.JSON_NAME + ".json");
 
 	benders.free();
 	std::stringstream str;
