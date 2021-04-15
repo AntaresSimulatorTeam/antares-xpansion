ff42ae576a7ee50722a06984ec3dd53a57c82bec
jmkerloch <53606373+jmkerloch@users.noreply.github.com>
2021-04-15 11:36:05 +0200

    add build_config.yaml.in to indicate installDir to python test programs

--- tests/examples/conftest.py
+++ tests/examples/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
+from tests import build_config_reader
+
 def pytest_addoption(parser):
-    parser.addoption("--installDir", action="store")
+    parser.addoption("--installDir", action="store", default = build_config_reader.get_install_dir())
     
 @pytest.fixture()
 def installDir(request):
