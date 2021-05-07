ad6441ca2c3f900fe2421157522f0a4a43a7d8a4
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-05-07 17:46:52 +0200

    refactor: xpansion_study_reader.py

--- src/python/antares_xpansion/xpansion_study_reader.py
+++ src/python/antares_xpansion/xpansion_study_reader.py
@@ -5,7 +5,6 @@ from typing import List
 
 
 class XpansionStudyReader:
-
     class BaseException(Exception):
         pass
 
@@ -35,28 +34,44 @@ class XpansionStudyReader:
         null_weights = True
         nb_values = 0
         with open(filename_path, 'r') as weights_file:
-            for idx, line in enumerate(weights_file):
-                if line.strip():
-                    try:
-                        nb_values += 1
-                        line_value = float(line.strip())
-                        if line_value > 0:
-                            null_weights = False
-                        elif line_value < 0:
-                            raise XpansionStudyReader.InvalidYearsWeightValue('Line %d in file %s indicates a negative value'
-                                                                              % (idx + 1, filename_path))
-                    except ValueError:
-                        raise XpansionStudyReader.InvalidYearsWeightValue('Line %d in file %s is not a single non-negative value'
-                                                                          % (idx + 1, filename_path))
+            nb_values, null_weights = XpansionStudyReader._count_values_and_check_if_all_weights_are_null(filename_path,
+                                                                                                          weights_file)
 
         if nb_values != nb_active_years:
-            raise XpansionStudyReader.InvalidYearsWeightNumber(f'file {str(filename_path)} : invalid weight number : {nb_values} values / {nb_active_years} expected')
+            raise XpansionStudyReader.InvalidYearsWeightNumber(
+                f'file {str(filename_path)} : invalid weight number : {nb_values} values / {nb_active_years} expected')
 
         if null_weights:
             raise XpansionStudyReader.OnlyNullYearsWeightValue('file %s : all values are null'
                                                                % filename_path)
 
         return True
+
+    @staticmethod
+    def _count_values_and_check_if_all_weights_are_null(filename_path, weights_file):
+        _null_weights = True
+        _nb_values = 0
+        for idx, line in enumerate(weights_file):
+            if line.strip():
+                line_value = XpansionStudyReader._get_line_value(line, idx, filename_path)
+                _nb_values += 1
+                if line_value > 0:
+                    _null_weights = False
+                if line_value < 0:
+                    raise XpansionStudyReader.InvalidYearsWeightValue(
+                        'Line %d in file %s indicates a negative value'
+                        % (idx + 1, filename_path))
+        return _nb_values, _null_weights
+
+    @staticmethod
+    def _get_line_value(line, idx, filename_path) -> float:
+        try:
+            line_value = float(line.strip())
+        except ValueError:
+            raise XpansionStudyReader.InvalidYearsWeightValue(
+                'Line %d in file %s is not a single non-negative value'
+                % (idx + 1, filename_path))
+        return line_value
 
     @staticmethod
     def get_years_weight_from_file(file_path: Path) -> List[float]:
