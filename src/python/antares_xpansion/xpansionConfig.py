5deb53953cdd27236ed8634b2b2c9608e2931cfe
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-05-19 19:28:20 +0200

    refactor: rearrange XpansionDriver to improve readability

--- src/python/antares_xpansion/xpansionConfig.py
+++ src/python/antares_xpansion/xpansionConfig.py
@@ -40,7 +40,7 @@ class XpansionConfig():
         parser = argparse.ArgumentParser()
         parser.add_argument("--step",
                             dest="step",
-                            choices=["lp", "optim", "full", "antares", "getnames", "update"],
+                            choices=["full", "antares", "getnames", "lp", "optim", "update"],
                             help='Step to execute ("lp", "optim", "full", "antares", "getnames", "update")',
                             default="full")
         parser.add_argument("--simulationName",
