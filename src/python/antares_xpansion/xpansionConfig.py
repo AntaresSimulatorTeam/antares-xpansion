3192be99688a6c6f79106a0f9735758d8ac7eea5
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-07-12 17:26:26 +0200

    Revert "Feature/define solver output log level (#174)"
    This reverts commit d48bee82fd1fd652e3494853e10ac662caabbc31.

--- src/python/antares_xpansion/xpansionConfig.py
+++ src/python/antares_xpansion/xpansionConfig.py
@@ -148,7 +148,7 @@ class XpansionConfig():
 
     def _set_default_options(self):
         self.options_default = {
-            'LOG_LEVEL': '0',
+            'LOG_LEVEL': '3',
             'MAX_ITERATIONS': '-1',
             'GAP': '1e-06',
             'AGGREGATION': '0',
