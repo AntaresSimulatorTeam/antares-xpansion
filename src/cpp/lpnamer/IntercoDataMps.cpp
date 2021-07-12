7b375b9cd4097c047bbe90efcc4f8086bb277d26
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-07-12 17:26:26 +0200

    Revert "fix: useless check causes malfunction (#150)"
    This reverts commit 217b895cb495bbc032007c894cf0bddee4512a30.

--- src/cpp/lpnamer/IntercoDataMps.cpp
+++ src/cpp/lpnamer/IntercoDataMps.cpp
@@ -295,6 +295,11 @@ void Candidates::createMpsFileAndFillCouplings(std::string const & mps_name,
 	int ncols(in_prblm.NumVariables());
 	int nrows(in_prblm.NumConstraints());
 
+	// check if number of rows in the solver matrix is equal to the number of constraints
+	if (nrows != cstr.size()) {
+		std::cout << "WRONG NUMBER OF CSTR NAMES, solver = " << nrows << ", " << cstr.size() << " given" << std::endl;
+	}
+
 	// check if number of columns in the solver matrix is equal to the number of variables
 	if (ncols != var.size()) {
 		std::cout << "WRONG NUMBER OF VAR NAMES, solver = " << ncols << ", " << var.size() << " given" << std::endl;
