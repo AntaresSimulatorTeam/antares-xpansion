00afa81a1b903db64c8b623d54566330de730e06
Sebastien Francois <sfrancois@silicom.fr>
2021-07-15 19:35:29 +0200

    First parametrization for lpnamer testing

--- tests/end_to_end/lpnamer/test_lpnamerEndToEnd.py
+++ tests/end_to_end/lpnamer/test_lpnamerEndToEnd.py
@@ -2,28 +2,38 @@ import filecmp
 import os
 import shutil
 import subprocess
+import pytest
 from pathlib import Path
 
 DATA_TEST = Path("../../../data_test/")
-TEST_LP_01 = DATA_TEST / "test_lpnamer_01" / "output" / "20210713-1635eco-new/"
+TEST_LP_01 = DATA_TEST / "tests_lpnamer" / "test_lpnamer_01" / "output" / "20210713-1635eco-new/"
 
 
-def test_lp_namer_01(installDir):
-    # given
-    reference_lp_dir = TEST_LP_01/"reference_lp"
-    lp_dir = TEST_LP_01/"lp"
+@pytest.fixture
+def setup_and_teardown_lp_directory(request):
+    testDir = request._fixture_defs['testDir'].params[0]
+    lp_dir = testDir / "lp"
     lp_dir.mkdir()
+    yield
+    shutil.rmtree(lp_dir)
+
+
+@pytest.mark.parametrize("testDir", [TEST_LP_01])
+def test_lp_namer_01(installDir, testDir, setup_and_teardown_lp_directory):
+    # given
+    old_path = os.getcwd()
+    reference_lp_dir = testDir / "reference_lp"
+    lp_dir = testDir / "lp"
+    
     lp_namer_exe = Path(installDir) / "lp_namer"
-    os.chdir(TEST_LP_01.parent)
-    launch_command = [str(lp_namer_exe), "-o", str(TEST_LP_01.name), "-e", "contraintes.txt"]
+    os.chdir(testDir.parent)
+    launch_command = [str(lp_namer_exe), "-o", str(testDir.name), "-e", "contraintes.txt"]
     # when
     returned_l = subprocess.run(launch_command, shell=False)
     # then
+    os.chdir(old_path)
     files_to_compare = os.listdir(reference_lp_dir)
     match, mismatch, errors = filecmp.cmpfiles(reference_lp_dir, lp_dir, files_to_compare)
     assert len(match) == len(files_to_compare)
     assert len(mismatch) == 0
     assert returned_l.returncode == 0
-    # teardown
-    shutil.rmtree(lp_dir)
-
