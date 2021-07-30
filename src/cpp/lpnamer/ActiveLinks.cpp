e9898fcf5d557587e6c497416742d882bb5c0b07
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-07-30 17:16:42 +0200

    add: ActiveLinks as an empty class for the moment

---
+++ src/cpp/lpnamer/ActiveLinks.cpp
@@ -0,0 +1,2 @@
+
+#include "ActiveLinks.h"
---
+++ tests/cpp/lp_namer/ActiveLinkTest.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "ActiveLinks.h"
+
+
+TEST(ActiveLinkTests, empty_test)
+{
+    auto active_links = ActiveLinks();
+    ASSERT_EQ(1, 1);
+
+}
+
