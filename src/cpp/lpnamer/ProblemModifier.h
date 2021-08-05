1efd98df6a5030829f1e916535738dfff54993a2
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-08-05 15:17:59 +0200

    add: ProblemModifier add new columns to problem

--- src/cpp/lpnamer/ProblemModifier.h
+++ src/cpp/lpnamer/ProblemModifier.h
@@ -5,13 +5,14 @@
 #include <vector>
 #include <memory>
 #include <multisolver_interface/SolverAbstract.h>
+#include <map>
 
 struct ColumnToChange{
     int id;
     int time_step;
 };
 
-using ColumnsToChange= std::vector<ColumnToChange>;
+using ColumnsToChange = std::vector<ColumnToChange>;
 
 
 struct Cand {
@@ -22,13 +23,13 @@ using Cands=std::vector<Cand>;
 struct ActiveLink{
     unsigned int id;
     std::string name;
-    Cands link_candidates;
+    Cands candidates;
     ColumnsToChange columns;
 };
 struct ActiveLinks {
     std::vector<ActiveLink> _links;
 
-    void add_link(ActiveLink link);
+    void add_link(const ActiveLink link);
 
     std::vector<ActiveLink> getItems() const;
 };
@@ -41,16 +42,26 @@ public:
 
     void setProblem(const std::shared_ptr<SolverAbstract> &mathProblem);
 
-    void changeProblem(const ColumnsToChange& columns_to_change);
+    std::shared_ptr<SolverAbstract> changeProblem(std::shared_ptr<SolverAbstract> &mathProblem, const ColumnsToChange &columns_to_change);
+
+    void remove_bounds_for(const ColumnsToChange &columns_to_change);
 
     void changeProblem(const ActiveLinks& active_links);
+    std::shared_ptr<SolverAbstract> changeProblem(std::shared_ptr<SolverAbstract> mathProblem, const ActiveLinks& active_links);
+
 
 private:
-    std::shared_ptr<SolverAbstract> math_problem;
+    std::shared_ptr<SolverAbstract> _math_problem;
+    std::map<std::string ,unsigned int> _candidate_col_id;
+    unsigned int _n_cols_at_start;
 
     void change_upper_bounds_to_pos_inf(const std::vector<int> &col_id);
 
     void change_lower_bounds_to_neg_inf(const std::vector<int> &col_id);
+
+    void add_new_columns(const Cands &candidates);
+
+    Cands collect_candidates_from_all_links(const ActiveLinks &active_links) const;
 };
 
 
