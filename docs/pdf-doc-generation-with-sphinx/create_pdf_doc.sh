6b711c298f09c07c2aa1c0d33517b7d8a04af9d9
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-10-08 13:41:54 +0200

    feature: add script to generate doc, stop using make

---
+++ docs/pdf-doc-generation-with-sphinx/create_pdf_doc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# copy reference guide md files and assets
+cp -r ../user-guide source/
+cp -r ../assets/ source/
+# change style for inline latex math \\( -> $ and \\) -> $
+find source/user-guide/ -type f -name "*.md" -exec sed -i 's=\\\\)=$=g ; s=\\\\(=$=g' {} \;
+# actually make the pdf
+sphinx-build -M latexpdf source build
+mv build/latex/antaresxpansionuserguide.pdf .
+# clean
+rm -rf source/user-guide source/assets build
