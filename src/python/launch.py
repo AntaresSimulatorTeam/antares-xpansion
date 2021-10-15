42c5fb898c8c798871a8d25e81a37afa8ea22040
Abdoulbari ZAKIR <azakir@silicom.fr>
2021-10-15 10:32:24 +0200

    update launch.py, driver.py

--- src/python/launch.py
+++ src/python/launch.py
@@ -4,14 +4,18 @@
 from pathlib import Path
 
 from antares_xpansion.input_parser import InputParser
+from antares_xpansion.config_file_parser import ConfigFileParser
 from antares_xpansion.xpansionConfig import XpansionConfig
 from antares_xpansion.driver import XpansionDriver
 import os
 
 conf_file = Path(os.path.abspath(__file__)).parent / "config.yaml"
+config_parser = ConfigFileParser(conf_file)
+configuration_data = config_parser.get_config_Parameters()
+
 parser = InputParser()
 
-CONFIG = XpansionConfig(parser.parse_args(), conf_file)
+CONFIG = XpansionConfig(parser.parse_args(), configuration_data)
 DRIVER = XpansionDriver(CONFIG)
 
 DRIVER.launch()
