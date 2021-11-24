fda0217f01dd70a73414dccfaa12911e0808b69c
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-11-24 16:33:08 +0100

    add: unit test for study update step

--- src/python/antares_xpansion/study_updater_driver.py
+++ src/python/antares_xpansion/study_updater_driver.py
@@ -15,33 +15,22 @@ from antares_xpansion.flushed_print import flushed_print
 class StudyUpdaterDriver:
     def __init__(self, study_updater_exe, json_name) -> None:
 
-        self.set_study_updater_exe(study_updater_exe)
+        self._set_study_updater_exe(study_updater_exe)
         self.JSON_NAME = json_name
 
-    def set_simulation_output_path(self, simulation_output_path: Path):
-        if simulation_output_path.is_dir():
-            self.simulation_output_path = simulation_output_path
-        else:
-            raise StudyUpdaterDriver.StudyUpdaterOutputPathError(
-                f"Study Updater Error: {simulation_output_path} not found ")
-
-    def set_study_updater_exe(self, study_updater_exe: str):
+    def _set_study_updater_exe(self, study_updater_exe: str):
         if Path(study_updater_exe).is_file():
             self.study_updater_exe = study_updater_exe
         else:
             raise StudyUpdaterDriver.StudyUpdaterOutputPathError(
                 f"Study Updater Error: {study_updater_exe} not found ")
 
-    def _clear_old_log(self):
-        if os.path.isfile(self.study_updater_exe + '.log'):
-            os.remove(self.study_updater_exe + '.log')
-
     def launch(self, simulation_output_path, keep_mps=False):
         """
             updates the antares study using the candidates file and the json solution output
 
         """
-        self.set_simulation_output_path(simulation_output_path)
+        self._set_simulation_output_path(simulation_output_path)
         flushed_print("-- Study Update")
         self._clear_old_log()
 
@@ -50,12 +39,23 @@ class StudyUpdaterDriver:
                                         stdout=output_file,
                                         stderr=output_file)
             if returned_l.returncode != 0:
-                flushed_print(
-                    "ERROR: exited study-updater with status %d" % returned_l.returncode)
-                sys.exit(1)
+                raise StudyUpdaterDriver.UpdaterExecutionError(
+                    f"ERROR: exited study-updater with status {returned_l.returncode}")
+
             elif not keep_mps:
                 StudyOutputCleaner.clean_study_update_step(
                     self.simulation_output_path)
+
+    def _set_simulation_output_path(self, simulation_output_path: Path):
+        if simulation_output_path.is_dir():
+            self.simulation_output_path = simulation_output_path
+        else:
+            raise StudyUpdaterDriver.StudyUpdaterOutputPathError(
+                f"Study Updater Error: {simulation_output_path} not found ")
+
+    def _clear_old_log(self):
+        if os.path.isfile(self.study_updater_exe + '.log'):
+            os.remove(self.study_updater_exe + '.log')
 
     def get_study_updater_log_filename(self):
         return os.path.join(self.simulation_output_path, self.study_updater_exe + '.log')
@@ -63,6 +63,9 @@ class StudyUpdaterDriver:
     def get_study_updater_command(self):
         return [self.study_updater_exe, "-o", str(self.simulation_output_path), "-s",
                 self.JSON_NAME + ".json"]
+
+    class UpdaterExecutionError(Exception):
+        pass
 
     class StudyUpdaterOutputPathError(Exception):
         pass
