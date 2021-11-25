20a2cf681f91878ec59e8aa3a57104022d4064c9
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-11-25 11:59:46 +0100

    refactor: unit test for StudyLocker

--- tests/python/test_study_locker.py
+++ tests/python/test_study_locker.py
@@ -1,80 +1,65 @@
+import os
+import shutil
+import tempfile
 from pathlib import Path
 import pytest
 
 from antares_xpansion.study_locker import StudyLocker
-class TestStudyLocker:
 
+
+class TestStudyLocker:
+    def setup_method(self):
+        self.study_dir = Path(tempfile.mkdtemp())
+        self.lock_file = self.study_dir / ".xpansion_locker"
+
+    def teardown_method(self):
+        shutil.rmtree(self.study_dir)
 
     def test_non_existing_study_dir(self, tmp_path):
-
         with pytest.raises(StudyLocker.NotAValidDirectory):
             StudyLocker(tmp_path / "ghost")
-    
 
-    def test_fail_to_unlocked_already_locked_dir(self, tmp_path):
+    def test_lock_study_create_lock_file(self):
+        TestStudyLocker.lock_study(self.study_dir)
+        assert self.lock_file.exists()
+        assert os.stat(self.lock_file).st_size > 0
 
-        study_dir = tmp_path / "secret_study"
-        _ = TestStudyLocker._get_a_locked_study(study_dir)
+    def test_fail_to_unlock_already_locked_dir(self):
+        TestStudyLocker.lock_study(self.study_dir)
 
         with pytest.raises(StudyLocker.Locked):
-            _ = TestStudyLocker._get_a_locked_study(study_dir)
-    
-    
-    def test_locker_file_contains_multiple_lines(self, tmp_path):
-        study_dir = tmp_path / "secret_study"
-        study_locker_1 = TestStudyLocker._get_a_locked_study(study_dir)
+            TestStudyLocker.lock_study(self.study_dir)
 
-        assert study_locker_1.locker_file.exists()
-        with open(study_locker_1.locker_file, "w") as file :
+    def test_locker_file_contains_multiple_lines_raises_exception(self):
+        with open(self.lock_file, "w") as file:
             lines = ["line 1 \n", "\n line 2 \n"]
             file.writelines(lines)
-        
+
         with pytest.raises(StudyLocker.CorruptedLockerFile):
-            _ = TestStudyLocker._get_a_locked_study(study_dir)
-    
+            TestStudyLocker.lock_study(self.study_dir)
 
-    def test_locker_file_corrupted(self, tmp_path):
-        study_dir = tmp_path / "secret_study"
-        study_locker_1 = TestStudyLocker._get_a_locked_study(study_dir)
-
-        assert study_locker_1.locker_file.exists()
-        with open(study_locker_1.locker_file, "w") as file :
-            line = ["something = "+ str(123)]
+    def test_locker_file_with_one_non_valid_line_raises_exception(self):
+        with open(self.lock_file, "w") as file:
+            line = ["something = " + str(123)]
             file.writelines(line)
-        
+
         with pytest.raises(StudyLocker.CorruptedLockerFile):
-            _ = TestStudyLocker._get_a_locked_study(study_dir)
-    
+            TestStudyLocker.lock_study(self.study_dir)
 
-    def test_locker_file_corrupted_pid_is_not_an_int(self, tmp_path):
-        study_dir = tmp_path / "secret_study"
-        study_locker_1 = TestStudyLocker._get_a_locked_study(study_dir)
-
-        assert study_locker_1.locker_file.exists()
-        with open(study_locker_1.locker_file, "w") as file :
+    def test_locker_file_corrupted_pid_is_not_an_int_raises_exception(self):
+        with open(self.lock_file, "w") as file:
             line = ["PID = not_an_int"]
             file.writelines(line)
-        
+
         with pytest.raises(StudyLocker.CorruptedLockerFile):
-            _ = TestStudyLocker._get_a_locked_study(study_dir)
-    
+            TestStudyLocker.lock_study(self.study_dir)
 
     def test_empty_locker_file(self, tmp_path):
-        study_dir = tmp_path / "secret_study"
-        study_dir.mkdir()
+        self.lock_file.write_text("")
 
-        locker_file = study_dir / ".xpansion_locker"
-        locker_file.write_text("")
-
-        _ = TestStudyLocker._get_a_locked_study(study_dir)
-
+        TestStudyLocker.lock_study(self.study_dir)
 
     @staticmethod
-    def _get_a_locked_study(study_dir : Path):
-
-        if not study_dir.is_dir():
-            study_dir.mkdir()
-
+    def lock_study(study_dir: Path):
         study_locker = StudyLocker(study_dir)
         study_locker.lock()
-        return study_locker
