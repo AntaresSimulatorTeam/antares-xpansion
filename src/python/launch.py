381998af0c3f9c7353a12cd15e02cc94aee1183a
azakir <32519851+a-zakir@users.noreply.github.com>
2021-11-25 19:24:13 +0100

    Feature/lock study already launched (#289)
    * add class to lock study
    
    * tests StudyLocker class
    
    * fix centos ci issue - import psutil
    
    * refactor: unit test for StudyLocker
    
    * refactor: TestStudyLocker and  StudyLocker
    
    * feature: add unlock  method
    
    * fix: centos CI
    
    * fix code smell
    
    Co-authored-by: Andrea Sgattoni <andrea.sgattoni@gmail.com>

--- src/python/launch.py
+++ src/python/launch.py
@@ -3,12 +3,14 @@
 """
 from pathlib import Path
 
+from antares_xpansion.study_locker import StudyLocker
 from antares_xpansion.input_parser import InputParser
 from antares_xpansion.config_file_parser import ConfigFileParser
 from antares_xpansion.xpansionConfig import XpansionConfig
 from antares_xpansion.driver import XpansionDriver
 from antares_xpansion.__version__ import __version__, __antares_simulator_version__
 import os
+
 
 conf_file = Path(os.path.abspath(__file__)).parent / "config.yaml"
 config_parser = ConfigFileParser(conf_file)
@@ -23,7 +25,11 @@ print("Xpansion version: %s"%__version__)
 print("antares simulator version: %s"%__antares_simulator_version__)
 print("----------------------------------------------------------------")
 
+
+locker = StudyLocker(Path(input_parameters.data_dir)) 
+locker.lock()
 CONFIG = XpansionConfig(input_parameters, configuration_data)
 DRIVER = XpansionDriver(CONFIG)
 
 DRIVER.launch()
+locker.unlock()
