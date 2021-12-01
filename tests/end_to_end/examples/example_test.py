bfb49b8f1c1bd606bd5631f24922d6aa9af7c2bb
Thomas Bittar <thomas.bittar@rte-france.com>
2021-12-01 14:13:42 +0100

    Split mpi and sequential tests for long tests, check optimality and relative gap

--- tests/end_to_end/examples/example_test.py
+++ tests/end_to_end/examples/example_test.py
@@ -59,7 +59,6 @@ def verify_solution(study_path, expected_values, expected_investment_solution):
     json_file.close()
 
     RELATIVE_TOLERANCE = 1e-4
-    ABSOLUTE_TOLERANCE = 0.1
 
     np.testing.assert_allclose(
         solution["investment_cost"], expected_values["investment_cost"], rtol=RELATIVE_TOLERANCE)
@@ -67,6 +66,10 @@ def verify_solution(study_path, expected_values, expected_investment_solution):
                                rtol=RELATIVE_TOLERANCE)
     np.testing.assert_allclose(
         solution["overall_cost"], expected_values["overall_cost"], rtol=RELATIVE_TOLERANCE)
+    np.testing.assert_allclose(
+        solution["optimality_gap"], expected_values["optimality_gap"], rtol=RELATIVE_TOLERANCE)
+    np.testing.assert_allclose(
+        solution["relative_gap"], expected_values["relative_gap"], rtol=RELATIVE_TOLERANCE)
 
     for investment in expected_investment_solution.keys():
         assert investment in investment_solution.keys(), "Investment " + investment + \
@@ -101,19 +104,12 @@ def verify_study_update(study_path, expected_investment_solution):
 
         np.testing.assert_allclose(
             link_capacity, expected_link_capacity, rtol=1e-4)
-
-
-def verify_full_study(install_dir, study_path, expected_values, expected_investment_solution, solver: str):
-    launch_xpansion(install_dir, study_path, solver)
-    verify_solution(study_path, expected_values, expected_investment_solution)
-    verify_study_update(study_path, expected_investment_solution)
-    remove_outputs(study_path)
+    
 
 
 ## TESTS ##
-@pytest.mark.parametrize(
-    "study_path, expected_values, expected_investment_solution",
-    [
+long_parameters_names = "study_path, expected_values, expected_investment_solution"
+long_parameters_values = [
         (
             ALL_STUDIES_PATH / "xpansion-test-02",
             {
@@ -165,91 +161,201 @@ def verify_full_study(install_dir, study_path, expected_values, expected_investm
                 "p2g_marg_area2": 2000.0,
             },
         ),
-    ],
-)
-@pytest.mark.long
-def test_full_study_long(install_dir, study_path, expected_values, expected_investment_solution):
-    verify_full_study(install_dir, study_path, expected_values,
-                      expected_investment_solution, "sequential")
-    verify_full_study(install_dir, study_path, expected_values,
-                      expected_investment_solution, "mpibenders")
+    ]
+
+@pytest.mark.parametrize(long_parameters_names, long_parameters_values)
+@pytest.mark.long_sequential
+def verify_full_study_long_sequential(install_dir, study_path, expected_values, expected_investment_solution):
+    launch_xpansion(install_dir, study_path, "sequential")
+    verify_solution(study_path, expected_values, expected_investment_solution)
+    verify_study_update(study_path, expected_investment_solution)
+    remove_outputs(study_path)
+
+
+@pytest.mark.parametrize(long_parameters_names, long_parameters_values)
+@pytest.mark.long_mpi
+def verify_full_study_long_mpi(install_dir, study_path, expected_values, expected_investment_solution):
+    launch_xpansion(install_dir, study_path, "mpibenders")
+    verify_solution(study_path, expected_values, expected_investment_solution)
+    verify_study_update(study_path, expected_investment_solution)
+    remove_outputs(study_path)
 
 
 medium_parameters_names = "study_path, expected_values, expected_investment_solution"
-medium_paramters_values = [
-    (ALL_STUDIES_PATH / "xpansion-test-01",
-     {"gap": -1.1444091796875e-05, "investment_cost": 224599999.99999237,
-      "operational_cost": 22513656189.121899, "overall_cost": 22738256189.121891},
-     {"battery": 1.0e+03, "peak": 1.4e+03, "pv": 1.0e+03,
-         "semibase": 2.0e+02, "transmission_line": 0.0}
-     ),
-    (ALL_STUDIES_PATH / "xpansion-test-03",
-     {"gap": -1.1920928955078125e-06, "investment_cost": 201999999.99999976, "operational_cost": 1161894495.7433052,
-      "overall_cost": 1363894495.743305},
-     {"peak": 2500.0, "transmission_line": 1600.0, "semibase": 400.0}
-     ),
-    (ALL_STUDIES_PATH / "xpansion-test-04-mps-rounding",
-     {"gap": -0.000560760498046875, "investment_cost": 115399999.99998856, "operational_cost": 21942457893.943958,
-      "overall_cost": 22057857893.943947},
-     {"battery": 1000.0000000000124, "peak": 0.0, "pv": 1000.0,
-         "semibase": 0.0, "transmission_line": 0.0}
-     ),
-    (ALL_STUDIES_PATH / "xpansion-test-01-weights",
-     {"gap": 3.814697265625e-06, "investment_cost": 230600000.00002289, "operational_cost": 24001577891.450424,
-      "overall_cost": 24232177891.450447},
-     {"battery": 1000.0, "peak": 1500.0, "pv": 1000.0,
-         "semibase": 200.0, "transmission_line": 0.0}
-     ),
-    (ALL_STUDIES_PATH / "xpansion-test-one-link-two-candidates",
-     {"gap": -1.9073486328125e-06, "investment_cost": 15999999.999994278, "operational_cost": 11800333780.786646,
-      "overall_cost": 11816333780.78664},
-     {"transmission_line": 800.0, "transmission_line_2": 800.0}
-     ),
-    (ALL_STUDIES_PATH / "xpansion-test-01-hurdles-cost",
-     {"gap": -7.62939453125e-06, "investment_cost": 224599999.99996948,
-      "operational_cost": 22516674015.060184, "overall_cost": 22741274015.060154},
-     {"battery": 1.0e+03, "peak": 1.4e+03, "pv": 1.0e+03,
-         "semibase": 2.0e+02, "transmission_line": 0.0}
-     ),
-    (ALL_STUDIES_PATH / "additionnal-constraints",
-     {"gap": 3.0517578125e-05, "investment_cost": 224600000.00003052,
-      "operational_cost": 22513655727.899261, "overall_cost": 22738255727.899292},
-     {"battery": 1.0e+03, "peak": 1.4e+03, "pv": 1.0e+03,
-         "semibase": 2.0e+02, "transmission_line": 0.0}
-     ),
-    (ALL_STUDIES_PATH / "additionnal-constraints-binary",
-     {"gap": -7.62939453125e-06, "investment_cost": 220499999.99999809,
-      "operational_cost": 13501512886.702877, "overall_cost": 13722012886.702875},
-     {"battery": 885, "peak": 1800, "pv": 1.0e+03,
-         "semibase": 0.0, "transmission_line": 400.0}
-     ),
-    (ALL_STUDIES_PATH / "hurdles-cost-profile-value-over-one",
-     {"gap": 239316.41654706001, "investment_cost": 231999999.99999976,
-      "operational_cost": 1089603112.1225781, "overall_cost": 1321603112.1225779},
-     {"peak": 1300.0, "semibase": 1600.0, "transmission_line": 1000.0}
-     ),
-    (ALL_STUDIES_PATH / "link-profile-with-empty-week",
-     {"gap": 0.00983428955078125, "investment_cost": 27585000000.000111,
-      "operational_cost": 10629896636.068903, "overall_cost": 38214896636.069016},
-     {"base": 51150.0, "pointe": 33500, "semibase_winter": 0.0}
-     ),
-    (ALL_STUDIES_PATH / "empty-link-profile",
-     {"gap": 0.0098419189453125, "investment_cost": 27585000000.000114,
-      "operational_cost": 10629896636.068903, "overall_cost": 38214896636.069016},
-     {"base": 51150.0, "pointe": 33500, "semibase_empty": 0.0}
-     )
-]
+medium_parameters_values = [
+        (
+            ALL_STUDIES_PATH / "xpansion-test-01",
+            {
+                "optimality_gap": 3.0517578125e-05,
+                "investment_cost": 224600000.00003052,
+                "operational_cost": 22513655727.899261,
+                "overall_cost": 22738255727.899292,
+                "relative_gap": 1.3421248529435642e-15,
+            },
+            {
+                "battery": 1.0e03,
+                "peak": 1.4e03,
+                "pv": 1.0e03,
+                "semibase": 2.0e02,
+                "transmission_line": 0.0,
+            },
+        ),
+        (
+            ALL_STUDIES_PATH / "xpansion-test-03",
+            {
+                "optimality_gap": -1.1920928955078125e-06,
+                "investment_cost": 201999999.99999976,
+                "operational_cost": 1161894495.7433052,
+                "overall_cost": 1363894495.743305,
+                "relative_gap": -8.7403600441846301e-16,
+            },
+            {"peak": 2500.0, "transmission_line": 1600.0, "semibase": 400.0},
+        ),
+        (
+            ALL_STUDIES_PATH / "xpansion-test-04-mps-rounding",
+            {
+                "optimality_gap": -1.1444091796875e-05,
+                "investment_cost": 115399999.99999619,
+                "operational_cost": 21942458064.791809,
+                "overall_cost": 22057858064.791805,
+                "relative_gap": -5.1882153576560404e-16,
+            },
+            {
+                "battery": 1000.0000000000124,
+                "peak": 0.0,
+                "pv": 1000.0,
+                "semibase": 0.0,
+                "transmission_line": 0.0,
+            },
+        ),
+        (
+            ALL_STUDIES_PATH / "xpansion-test-01-weights",
+            {
+                "optimality_gap": -3.814697265625e-06,
+                "investment_cost": 230600000.00001144,
+                "operational_cost": 24001577891.450439,
+                "overall_cost": 24232177891.450451,
+                "relative_gap": -1.5742279883851852e-16,
+            },
+            {
+                "battery": 1000.0,
+                "peak": 1500.0,
+                "pv": 1000.0,
+                "semibase": 200.0,
+                "transmission_line": 0.0,
+            },
+        ),
+        (
+            ALL_STUDIES_PATH / "xpansion-test-one-link-two-candidates",
+            {
+                "optimality_gap": -1.9073486328125e-06,
+                "investment_cost": 15999999.999994278,
+                "operational_cost": 11800333780.786646,
+                "overall_cost": 11816333780.78664,
+                "relative_gap": -1.6141627921122616e-16,
+            },
+            {"transmission_line": 800.0, "transmission_line_2": 800.0},
+        ),
+        (
+            ALL_STUDIES_PATH / "xpansion-test-01-hurdles-cost",
+            {
+                "optimality_gap": -7.62939453125e-06,
+                "investment_cost": 224599999.99996948,
+                "operational_cost": 22516674015.060184,
+                "overall_cost": 22741274015.060154,
+                "relative_gap": -3.3548668057020544e-16,
+            },
+            {
+                "battery": 1.0e03,
+                "peak": 1.4e03,
+                "pv": 1.0e03,
+                "semibase": 2.0e02,
+                "transmission_line": 0.0,
+            },
+        ),
+        (
+            ALL_STUDIES_PATH / "additionnal-constraints",
+            {
+                "optimality_gap": 3.0517578125e-05,
+                "investment_cost": 224600000.00003052,
+                "operational_cost": 22513655727.899261,
+                "overall_cost": 22738255727.899292,
+                "relative_gap": 1.3421248529435642e-15,
+            },
+            {
+                "battery": 1.0e03,
+                "peak": 1.4e03,
+                "pv": 1.0e03,
+                "semibase": 2.0e02,
+                "transmission_line": 0.0,
+            },
+        ),
+        (
+            ALL_STUDIES_PATH / "additionnal-constraints-binary",
+            {
+                "optimality_gap": -7.62939453125e-06,
+                "investment_cost": 220499999.99999809,
+                "operational_cost": 13501512886.702877,
+                "overall_cost": 13722012886.702875,
+                "relative_gap": -5.5599674728794044e-16,
+            },
+            {
+                "battery": 885,
+                "peak": 1800,
+                "pv": 1.0e03,
+                "semibase": 0.0,
+                "transmission_line": 400.0,
+            },
+        ),
+        (
+            ALL_STUDIES_PATH / "hurdles-cost-profile-value-over-one",
+            {
+                "optimality_gap": 239316.41654706001,
+                "investment_cost": 231999999.99999976,
+                "operational_cost": 1089603112.1225781,
+                "overall_cost": 1321603112.1225779,
+                "relative_gap": 0.00018108039724778097,
+            },
+            {"peak": 1300.0, "semibase": 1600.0, "transmission_line": 1000.0},
+        ),
+        (
+            ALL_STUDIES_PATH / "link-profile-with-empty-week",
+            {
+                "optimality_gap": -7.62939453125e-06,
+                "investment_cost": 27585000000.000004,
+                "operational_cost": 10629896636.069275,
+                "overall_cost": 38214896636.069275,
+                "relative_gap": -1.9964451569519531e-16,
+            },
+            {"base": 51150.0, "pointe": 33500, "semibase_winter": 0.0},
+        ),
+        (
+            ALL_STUDIES_PATH / "empty-link-profile",
+            {
+                "optimality_gap": -0.0001373291015625,
+                "investment_cost": 27584999999.999992,
+                "operational_cost": 10629896636.069145,
+                "overall_cost": 38214896636.069138,
+                "relative_gap": -3.5936012825135282e-15,
+            },
+            {"base": 51150.0, "pointe": 33500, "semibase_empty": 0.0},
+        ),
+    ]
 
 
-@pytest.mark.parametrize(medium_parameters_names, medium_paramters_values, )
+@pytest.mark.parametrize(medium_parameters_names, medium_parameters_values)
 @pytest.mark.medium_sequential
-def test_full_study_medium_sequential(install_dir, study_path, expected_values, expected_investment_solution):
-    verify_full_study(install_dir, study_path, expected_values,
-                      expected_investment_solution, "sequential")
+def verify_full_study_medium_sequential(install_dir, study_path, expected_values, expected_investment_solution):
+    launch_xpansion(install_dir, study_path, "sequential")
+    verify_solution(study_path, expected_values, expected_investment_solution)
+    verify_study_update(study_path, expected_investment_solution)
+    remove_outputs(study_path)
 
 
-@pytest.mark.parametrize(medium_parameters_names, medium_paramters_values, )
+@pytest.mark.parametrize(medium_parameters_names, medium_parameters_values)
 @pytest.mark.medium_mpi
-def test_full_study_medium_parallel(install_dir, study_path, expected_values, expected_investment_solution):
-    verify_full_study(install_dir, study_path, expected_values,
-                      expected_investment_solution, "mpibenders")
+def verify_full_study_medium_parallel(install_dir, study_path, expected_values, expected_investment_solution):
+    launch_xpansion(install_dir, study_path, "mpibenders")
+    verify_solution(study_path, expected_values, expected_investment_solution)
+    verify_study_update(study_path, expected_investment_solution)
+    remove_outputs(study_path)
