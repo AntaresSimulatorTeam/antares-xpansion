0b8a104b3a9a5bb7993c3d886f8d5f65f5e4269d
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-01-14 10:56:19 +0100

    delete THRESHOLD_AGGREGATION

--- src/cpp/benders/benders_core/BendersOptions.hxx
+++ src/cpp/benders/benders_core/BendersOptions.hxx
@@ -1,67 +1,64 @@
-//Determine the degree of detail of the output, from 1 to 3
+// Determine the degree of detail of the output, from 1 to 3
 BENDERS_OPTIONS_MACRO(LOG_LEVEL, int, 0)
 
-//Maximum number of iterations accepted
+// Maximum number of iterations accepted
 BENDERS_OPTIONS_MACRO(MAX_ITERATIONS, int, -1)
 
-//Absolute required level of precision
+// Absolute required level of precision
 BENDERS_OPTIONS_MACRO(ABSOLUTE_GAP, double, 1)
 
-//Absolute required level of precision
+// Absolute required level of precision
 BENDERS_OPTIONS_MACRO(RELATIVE_GAP, double, 1e-12)
 
-//True if cuts need to be aggregated, false otherwise
+// True if cuts need to be aggregated, false otherwise
 BENDERS_OPTIONS_MACRO(AGGREGATION, bool, false)
 
-//Path to the folder where output files should be printed
+// Path to the folder where output files should be printed
 BENDERS_OPTIONS_MACRO(OUTPUTROOT, std::string, ".")
 
-//True if a trace should be built, false otherwise
+// True if a trace should be built, false otherwise
 BENDERS_OPTIONS_MACRO(TRACE, bool, true)
 
-//True if similar cuts should be deleted, false otherwise
+// True if similar cuts should be deleted, false otherwise
 BENDERS_OPTIONS_MACRO(DELETE_CUT, bool, false)
 
-//UNIFORM (1/n), CONSTANT (to set in SLAVE_WEIGHT_VALUE), or a txt file linking each slave to its weight
+// UNIFORM (1/n), CONSTANT (to set in SLAVE_WEIGHT_VALUE), or a txt file linking each slave to its weight
 BENDERS_OPTIONS_MACRO(SLAVE_WEIGHT, std::string, "CONSTANT")
 
-//If SLAVE_WEIGHT is CONSTANT, set here the divisor required
+// If SLAVE_WEIGHT is CONSTANT, set here the divisor required
 BENDERS_OPTIONS_MACRO(SLAVE_WEIGHT_VALUE, double, 1)
 
-//Name of the master problem file, if different from 'master'
+// Name of the master problem file, if different from 'master'
 BENDERS_OPTIONS_MACRO(MASTER_NAME, std::string, "master")
 
-//Number of slaves to use to solve the problem
+// Number of slaves to use to solve the problem
 BENDERS_OPTIONS_MACRO(SLAVE_NUMBER, int, -1)
 
-//Number of slaves to use to solve the problem
+// Number of slaves to use to solve the problem
 BENDERS_OPTIONS_MACRO(STRUCTURE_FILE, std::string, "structure.txt")
 
-//Path to the folder where input files are stored
+// Path to the folder where input files are stored
 BENDERS_OPTIONS_MACRO(INPUTROOT, std::string, ".")
 
-//True if simplex bases need to be stored, false otherwise
+// True if simplex bases need to be stored, false otherwise
 BENDERS_OPTIONS_MACRO(BASIS, bool, true)
 
-//True if a statement of active cuts need to be done, false otherwise
+// True if a statement of active cuts need to be done, false otherwise
 BENDERS_OPTIONS_MACRO(ACTIVECUTS, bool, false)
 
-//Number of iterations before aggregation of all previous cuts, set to 0 if no aggregation needed
-BENDERS_OPTIONS_MACRO(THRESHOLD_AGGREGATION, int, 0)
-
-//Number of iterations before aggregation of all previous cuts by iteration, set to 0 if no aggregation needed
+// Number of iterations before aggregation of all previous cuts by iteration, set to 0 if no aggregation needed
 BENDERS_OPTIONS_MACRO(THRESHOLD_ITERATION, int, 0)
 
-//Name of the csv output file
+// Name of the csv output file
 BENDERS_OPTIONS_MACRO(CSV_NAME, std::string, "benders_output_trace")
 
-//True if alpha needs to be bounded by best upper bound, false otherwise
+// True if alpha needs to be bounded by best upper bound, false otherwise
 BENDERS_OPTIONS_MACRO(BOUND_ALPHA, bool, true)
 
-//Name of solver to use
+// Name of solver to use
 BENDERS_OPTIONS_MACRO(SOLVER_NAME, std::string, "COIN")
 
-// json file in output/expansion/ 
+// json file in output/expansion/
 BENDERS_OPTIONS_MACRO(JSON_FILE, std::string, ".")
-//TIME_LIMIT
+// TIME_LIMIT
 BENDERS_OPTIONS_MACRO(TIME_LIMIT, double, 1e12)
