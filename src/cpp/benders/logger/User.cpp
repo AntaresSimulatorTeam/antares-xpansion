46534489705b42265135c5357fc2eeba840efa2f
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-01-18 12:22:14 +0100

    specify reached stop criterion Closes #347

--- src/cpp/benders/logger/User.cpp
+++ src/cpp/benders/logger/User.cpp
@@ -9,80 +9,69 @@
 #include "IterationResultLog.h"
 #include "Commons.h"
 
-namespace xpansion{
-namespace logger {
-
-    const std::string indent_0 = "\t\t";
-    const std::string indent_1 = "\t";
-
-    User::User(std::ostream& stream)
-    :_stream(stream)
+namespace xpansion
+{
+    namespace logger
     {
-        if (_stream.fail())
+
+        const std::string indent_0 = "\t\t";
+        const std::string indent_1 = "\t";
+
+        User::User(std::ostream &stream)
+            : _stream(stream)
         {
-            std::cerr << "Invalid stream passed as parameter" << std::endl;
+            if (_stream.fail())
+            {
+                std::cerr << "Invalid stream passed as parameter" << std::endl;
+            }
         }
-    }
 
-    void User::display_message(const std::string& str) {
-        _stream << str << std::endl;
-    }
-
-    void User::log_at_initialization(const LogData &d) {
-        _stream << "ITERATION " << d.it << ":" << std::endl;
-    }
-
-    void User::log_iteration_candidates(const LogData &d) {
-        _stream << CandidateLog().log_iteration_candidates(d);
-    }
-
-    void User::log_master_solving_duration(double durationInSeconds) {
-        _stream << indent_1 << "Master solved in " << durationInSeconds << " s"  << std::endl;
-    }
-
-    void User::log_subproblems_solving_duration(double durationInSeconds) {
-        _stream << indent_1 << "Subproblems solved in " << durationInSeconds << " s"  << std::endl;
-    }
-
-    void User::log_at_iteration_end(const LogData &d) {
-        _stream << IterationResultLog().Log_at_iteration_end(d);
-    }
-
-    void User::log_at_ending(const LogData &d) {
-
-        const double overall_cost = d.slave_cost + d.invest_cost;
-        _stream << indent_1 << "Best solution = it " << d.best_it << std::endl;
-        _stream << indent_1 << " Overall cost = " << commons::create_str_million_euros(overall_cost) << " Me" << std::endl;
-    }
-    void User::log_total_duration(double durationInSeconds) {
-        _stream << "Problem ran in " << durationInSeconds << " s"  << std::endl;
-    }
-
-    void User::log_stop_criterion_reached(const StoppingCriterion stopping_criterion) {
-        std::string stop_crit;
-        switch (stopping_criterion)
+        void User::display_message(const std::string &str)
         {
-            case StoppingCriterion::absolute_gap:
-                stop_crit = "absolute gap";
-                break;
-
-            case StoppingCriterion::relative_gap:
-                stop_crit = "relative gap";
-                break;
-
-            case StoppingCriterion::max_iteration:
-                stop_crit = "maximum iterations";
-                break;
-
-            case StoppingCriterion::timelimit:
-                stop_crit = "timelimit";
-                break;
-
-            default:
-                break;
+            _stream << str << std::endl;
         }
-        _stream << "--- Run completed: " << stop_crit << " reached"<< std::endl;
-    }
 
-} // namespace logger
+        void User::log_at_initialization(const LogData &d)
+        {
+            _stream << "ITERATION " << d.it << ":" << std::endl;
+        }
+
+        void User::log_iteration_candidates(const LogData &d)
+        {
+            _stream << CandidateLog().log_iteration_candidates(d);
+        }
+
+        void User::log_master_solving_duration(double durationInSeconds)
+        {
+            _stream << indent_1 << "Master solved in " << durationInSeconds << " s" << std::endl;
+        }
+
+        void User::log_subproblems_solving_duration(double durationInSeconds)
+        {
+            _stream << indent_1 << "Subproblems solved in " << durationInSeconds << " s" << std::endl;
+        }
+
+        void User::log_at_iteration_end(const LogData &d)
+        {
+            _stream << IterationResultLog().Log_at_iteration_end(d);
+        }
+
+        void User::log_at_ending(const LogData &d)
+        {
+
+            const double overall_cost = d.slave_cost + d.invest_cost;
+            _stream << indent_1 << "Best solution = it " << d.best_it << std::endl;
+            _stream << indent_1 << " Overall cost = " << commons::create_str_million_euros(overall_cost) << " Me" << std::endl;
+        }
+        void User::log_total_duration(double durationInSeconds)
+        {
+            _stream << "Problem ran in " << durationInSeconds << " s" << std::endl;
+        }
+
+        void User::log_stop_criterion_reached(const StoppingCriterion stopping_criterion)
+        {
+            _stream << "--- Run completed: " << stopping_criterion_to_str(stopping_criterion) << " reached" << std::endl;
+        }
+
+    } // namespace logger
 } // namespace xpansion
