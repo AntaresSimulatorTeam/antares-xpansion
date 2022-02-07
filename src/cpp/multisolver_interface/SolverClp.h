be62ea77e4deaa8d22872185d28743cd6507e0f1
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-02-07 10:18:49 +0100

    add override specifer in virtual methods declarations

--- src/cpp/multisolver_interface/SolverClp.h
+++ src/cpp/multisolver_interface/SolverClp.h
@@ -48,9 +48,9 @@ public:
 	SolverClp(const SolverAbstract::Ptr toCopy);
 
 	virtual ~SolverClp();
-	virtual int get_number_of_instances();
+	virtual int get_number_of_instances() override;
 
-	virtual std::string get_solver_name() const { return "CLP"; }
+	virtual std::string get_solver_name() const override { return "CLP"; }
 
 	/*************************************************************************************************
 	---------------------------------    Output and stream management    -----------------------------
@@ -60,70 +60,70 @@ public:
 	------    Destruction or creation of inner strctures and datas, closing environments    ----------
 	*************************************************************************************************/
 public:
-	virtual void init();
-	virtual void free();
+	virtual void init() override;
+	virtual void free() override;
 
 	/*************************************************************************************************
 	-------------------------------    Reading & Writing problems    -------------------------------
 	*************************************************************************************************/
 public:
-	virtual void write_prob_mps(const std::string &filename);
-	virtual void write_prob_lp(const std::string &filename);
+	virtual void write_prob_mps(const std::string &filename) override;
+	virtual void write_prob_lp(const std::string &filename) override;
 
-	virtual void read_prob_mps(const std::string &filename);
-	virtual void read_prob_lp(const std::string &filename);
+	virtual void read_prob_mps(const std::string &filename) override;
+	virtual void read_prob_lp(const std::string &filename) override;
 
-	virtual void copy_prob(const SolverAbstract::Ptr fictif_solv);
+	virtual void copy_prob(const SolverAbstract::Ptr fictif_solv) override;
 
 	/*************************************************************************************************
 	-----------------------    Get general informations about problem    ----------------------------
 	*************************************************************************************************/
 public:
-	virtual int get_ncols() const;
-	virtual int get_nrows() const;
-	virtual int get_nelems() const;
-	virtual int get_n_integer_vars() const;
-	virtual void get_obj(double *obj, int first, int last) const;
+	virtual int get_ncols() const override;
+	virtual int get_nrows() const override;
+	virtual int get_nelems() const override;
+	virtual int get_n_integer_vars() const override;
+	virtual void get_obj(double *obj, int first, int last) const override;
 	virtual void get_rows(int *mstart, int *mclind, double *dmatval, int size, int *nels,
-						  int first, int last) const;
-	virtual void get_row_type(char *qrtype, int first, int last) const;
-	virtual void get_rhs(double *rhs, int first, int last) const;
-	virtual void get_rhs_range(double *range, int first, int last) const;
-	virtual void get_col_type(char *coltype, int first, int last) const;
-	virtual void get_lb(double *lb, int fisrt, int last) const;
-	virtual void get_ub(double *ub, int fisrt, int last) const;
+						  int first, int last) const override;
+	virtual void get_row_type(char *qrtype, int first, int last) const override;
+	virtual void get_rhs(double *rhs, int first, int last) const override;
+	virtual void get_rhs_range(double *range, int first, int last) const override;
+	virtual void get_col_type(char *coltype, int first, int last) const override;
+	virtual void get_lb(double *lb, int fisrt, int last) const override;
+	virtual void get_ub(double *ub, int fisrt, int last) const override;
 
-	virtual int get_row_index(std::string const &name) const;
-	virtual int get_col_index(std::string const &name) const;
-	virtual std::vector<std::string> get_row_names(int first, int last);
-	virtual std::vector<std::string> get_col_names(int first, int last);
+	virtual int get_row_index(std::string const &name) const override;
+	virtual int get_col_index(std::string const &name) const override;
+	virtual std::vector<std::string> get_row_names(int first, int last) override;
+	virtual std::vector<std::string> get_col_names(int first, int last) override;
 
 	/*************************************************************************************************
 	------------------------------    Methods to modify problem    ----------------------------------
 	*************************************************************************************************/
 public:
-	virtual void del_rows(int first, int last);
+	virtual void del_rows(int first, int last) override;
 	virtual void add_rows(int newrows, int newnz, const char *qrtype, const double *rhs,
 						  const double *range, const int *mstart, const int *mclind,
-						  const double *dmatval);
+						  const double *dmatval) override;
 	virtual void add_cols(int newcol, int newnz, const double *objx, const int *mstart,
 						  const int *mrwind, const double *dmatval, const double *bdl,
-						  const double *bdu);
-	virtual void add_name(int type, const char *cnames, int indice);
-	virtual void chg_obj(const std::vector<int> &mindex, const std::vector<double> &obj);
+						  const double *bdu) override;
+	virtual void add_name(int type, const char *cnames, int indice) override;
+	virtual void chg_obj(const std::vector<int> &mindex, const std::vector<double> &obj) override;
 	virtual void chg_bounds(const std::vector<int> &mindex, const std::vector<char> &qbtype, const std::vector<double> &bnd);
-	virtual void chg_col_type(const std::vector<int> &mindex, const std::vector<char> &qctype);
-	virtual void chg_rhs(int id_row, double val);
-	virtual void chg_coef(int id_row, int id_col, double val);
-	virtual void chg_row_name(int id_row, std::string const &name);
-	virtual void chg_col_name(int id_col, std::string const &name);
+	virtual void chg_col_type(const std::vector<int> &mindex, const std::vector<char> &qctype) override;
+	virtual void chg_rhs(int id_row, double val) override;
+	virtual void chg_coef(int id_row, int id_col, double val) override;
+	virtual void chg_row_name(int id_row, std::string const &name) override;
+	virtual void chg_col_name(int id_col, std::string const &name) override;
 
 	/*************************************************************************************************
 	-----------------------------    Methods to solve the problem    ---------------------------------
 	*************************************************************************************************/
 public:
-	virtual int solve_lp();
-	virtual int solve_mip();
+	virtual int solve_lp() override;
+	virtual int solve_mip() override;
 
 	/*************************************************************************************************
 	-------------------------    Methods to get solutions information    -----------------------------
@@ -149,21 +149,21 @@ public:
 						4 variable is super basic
 						May be NULL if not required.
 	*/
-	virtual void get_basis(int *rstatus, int *cstatus) const;
-	virtual double get_mip_value() const;
-	virtual double get_lp_value() const;
-	virtual int get_splex_num_of_ite_last() const;
+	virtual void get_basis(int *rstatus, int *cstatus) const override;
+	virtual double get_mip_value() const override;
+	virtual double get_lp_value() const override;
+	virtual int get_splex_num_of_ite_last() const override;
 	virtual void get_lp_sol(double *primals, double *duals,
-							double *reduced_costs);
-	virtual void get_mip_sol(double *primals);
+							double *reduced_costs) override;
+	virtual void get_mip_sol(double *primals) override;
 
 	/*************************************************************************************************
 	------------------------    Methods to set algorithm or logs levels    ---------------------------
 	*************************************************************************************************/
 public:
-	virtual void set_output_log_level(int loglevel);
-	virtual void set_algorithm(std::string const &algo);
-	virtual void set_threads(int n_threads);
-	virtual void set_optimality_gap(double gap);
-	virtual void set_simplex_iter(int iter);
+	virtual void set_output_log_level(int loglevel) override;
+	virtual void set_algorithm(std::string const &algo) override;
+	virtual void set_threads(int n_threads) override;
+	virtual void set_optimality_gap(double gap) override;
+	virtual void set_simplex_iter(int iter) override;
 };
