bab7a25ec1ccad3954b0626aded30e131d64f240
Thomas Bittar <thomas.bittar@rte-france.com>
2022-02-22 10:33:40 +0100

    Do not clean last master mps

--- src/python/antares_xpansion/study_output_cleaner.py
+++ src/python/antares_xpansion/study_output_cleaner.py
@@ -7,6 +7,19 @@ def remove_files_containing_str_from_dir(contains_str: str, dirpath: Path):
         if contains_str in instance:
             os.remove(dirpath / instance)
 
+def rename_master(old_master_path: Path, new_master_path: Path):
+    if old_master_path.is_file():
+        old_master_path.rename(new_master_path)
+    else:
+        # Just throw a warning instead of exception ?
+        pass
+
+def get_last_master_path(dirpath: Path) -> Path:
+    return Path(dirpath / 'lp/master_last_iteration.mps')
+
+def get_tmp_master_path(master_path: Path) -> Path:
+    return Path(master_path.parent, master_path.stem + '.tmp')
+
 
 class StudyOutputCleaner:
 
@@ -23,7 +36,14 @@ class StudyOutputCleaner:
         remove_files_containing_str_from_dir('variables', study_output)
     @staticmethod
     def clean_benders_step(study_output: Path):
+        
+        master_path = get_last_master_path(study_output)
+        tmp_master_path = get_tmp_master_path(master_path)
+        
+        rename_master(master_path, tmp_master_path)
         remove_files_containing_str_from_dir('.mps', study_output / 'lp')
+        rename_master(tmp_master_path, master_path)
+
         remove_files_containing_str_from_dir('.lp', study_output / 'lp')
 
     @staticmethod
