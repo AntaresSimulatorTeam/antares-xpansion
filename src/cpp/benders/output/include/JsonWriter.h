210b82166d13ad0ccef808100682187593e4b41d
azakir <32519851+a-zakir@users.noreply.github.com>
2022-02-25 10:20:17 +0100

    print some options in out.json (#380)

--- src/cpp/benders/output/include/JsonWriter.h
+++ src/cpp/benders/output/include/JsonWriter.h
@@ -69,5 +69,9 @@ class JsonWriter : public OutputWriter {
   void initialize();
 
   void end_writing(const IterationsData &iterations_data);
+
+  void write_solver_name(const std::string &solver_name) override;
+  void write_master_name(const std::string &master_name) override;
+  void write_log_level(const int log_level) override;
 };
 }  // namespace Output
