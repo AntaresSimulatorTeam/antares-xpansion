ce4aca8365b3b3cf1f34f20fc66f7171a75dd4ca
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2022-03-01 15:22:01 +0100

    should fix problem (#387)

--- tests/python/test_yearly_weigth_writer.py
+++ tests/python/test_yearly_weigth_writer.py
@@ -7,27 +7,25 @@ from file_creation import _create_weight_file, _create_empty_file_from_list
 def test_weight_file_write(tmp_path):
     input_weight_file_name = "weight_file"
     file_path: Path = tmp_path / input_weight_file_name
-    weight_list = [1, 2, 3]
+    weight_list = [1.1, 2.1, 3.1]
+    active_years = [1, 2, 4]
     _create_weight_file(file_path, weight_list)
 
     mps_files = (
-    "problem-1-1-AAAAMMDD-hhmmss.mps", "problem-1-2-AAAAMMDD-hhmmss.mps", "problem-1-24-AAAAMMDD-hhmmss.mps",
-    "problem-2-1-AAAAMMDD-hhmmss.mps", "problem-2-2-AAAAMMDD-hhmmss.mps", "problem-2-24-AAAAMMDD-hhmmss.mps",
-    "problem-3-1-AAAAMMDD-hhmmss.mps", "problem-3-2-AAAAMMDD-hhmmss.mps", "problem-3-24-AAAAMMDD-hhmmss.mps")
+        f"problem-{active_years[0]}-1-AAAAMMDD-hhmmss.mps", f"problem-{active_years[0]}-24-AAAAMMDD-hhmmss.mps",
+        f"problem-{active_years[1]}-1-AAAAMMDD-hhmmss.mps", f"problem-{active_years[1]}-24-AAAAMMDD-hhmmss.mps",
+        f"problem-{active_years[2]}-1-AAAAMMDD-hhmmss.mps", f"problem-{active_years[2]}-24-AAAAMMDD-hhmmss.mps")
 
     _create_empty_file_from_list(tmp_path, mps_files)
-    YearlyWeightWriter(tmp_path).create_weight_file(weight_list, input_weight_file_name)
+    YearlyWeightWriter(tmp_path).create_weight_file(weight_list, input_weight_file_name, active_years)
 
-    expected_content = "problem-1-1-AAAAMMDD-hhmmss 1\n" \
-                       "problem-1-2-AAAAMMDD-hhmmss 1\n" \
-                       "problem-1-24-AAAAMMDD-hhmmss 1\n" \
-                       "problem-2-1-AAAAMMDD-hhmmss 2\n" \
-                       "problem-2-2-AAAAMMDD-hhmmss 2\n" \
-                       "problem-2-24-AAAAMMDD-hhmmss 2\n" \
-                       "problem-3-1-AAAAMMDD-hhmmss 3\n" \
-                       "problem-3-2-AAAAMMDD-hhmmss 3\n" \
-                       "problem-3-24-AAAAMMDD-hhmmss 3\n" \
-                       "WEIGHT_SUM 6"
+    expected_content = f"problem-{active_years[0]}-1-AAAAMMDD-hhmmss {weight_list[0]}\n" \
+                       f"problem-{active_years[0]}-24-AAAAMMDD-hhmmss {weight_list[0]}\n" \
+                       f"problem-{active_years[1]}-1-AAAAMMDD-hhmmss {weight_list[1]}\n" \
+                       f"problem-{active_years[1]}-24-AAAAMMDD-hhmmss {weight_list[1]}\n" \
+                       f"problem-{active_years[2]}-1-AAAAMMDD-hhmmss {weight_list[2]}\n" \
+                       f"problem-{active_years[2]}-24-AAAAMMDD-hhmmss {weight_list[2]}\n" \
+                       f"WEIGHT_SUM {sum(weight_list)}"
 
     with open(YearlyWeightWriter(tmp_path).output_dir / input_weight_file_name, 'r') as write_file:
         content = write_file.read()
