dc3712c30f425519e71007742dd35a3005b8d861
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-04-01 13:58:24 +0200

    fix sensitivity not showing error msg if no options provided--windows

--- src/cpp/exe/sensitivity/main.cpp
+++ src/cpp/exe/sensitivity/main.cpp
@@ -3,9 +3,9 @@
 
 #include "SensitivityFileLogger.h"
 #include "SensitivityInputReader.h"
+#include "SensitivityLogger.h"
 #include "SensitivityMasterLogger.h"
 #include "SensitivityStudy.h"
-#include "SensitivityLogger.h"
 
 namespace po = boost::program_options;
 
@@ -23,49 +23,49 @@ std::shared_ptr<SensitivityILogger> build_logger(
 }
 
 int main(int argc, char **argv) {
-  std::string json_input_path;
-  std::string json_output_path;
-  std::string benders_output_path;
-  std::string last_master_path;
-  std::string structure_path;
-  std::string log_path;
-
-  po::options_description desc("Allowed options");
-
-  desc.add_options()("help,h", "produce help message")(
-      "input,i", po::value<std::string>(&json_input_path)->required(),
-      "path to the json input file")(
-      "output,o", po::value<std::string>(&json_output_path)->required(),
-      "path to the sensitivity json output file")(
-      "benders,b", po::value<std::string>(&benders_output_path)->required(),
-      "path to the benders json output file")(
-      "master,m", po::value<std::string>(&last_master_path)->required(),
-      "path to the last master mps file")(
-      "structure,s", po::value<std::string>(&structure_path)->required(),
-      "path to the structure txt file")(
-      "log,l", po::value<std::string>(&log_path)->required(),
-      "path to the sensitivity log file");
-
-  po::variables_map opts;
-  po::store(po::parse_command_line(argc, argv, desc), opts);
-
-  if (opts.count("help")) {
-    std::cout << desc << std::endl;
-    return 0;
-  }
-
-  po::notify(opts);
-
-  auto logger = build_logger(log_path);
-
-  auto sensitivity_input_reader = SensitivityInputReader(
-      json_input_path, benders_output_path, last_master_path, structure_path);
-  SensitivityInputData input_data = sensitivity_input_reader.get_input_data();
-
-  auto writer = std::make_shared<SensitivityWriter>(json_output_path);
-  auto sensitivity_study = SensitivityStudy(input_data, logger, writer);
-
   try {
+    std::string json_input_path;
+    std::string json_output_path;
+    std::string benders_output_path;
+    std::string last_master_path;
+    std::string structure_path;
+    std::string log_path;
+
+    po::options_description desc("Allowed options");
+
+    desc.add_options()("help,h", "produce help message")(
+        "input,i", po::value<std::string>(&json_input_path)->required(),
+        "path to the json input file")(
+        "output,o", po::value<std::string>(&json_output_path)->required(),
+        "path to the sensitivity json output file")(
+        "benders,b", po::value<std::string>(&benders_output_path)->required(),
+        "path to the benders json output file")(
+        "master,m", po::value<std::string>(&last_master_path)->required(),
+        "path to the last master mps file")(
+        "structure,s", po::value<std::string>(&structure_path)->required(),
+        "path to the structure txt file")(
+        "log,l", po::value<std::string>(&log_path)->required(),
+        "path to the sensitivity log file");
+
+    po::variables_map opts;
+    po::store(po::parse_command_line(argc, argv, desc), opts);
+
+    if (opts.count("help")) {
+      std::cout << desc << std::endl;
+      return 0;
+    }
+
+    po::notify(opts);
+
+    auto logger = build_logger(log_path);
+
+    auto sensitivity_input_reader = SensitivityInputReader(
+        json_input_path, benders_output_path, last_master_path, structure_path);
+    SensitivityInputData input_data = sensitivity_input_reader.get_input_data();
+
+    auto writer = std::make_shared<SensitivityWriter>(json_output_path);
+    auto sensitivity_study = SensitivityStudy(input_data, logger, writer);
+
     sensitivity_study.launch();
     return 0;
   } catch (const std::exception &e) {
