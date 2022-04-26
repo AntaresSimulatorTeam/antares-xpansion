b2b44c87175c1df9a4f8e0644a81492490904469
Bari <azakir@silicom.fr>
2022-04-26 17:22:57 +0200

    split already-installed-link-profile file

--- src/python/antares_xpansion/profile_link_checker.py
+++ src/python/antares_xpansion/profile_link_checker.py
@@ -12,8 +12,13 @@ from antares_xpansion.flushed_print import flushed_print, INFO_MSG
 class ProfileLinkChecker:
     def __init__(self, candidate_file: Path, capacity_dir: Path) -> None:
         self.LINK_PROFILE_KEY = "link-profile"
-        self.DIRECT_LINK_PROFILE_KEY = "direct-link-profile"
-        self.INDIRECT_LINK_PROFILE_KEY = "indirect-link-profile"
+        self.ALREADY_INSTALLED_LINK_PROFILE_KEY = "already-installed-link-profile"
+        self.DIRECT_ALREADY_INSTALLED_LINK_PROFILE_KEY = "direct-" + \
+            self.ALREADY_INSTALLED_LINK_PROFILE_KEY
+        self.INDIRECT_ALREADY_INSTALLED_LINK_PROFILE_KEY = "indirect-" + \
+            self.ALREADY_INSTALLED_LINK_PROFILE_KEY
+        self.DIRECT_LINK_PROFILE_KEY = "direct-"+self.LINK_PROFILE_KEY
+        self.INDIRECT_LINK_PROFILE_KEY = "indirect-"+self.LINK_PROFILE_KEY
 
         if (not candidate_file.exists()):
             raise ProfileLinkChecker.CandidateFileNotFound(
@@ -34,17 +39,6 @@ class ProfileLinkChecker:
         pass
 
     def _read(self):
-        # default_values = {
-        #     'name': 'NA',
-        #     'link': 'NA',
-        #     'annual-cost-per-mw': '0',
-        #     'unit-size': '0',
-        #     'max-units': '0',
-        #     'max-investment': '0',
-        #     'link-profile': '1',
-        #     'already-installed-capacity': '0',
-        #     'already-installed-link-profile': '1'
-        # }
         self.config = configparser.ConfigParser()
         self.config.read(self._candidate_file)
 
@@ -59,6 +53,9 @@ class ProfileLinkChecker:
 
     def has_indirect_link_profile_key(self, section) -> bool:
         return self.config.has_option(section=section, option=self.INDIRECT_LINK_PROFILE_KEY)
+
+    def has_already_installed_link_profile_key(self, section) -> bool:
+        return self.config.has_option(section=section, option=self.ALREADY_INSTALLED_LINK_PROFILE_KEY)
 
     def update(self):
 
@@ -77,6 +74,22 @@ class ProfileLinkChecker:
                     candidate, self.DIRECT_LINK_PROFILE_KEY, direct_link_profile.name)
                 self.config.set(
                     candidate, self.INDIRECT_LINK_PROFILE_KEY, indirect_link_profile.name)
+
+            if self.has_already_installed_link_profile_key(candidate):
+                flushed_print(
+                    f"{INFO_MSG} in candidate {candidate} description, deprecated option: **{self.ALREADY_INSTALLED_LINK_PROFILE_KEY}** is replaced by **{self.DIRECT_ALREADY_INSTALLED_LINK_PROFILE_KEY}** and **{self.INDIRECT_ALREADY_INSTALLED_LINK_PROFILE_KEY}** options")
+                update_candidate_file = True
+                link_file = self.config.get(
+                    candidate, self.ALREADY_INSTALLED_LINK_PROFILE_KEY)
+                spliter = SplitLinkProfile(
+                    link_file, self._capacity_dir)
+                direct_link_profile, indirect_link_profile = spliter.split()
+                self.config.remove_option(
+                    candidate, self.ALREADY_INSTALLED_LINK_PROFILE_KEY)
+                self.config.set(
+                    candidate, self.DIRECT_ALREADY_INSTALLED_LINK_PROFILE_KEY, direct_link_profile.name)
+                self.config.set(
+                    candidate, self.INDIRECT_ALREADY_INSTALLED_LINK_PROFILE_KEY, indirect_link_profile.name)
 
         return update_candidate_file
 
