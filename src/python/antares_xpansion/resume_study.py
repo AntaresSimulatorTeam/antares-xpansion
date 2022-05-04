1c63679bbeef9a5ded1241a1ac67f80d8b68d544
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-05-04 10:49:53 +0200

    to update options.json file and copy last master

--- src/python/antares_xpansion/resume_study.py
+++ src/python/antares_xpansion/resume_study.py
@@ -2,17 +2,19 @@
 
 import json
 from pathlib import Path
+import shutil
 
 
 class ResumeStudy:
     def __init__(self) -> None:
         pass
 
-    def launch(self, simulation_output_path: Path, options_file: str, last_master_keyword):
+    def launch(self, simulation_output_path: Path, options_file: str, last_master_keyword, master_keyword):
         self.set_simulation_output_path(simulation_output_path)
         # last_master_file = simulation_output_path / last_master_file
 
-        self._update_options_file(options_file, last_master_keyword)
+        self._update_options_file(
+            options_file, last_master_keyword, master_keyword)
 
     def set_simulation_output_path(self, simulation_output_path: Path):
         if simulation_output_path.is_dir():
@@ -25,28 +27,49 @@ class ResumeStudy:
     class StudyOutputPathError(Exception):
         pass
 
-    class LastMasterFileNotFound(Exception):
-        pass
-
-    def _update_options_file(self, options_file, last_master_keyword: str):
+    def _update_options_file(self, options_file, last_master_keyword: str, master_keyword: str):
         options_file_path = self._simulation_output_path / options_file
         if not options_file_path.exists():
             raise ResumeStudy.OptionsFileNotFound(
                 f"last master file: {options_file_path} not found")
-        with open(self._simulation_output_path / options_file, "r") as json_file:
+        options_file_path = self._simulation_output_path / options_file
+        with open(options_file_path, "r") as json_file:
             options = json.load(json_file)
         try:
             last_master_file = options[last_master_keyword]
         except KeyError:
-            raise ResumeStudy.LastMasterKeyWordNotFound(
+            raise ResumeStudy.KeyWordNotFound(
                 f"Error {last_master_keyword} not found in {options_file_path}")
         last_master_file_path = self._simulation_output_path / last_master_file
+
         if not last_master_file_path.exists():
             raise ResumeStudy.LastMasterFileNotFound(
                 f"last master file: {last_master_file_path} not found")
 
+        try:
+            master_file = options[master_keyword]
+        except KeyError:
+            raise ResumeStudy.KeyWordNotFound(
+                f"Error {master_keyword} not found in {options_file_path}")
+        master_file_path = self._simulation_output_path / master_file
+        if not master_file_path.exists():
+            raise ResumeStudy.MasterFileNotFound(
+                f"master file: {master_file_path} not found")
+
+        options[master_keyword] = options[last_master_keyword]
+        with open(options_file_path, "w") as options_json:
+            json.dump(options, options_json)
+
+        shutil.copy(last_master_file_path, master_file_path)
+
     class OptionsFileNotFound(Exception):
         pass
 
-    class LastMasterKeyWordNotFound(Exception):
+    class KeyWordNotFound(Exception):
+        pass
+
+    class LastMasterFileNotFound(Exception):
+        pass
+
+    class MasterFileNotFound(Exception):
         pass
--- tests/python/test_resume_study.py
+++ tests/python/test_resume_study.py
@@ -10,8 +10,12 @@ class TestResumeStudy:
     def setup_method(self):
         self.created_ouput_dir = Path(tempfile.mkdtemp()) / "lp"
         self.created_ouput_dir.mkdir()
-        self.LAST_MASTER_MPS = "LAST_MASTER_MPS"
-        options = {self.LAST_MASTER_MPS: "master_last_iteration"}
+        self.LAST_MASTER_MPS_KEYWORD = "LAST_MASTER_MPS"
+        self.MASTER_MPS_KEYWORD = "MASTER"
+        self.last_master_file = "master_last_iteration"
+        self.master_file = "master"
+        options = {self.LAST_MASTER_MPS_KEYWORD: self.last_master_file,
+                   self.MASTER_MPS_KEYWORD: self.master_file}
 
         self.created_option_file = self.created_ouput_dir / "options.json"
         with open(self.created_option_file, 'w') as options_file:
@@ -23,20 +27,21 @@ class TestResumeStudy:
 
         resume_study = ResumeStudy()
         with pytest.raises(ResumeStudy.StudyOutputPathError):
-            resume_study.launch(output_path, "hello", "hello")
+            resume_study.launch(output_path, "hello", "hello", "hello")
 
     def test_fail_if_options_file_not_found(self, tmp_path):
         resume_study = ResumeStudy()
         with pytest.raises(ResumeStudy.OptionsFileNotFound):
-            resume_study.launch(self.created_ouput_dir, "hello", "hello")
+            resume_study.launch(self.created_ouput_dir,
+                                "hello", "hello", "master")
 
     def test_fail_if_last_master_file_invalid_keyword(self, tmp_path):
 
         resume_study = ResumeStudy()
 
-        with pytest.raises(ResumeStudy.LastMasterKeyWordNotFound):
+        with pytest.raises(ResumeStudy.KeyWordNotFound):
             resume_study.launch(self.created_ouput_dir,
-                                self.created_option_file.name, "hello")
+                                self.created_option_file.name, "hello", "master")
 
     def test_fail_if_last_master_file_not_found(self, tmp_path):
 
@@ -44,10 +49,59 @@ class TestResumeStudy:
 
         with pytest.raises(ResumeStudy.LastMasterFileNotFound):
             resume_study.launch(self.created_ouput_dir,
-                                self.created_option_file.name, self.LAST_MASTER_MPS)
+                                self.created_option_file.name, self.LAST_MASTER_MPS_KEYWORD, self.MASTER_MPS_KEYWORD)
 
-        # with open(self.created_ouput_dir /
-        #           self.created_option_file.name, "r") as json_file:
-        #     options = json.load(json_file)
+    def test_fail_if_master_file_not_found(self, tmp_path):
 
-        # assert options
+        resume_study = ResumeStudy()
+        create_last_master_file = self.created_ouput_dir / self.last_master_file
+        create_last_master_file.touch()
+        with pytest.raises(ResumeStudy.MasterFileNotFound):
+            resume_study.launch(self.created_ouput_dir,
+                                self.created_option_file.name, self.LAST_MASTER_MPS_KEYWORD, self.MASTER_MPS_KEYWORD)
+
+    def test_fail_if_options_file_is_updated(self, tmp_path):
+
+        resume_study = ResumeStudy()
+        create_last_master_file = self.created_ouput_dir / self.last_master_file
+        create_last_master_file.touch()
+        expected_last_master_file_content = "last master file content"
+        create_last_master_file.write_text(expected_last_master_file_content)
+
+        create_master_file = self.created_ouput_dir / self.master_file
+        create_master_file.touch()
+        create_master_file.write_text("master file content")
+
+        resume_study.launch(self.created_ouput_dir,
+                            self.created_option_file.name, self.LAST_MASTER_MPS_KEYWORD, self.MASTER_MPS_KEYWORD)
+
+        with open(self.created_option_file, "r") as options_json:
+            options = json.load(options_json)
+
+        assert options[self.MASTER_MPS_KEYWORD] == options[self.LAST_MASTER_MPS_KEYWORD]
+
+    def test_fail_if_master_file_is_updated(self, tmp_path):
+
+        resume_study = ResumeStudy()
+        create_last_master_file = self.created_ouput_dir / self.last_master_file
+        create_last_master_file.touch()
+        expected_last_master_file_content = "last master file content"
+        create_last_master_file.write_text(expected_last_master_file_content)
+
+        create_master_file = self.created_ouput_dir / self.master_file
+        create_master_file.touch()
+        create_master_file.write_text("master file content")
+
+        resume_study.launch(self.created_ouput_dir,
+                            self.created_option_file.name, self.LAST_MASTER_MPS_KEYWORD, self.MASTER_MPS_KEYWORD)
+
+        with open(self.created_option_file, "r") as options_json:
+            options = json.load(options_json)
+
+        assert options[self.MASTER_MPS_KEYWORD] == options[self.LAST_MASTER_MPS_KEYWORD]
+
+        with open(create_last_master_file) as last_master_file, open(create_master_file) as master_file:
+            last_master_file_content = last_master_file.readlines()
+            master_file_content = master_file.readlines()
+            assert master_file_content == [expected_last_master_file_content]
+            assert master_file_content == last_master_file_content
