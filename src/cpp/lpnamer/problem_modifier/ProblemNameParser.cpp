186dd4577a1b798a2724f88f5b1872b8e9f39226
Jason Marechal <jason.marechal+github@gmail.com>
2022-05-09 16:22:21 +0200

    Remove potentially unsafe strncpy usage

--- src/cpp/lpnamer/problem_modifier/ProblemNameParser.cpp
+++ src/cpp/lpnamer/problem_modifier/ProblemNameParser.cpp
@@ -3,21 +3,16 @@
 //
 
 #include <filesystem>
-#include <cstring>
+#include <boost/tokenizer.hpp>
 #include "ProblemNameParser.h"
 
-constexpr int MAX_FILE_NAME_LENGTH = 255;
-constexpr int BUFFER_LAST_CHARACTER_INDEX = MAX_FILE_NAME_LENGTH - 1;
-
-unsigned int MCYear(std::filesystem::path file_path) {
+unsigned int MCYear(const std::filesystem::path &file_path) {
   return MCYear(file_path.filename().string());
 }
-unsigned int MCYear(std::string problem_file_name) {
-  char file_name[MAX_FILE_NAME_LENGTH];
-  strncpy(file_name, problem_file_name.c_str(), MAX_FILE_NAME_LENGTH);
-  file_name[BUFFER_LAST_CHARACTER_INDEX] = '\0';
-  std::string prefix = strtok(file_name, "-");
-  std::string year = strtok(nullptr, "-");
+unsigned int MCYear(const std::string &problem_file_name) {
+  boost::tokenizer<> tok(problem_file_name);
+  auto prefix = tok.begin();
+  std::string year = *(++prefix);
   unsigned int mc_year = std::stoi(year);
   return mc_year;
 }
