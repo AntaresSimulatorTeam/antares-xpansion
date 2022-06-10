b39d1cfcce6c96db8a539e4e8fb0f605c58c1978
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-06-10 16:28:36 +0200

    Rewrite the code so that no longer need  to delete pointer

--- src/cpp/multisolver_interface/SolverCbc.cpp
+++ src/cpp/multisolver_interface/SolverCbc.cpp
@@ -178,9 +178,8 @@ void SolverCbc::read_basis(const std::filesystem::path &filename) {
   // readBasis returns 1 if successful
   zero_status_check(status - 1, "read basis");
   if (status == 1) {
-    CoinWarmStartBasis *basis = clps->getBasis();
-    _clp_inner_solver.setWarmStart(basis);
-    delete basis;
+    auto basis = std::unique_ptr<CoinWarmStartBasis>(clps->getBasis());
+    _clp_inner_solver.setWarmStart(basis.get());
   }
 }
 
