9ec90140f0d4309cc629560ea91a8d7dc123d189
Jason Marechal <jason.marechal+github@gmail.com>
2022-06-14 17:42:34 +0200

    Remove support for Antares Link file version < 700

--- tests/cpp/lp_namer/LinkdataRecordTest.cc
+++ tests/cpp/lp_namer/LinkdataRecordTest.cc
@@ -29,42 +29,11 @@ protected:
 	}
 };
 
-
-TEST_F(LinkdataRecordTest, versionPre700)
-{
-	LinkdataRecord record_l(1000, 500, 1,1,1);
-
-    ASSERT_EQ(record_l.modernAntaresVersion_, false);
-    ASSERT_EQ(record_l.fileColumns.directCapacity_, 1000);
-    ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 500);
-    ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 1);
-    ASSERT_EQ(record_l.fileColumns.indirectHurdlesCost_, 1);
-    ASSERT_EQ(record_l.fileColumns.impedances_, 1);
-    ASSERT_EQ(record_l.fileColumns.loopFlow_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMin_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMax_, 0);
-
-    record_l.reset();
-
-    ASSERT_EQ(record_l.modernAntaresVersion_, false);
-    ASSERT_EQ(record_l.fileColumns.directCapacity_, 0);
-    ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 0);
-    ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 0);
-    ASSERT_EQ(record_l.fileColumns.indirectHurdlesCost_, 0);
-    ASSERT_EQ(record_l.fileColumns.impedances_, 0);
-    ASSERT_EQ(record_l.fileColumns.loopFlow_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMin_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMax_, 0);
-
-
-}
-
 TEST_F(LinkdataRecordTest, version700)
 {
     LinkdataRecord::FileColumns aFileColumns {1000, 500, 1,1,1,1,1,1};
 	LinkdataRecord record_l(aFileColumns);
 
-    ASSERT_EQ(record_l.modernAntaresVersion_, true);
     ASSERT_EQ(record_l.fileColumns.directCapacity_, 1000);
     ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 500);
     ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 1);
@@ -76,7 +45,6 @@ TEST_F(LinkdataRecordTest, version700)
 
     record_l.reset();
 
-    ASSERT_EQ(record_l.modernAntaresVersion_, true);
     ASSERT_EQ(record_l.fileColumns.directCapacity_, 0);
     ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 0);
     ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 0);
@@ -89,31 +57,17 @@ TEST_F(LinkdataRecordTest, version700)
 
 TEST_F(LinkdataRecordTest, fromRow)
 {
-    LinkdataRecord modernRecord_l(true);
-    modernRecord_l.fillFromRow("1000\t500\t1\t1\t1\t1\t1\t1");
+    LinkdataRecord linkdata_record;
+    linkdata_record.fillFromRow("1000\t500\t1\t1\t1\t1\t1\t1");
 
-    ASSERT_EQ(modernRecord_l.modernAntaresVersion_, true);
-    ASSERT_EQ(modernRecord_l.fileColumns.directCapacity_, 1000);
-    ASSERT_EQ(modernRecord_l.fileColumns.indirectCapacity_, 500);
-    ASSERT_EQ(modernRecord_l.fileColumns.directHurdlesCost_, 1);
-    ASSERT_EQ(modernRecord_l.fileColumns.indirectHurdlesCost_, 1);
-    ASSERT_EQ(modernRecord_l.fileColumns.impedances_, 1);
-    ASSERT_EQ(modernRecord_l.fileColumns.loopFlow_, 1);
-    ASSERT_EQ(modernRecord_l.fileColumns.pShiftMin_, 1);
-    ASSERT_EQ(modernRecord_l.fileColumns.pShiftMax_, 1);
-
-    LinkdataRecord record_l(false);
-    record_l.fillFromRow("1000\t500\t1\t1\t1\t1\t1\t1");
-
-    ASSERT_EQ(record_l.modernAntaresVersion_, false);
-    ASSERT_EQ(record_l.fileColumns.directCapacity_, 1000);
-    ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 500);
-    ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 1);
-    ASSERT_EQ(record_l.fileColumns.indirectHurdlesCost_, 1);
-    ASSERT_EQ(record_l.fileColumns.impedances_, 1);
-    ASSERT_EQ(record_l.fileColumns.loopFlow_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMin_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMax_, 0);
+    ASSERT_EQ(linkdata_record.fileColumns.directCapacity_, 1000);
+    ASSERT_EQ(linkdata_record.fileColumns.indirectCapacity_, 500);
+    ASSERT_EQ(linkdata_record.fileColumns.directHurdlesCost_, 1);
+    ASSERT_EQ(linkdata_record.fileColumns.indirectHurdlesCost_, 1);
+    ASSERT_EQ(linkdata_record.fileColumns.impedances_, 1);
+    ASSERT_EQ(linkdata_record.fileColumns.loopFlow_, 1);
+    ASSERT_EQ(linkdata_record.fileColumns.pShiftMin_, 1);
+    ASSERT_EQ(linkdata_record.fileColumns.pShiftMax_, 1);
 }
 
 
@@ -121,17 +75,14 @@ TEST_F(LinkdataRecordTest, fromRow)
 TEST_F(LinkdataRecordTest, toRow)
 {
     LinkdataRecord::FileColumns aFileColumns {1000, 500, 1,1,1,1,1,1};
-    LinkdataRecord modernRecord_l(aFileColumns);
-    ASSERT_EQ(modernRecord_l.to_row("\t"), "1000.000000\t500.000000\t1.000000\t1.000000\t1.000000\t1.000000\t1.000000\t1.000000");
-
-    LinkdataRecord record_l(1000, 500, 1,1,1);
-    ASSERT_EQ(record_l.to_row("\t"), "1000.000000\t500.000000\t1.000000\t1.000000\t1.000000");
+    LinkdataRecord linkdata_record(aFileColumns);
+    ASSERT_EQ(linkdata_record.to_row("\t"), "1000.000000\t500.000000\t1.000000\t1.000000\t1.000000\t1.000000\t1.000000\t1.000000");
 }
 
 
 TEST_F(LinkdataRecordTest, modernAntaresVersionRecord)
 {
-    LinkdataRecord record_l(true);
+    LinkdataRecord record_l;
     ASSERT_EQ(record_l.fileColumns.directCapacity_, 0);
     ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 0);
     ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 0);
@@ -155,42 +106,3 @@ TEST_F(LinkdataRecordTest, modernAntaresVersionRecord)
     ASSERT_EQ(record_l.fileColumns.directCapacity_, 10);
     ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 20);
 }
-
-
-TEST_F(LinkdataRecordTest, oldAntaresVersionRecord)
-{
-    LinkdataRecord record_l(false);
-    ASSERT_EQ(record_l.fileColumns.directCapacity_, 0);
-    ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 0);
-    ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 0);
-    ASSERT_EQ(record_l.fileColumns.indirectHurdlesCost_, 0);
-    ASSERT_EQ(record_l.fileColumns.impedances_, 0);
-    ASSERT_EQ(record_l.fileColumns.loopFlow_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMin_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMax_, 0);
-
-    record_l.fillFromRow("1\t2\t3\t4\t5");
-    ASSERT_EQ(record_l.fileColumns.directCapacity_, 1);
-    ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 2);
-    ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 3);
-    ASSERT_EQ(record_l.fileColumns.indirectHurdlesCost_, 4);
-    ASSERT_EQ(record_l.fileColumns.impedances_, 5);
-    ASSERT_EQ(record_l.fileColumns.loopFlow_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMin_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMax_, 0);
-
-    record_l.fillFromRow("1\t2\t3\t4\t5\t6\t7\t8");
-    ASSERT_EQ(record_l.fileColumns.directCapacity_, 1);
-    ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 2);
-    ASSERT_EQ(record_l.fileColumns.directHurdlesCost_, 3);
-    ASSERT_EQ(record_l.fileColumns.indirectHurdlesCost_, 4);
-    ASSERT_EQ(record_l.fileColumns.impedances_, 5);
-    ASSERT_EQ(record_l.fileColumns.loopFlow_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMin_, 0);
-    ASSERT_EQ(record_l.fileColumns.pShiftMax_, 0);
-
-    record_l.updateCapacities(10, 20);
-    ASSERT_EQ(record_l.fileColumns.directCapacity_, 10);
-    ASSERT_EQ(record_l.fileColumns.indirectCapacity_, 20);
-}
-
