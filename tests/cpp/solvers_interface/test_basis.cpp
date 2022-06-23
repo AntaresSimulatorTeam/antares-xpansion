461d802fa84c5c222c150631bea59b0d740ff797
Thomas Bittar <thomas.bittar@rte-france.com>
2022-06-23 16:08:03 +0200

    Clean test_basis

--- tests/cpp/solvers_interface/test_basis.cpp
+++ tests/cpp/solvers_interface/test_basis.cpp
@@ -2,8 +2,7 @@
 #include "define_datas.hpp"
 #include "multisolver_interface/Solver.h"
 
-TEST_CASE("Writing and reading basis", "[basis]") {
-  std::cout << "Writing and reading base test" << std::endl;
+TEST_CASE("Write and read basis", "[basis]") {
   AllDatas datas;
   fill_datas(datas);
 
@@ -11,41 +10,41 @@ TEST_CASE("Writing and reading basis", "[basis]") {
 
   auto inst = GENERATE(MIP_TOY);
   std::string instance = datas[inst]._path;
-  std::string solver_name = "CBC";
-  SolverAbstract::Ptr solver = factory.create_solver(solver_name);
-  solver->init();
-  solver->read_prob_mps(instance);
+  SECTION("Loop on solvers") for (auto const& solver_name : factory.get_solvers_list()) {
+    // As CLP is a pure LP solver, it cannot pass this test
+    if (solver_name != "CLP") {
+      SolverAbstract::Ptr solver = factory.create_solver(solver_name);
+      solver->init();
+      solver->read_prob_mps(instance);
+      solver->solve_mip();
 
-  std::string basis_file =
-      "/home/bittartho/development/antares-xpansion/test_basis.bss";
+      std::vector<int> rstatus(solver->get_nrows());
+      std::vector<int> cstatus(solver->get_ncols());
+      solver->get_basis(rstatus.data(), cstatus.data());
 
-  solver->solve_mip();
+      std::string basis_file = std::tmpnam(nullptr);
+      solver->write_basis(basis_file);
 
-  std::vector<int> rstatus(solver->get_nrows());
-  std::vector<int> cstatus(solver->get_ncols());
-  solver->get_basis(rstatus.data(), cstatus.data());
 
-  solver->write_basis(basis_file);
-  SolverAbstract::Ptr solver_2 = factory.create_solver(solver_name);
-  solver_2->init();
-  solver_2->read_prob_mps(instance);
+      SolverAbstract::Ptr solver_2 = factory.create_solver(solver_name);
+      solver_2->init();
+      solver_2->read_prob_mps(instance);
+      solver_2->read_basis(basis_file);
 
-  solver_2->read_basis(basis_file);
+      std::vector<int> rstatus_2(solver_2->get_nrows());
+      std::vector<int> cstatus_2(solver_2->get_ncols());
+      solver_2->get_basis(rstatus_2.data(), cstatus_2.data());
 
-  std::vector<int> rstatus_2(solver_2->get_nrows());
-  std::vector<int> cstatus_2(solver_2->get_ncols());
-  solver_2->get_basis(rstatus_2.data(), cstatus_2.data());
-
-  std::cout << "Row status" << std::endl;
-  for (int i(0); i < solver->get_nrows(); i++) {
-    // REQUIRE(rstatus[i] == rstatus_2[i]);
-    std::cout << rstatus[i] << " " << rstatus_2[i] << std::endl;
+      std::cout << "Row status" << std::endl;
+      for (int i(0); i < solver->get_nrows(); i++) {
+        REQUIRE(rstatus[i] == rstatus_2[i]);
+      }
+      std::cout << "Col status" << std::endl;
+      for (int i(0); i < solver->get_ncols(); i++) {
+        REQUIRE(cstatus[i] == cstatus_2[i]);
+      }
+      solver->free();
+      solver_2->free();
+    }
   }
-  std::cout << "Col status" << std::endl;
-  for (int i(0); i < solver->get_ncols(); i++) {
-    // REQUIRE(cstatus[i] == cstatus_2[i]);
-    std::cout << cstatus[i] << " " << cstatus_2[i] << std::endl;
-  }
-  solver->free();
-  solver_2->free();
 }
