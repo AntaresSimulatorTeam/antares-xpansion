1def23a7a3493b0d2bf18490a4ab197d38101069
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-07-12 14:43:39 +0200

    update tests

--- tests/cpp/logger/logger_test.cpp
+++ tests/cpp/logger/logger_test.cpp
@@ -158,6 +158,8 @@ TEST_F(FileLoggerTest, ShouldPrintBestIterationsInfos) {
       << prefix << indent_0 << indent_1
       << "candidate2 =  8.00 invested MW -- possible interval [ 3.00; 40.00] MW"
       << std::endl
+      << prefix << indent_1 << "Subproblems solved in " << l.subproblem_time
+      << " s" << std::endl
       << prefix << indent_0 << "Solution =" << std::endl
       << prefix << indent_0 << indent_1 << "Operational cost =       15.50 Me"
       << std::endl
