76d0e8ef4399786c099b439f7a1cf04830c2c124
Bari <azakir@silicom.fr>
2022-07-27 17:00:08 +0200

    add compressed-archive reader

---
+++ src/cpp/helpers/ArchiveReader.h
@@ -0,0 +1,22 @@
+#ifndef _ARCHIVEREADER_H
+#define _ARCHIVEREADER_H
+#include "ArchiveIO.h"
+class ArchiveReader : public ArchiveIO {
+ private:
+  void* internalPointer_ = NULL;
+  void* handle_ = NULL;
+
+ public:
+  explicit ArchiveReader(const std::filesystem::path& archivePath);
+
+  int32_t Close() override;
+  void Delete() override;
+  //   void* InternalPointer() const override;
+  //   void* handle() const override;
+
+  int Open() override;
+  int32_t ExtractFile(const std::filesystem::path& FileToExtractPath);
+  int32_t ExtractFile(const std::filesystem::path& FileToExtractPath,
+                      const std::filesystem::path& destination);
+};
+#endif  // _ARCHIVEREADER_H
