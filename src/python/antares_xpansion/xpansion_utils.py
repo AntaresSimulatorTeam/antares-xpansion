9eb598ee6794d62233786faabe6d08a6a35ae5b7
Jason Marechal <jason.marechal+github@gmail.com>
2022-09-07 14:36:26 +0200

    Filter antares output updated to 8.3

--- src/python/antares_xpansion/xpansion_utils.py
+++ src/python/antares_xpansion/xpansion_utils.py
@@ -4,6 +4,7 @@
 
 import os
 
+
 def read_and_write_mps(root_path):
     """
         :return: a dictionary giving instance file, variables file and
@@ -12,9 +13,12 @@ def read_and_write_mps(root_path):
     result = {}
     sorted_root_dir = sorted(os.listdir(root_path))
 
+    mps_ext = '.mps'
+    txt_ext = ".txt"
     for instance in sorted_root_dir:
-        if '.mps' in instance and not '-1.mps' in instance:
-            buffer_l = instance.strip().split("-")
+        if mps_ext in instance:
+            buffer_l = instance[:-len(mps_ext)]
+            buffer_l = buffer_l.strip().split("-")
             year = int(buffer_l[1])
             week = int(buffer_l[2])
             if (year, week) not in result:
@@ -22,14 +26,16 @@ def read_and_write_mps(root_path):
 
     for instance in sorted_root_dir:
         if 'variables' in instance:
-            buffer_l = instance.strip().split("-")
+            buffer_l = instance[:-len(txt_ext)]
+            buffer_l = buffer_l.strip().split("-")
             year = int(buffer_l[1])
             week = int(buffer_l[2])
             result[year, week][1] = instance
 
     for instance in sorted_root_dir:
         if 'constraints' in instance:
-            buffer_l = instance.strip().split("-")
+            buffer_l = instance[:-len(txt_ext)]
+            buffer_l = buffer_l.strip().split("-")
             year = int(buffer_l[1])
             week = int(buffer_l[2])
             result[year, week][2] = instance
