bade68475309d17dcf01115d3dcf142da2fa043d
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-09-20 18:07:15 +0200

    use parameterized values in test fixture

--- tests/end_to_end/restart/test_restartBendersSequentialEndToEnd.py
+++ tests/end_to_end/restart/test_restartBendersSequentialEndToEnd.py
@@ -1,10 +1,17 @@
 import pytest
-from test_restartBendersEndToEnd import run_solver
+from .test_restartBendersEndToEnd import run_solver
 
 ## TESTS ##
 
 
+from .studies import study_parameters, study_values
+
+
+@pytest.mark.parametrize(
+    study_parameters,
+    study_values,
+)
 @pytest.mark.optim
 @pytest.mark.benderssequential
-def test_001_sequential(install_dir, tmp_path):
-    run_solver(install_dir, 'BENDERS_SEQUENTIAL', tmp_path)
+def test_001_sequential(install_dir, tmp_path, study):
+    run_solver(install_dir, 'BENDERS_SEQUENTIAL', tmp_path, study)
