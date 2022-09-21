795cc3a89d6ae62411f746b613f1ac7e41162a9f
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-09-21 10:24:54 +0200

    avoid tmp_path in mpi tests

--- tests/end_to_end/restart/test_restartBendersmpibendersEndToEnd.py
+++ tests/end_to_end/restart/test_restartBendersmpibendersEndToEnd.py
@@ -13,18 +13,16 @@ from .studies import study_parameters, study_values
 @pytest.mark.bendersmpi
 def test_001_mpibenders(install_dir, tmp_path, study, allow_run_as_root):
 
-    instance_path = study['path']
-    if(tmp_path.exists()):
-        shutil.rmtree(tmp_path)
-    shutil.copytree(instance_path, tmp_path)
+    instance_path = Path(study['path'])
 
-    expansion_dir = tmp_path / "expansion"
+    expansion_dir = instance_path / "expansion"
     if expansion_dir.is_dir():
         shutil.rmtree(expansion_dir)
 
     expansion_dir.mkdir()
-    shutil.copyfile(tmp_path / study["output_file"],
+    shutil.copyfile(instance_path / study["output_file"],
                     expansion_dir / "out.json")
-    shutil.copyfile(tmp_path / study["last_iteration_file"],
+    shutil.copyfile(instance_path / study["last_iteration_file"],
                     expansion_dir / "last_iteration.json")
-    run_solver(install_dir, 'BENDERS_MPI', tmp_path, study, allow_run_as_root)
+    run_solver(install_dir, 'BENDERS_MPI',
+               instance_path, study, allow_run_as_root)
