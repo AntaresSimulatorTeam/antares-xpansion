309caaa25811d5f9caba521a68d4dc5e1bc53046
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-10-03 16:45:51 +0200

    add ProblemGenerationExeOptionsTest

--- tests/cpp/lp_namer/ProblemGenerationExeOptionsTest.cpp
+++ tests/cpp/lp_namer/ProblemGenerationExeOptionsTest.cpp
@@ -20,3 +20,26 @@ TEST_F(ProblemGenerationExeOptionsTest, WithOutOutputOption) {
   }
 }
 
+TEST_F(ProblemGenerationExeOptionsTest, MasterFormulationDefaultValue) {
+  const char argv0[] = "lp.exe ";
+  const char argv1[] = "--output";
+  const char argv2[] = "something";
+  std::vector<const char*> ppargv = {argv0, argv1, argv2};
+  problem_generation_options_parser_.Parse(3, ppargv.data());
+  ASSERT_EQ(problem_generation_options_parser_.MasterFormulation(),
+            std::string("relaxed"));
+} 
+TEST_F(ProblemGenerationExeOptionsTest, AdditionalConstraintsFilenameIsNotEmptyIfGiven) {
+  const char argv0[] = "lp.exe ";
+  const char argv1[] = "--output";
+  const char argv2[] = "something";
+  const char argv3[] = "-e";
+  std::vector<const char*> ppargv = {argv0, argv1, argv2, argv3};
+  try {
+    problem_generation_options_parser_.Parse(4, ppargv.data());
+  } catch (const std::exception& e) {
+    EXPECT_EQ(e.what(),
+              std::string("the required argument for option '--exclusion-files' is missing"));
+  }
+} 
+
