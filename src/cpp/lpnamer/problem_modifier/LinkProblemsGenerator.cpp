443994e3def848aba72ece841524101675f6280f
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-12-08 15:27:21 +0100

    activate --zip-mps for lpnamer E2E tests

--- src/cpp/lpnamer/problem_modifier/LinkProblemsGenerator.cpp
+++ src/cpp/lpnamer/problem_modifier/LinkProblemsGenerator.cpp
@@ -150,13 +150,16 @@ void LinkProblemsGenerator::treatloop(const std::filesystem::path &root,
         [&](const auto &mps) { treat(root, mps, couplings, reader, writer); });
     writer.Close();
     writer.Delete();
+    reader.Close();
+    reader.Delete();
     std::filesystem::remove(archivePath);
     std::filesystem::rename(tmpArchivePath, lpDir_ / (MPS_ZIP_FILE + ZIP_EXT));
   } else {
     std::for_each(
         std::execution::par, mpsList.begin(), mpsList.end(),
         [&](const auto &mps) { treat(root, mps, couplings, reader); });
+
+    reader.Close();
+    reader.Delete();
   }
-  reader.Close();
-  reader.Delete();
 }
