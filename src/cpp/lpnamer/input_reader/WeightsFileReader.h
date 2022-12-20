d67866805491a5b3321db9432d7e7592769000fd
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-12-20 17:05:45 +0100

    and CountValues and checckweights null

--- src/cpp/lpnamer/input_reader/WeightsFileReader.h
+++ src/cpp/lpnamer/input_reader/WeightsFileReader.h
@@ -2,6 +2,7 @@
 #define SRC_CPP_LPNAMER_INPUTREADER_WEIGHTSFILEREADER_H
 #include <filesystem>
 #include <set>
+#include <stdexcept>
 
 #include "ProblemGenerationLogger.h"
 
@@ -10,6 +11,9 @@ class WeightsFileReader {
   std::filesystem::path weights_file_path_;
   std::set<int> active_years_;
   ProblemGenerationLog::ProblemGenerationLoggerSharedPointer logger_;
+  std::ifstream file_reader_;
+  int CountValues() const;
+  bool AreAllWeightsNull() const;
 
  public:
   explicit WeightsFileReader(
@@ -19,6 +23,17 @@ class WeightsFileReader {
       : weights_file_path_(weights_file_path),
         active_years_(active_years),
         logger_(logger) {}
-  bool CheckWeightsFile() const;
+
+  class WeightsFileOpenError : public std::runtime_error {
+   public:
+    WeightsFileOpenError(const std::string& msg) : std::runtime_error(msg) {}
+  };
+
+  /// @brief checks that the yearly-weights file exists and has correct format:
+  // column of non-negative weights
+  // sum of weights is positive
+  // nb_weight equal nb_active_yearse
+  /// @return true if it's ok
+  bool CheckWeightsFile();
 };
 #endif  // SRC_CPP_LPNAMER_INPUTREADER_WEIGHTSFILEREADER_H
