d27b4c9babe8ba4625970ccfd7487b7c28afe11f
Abdoulbari ZAKIR <azakir@silicom.fr>
2022-12-21 16:42:29 +0100

    delete unused functions

--- src/cpp/lpnamer/helper/common_lpnamer.h
+++ src/cpp/lpnamer/helper/common_lpnamer.h
@@ -10,14 +10,4 @@
 #include <sstream>
 #include <string>
 #include <vector>
-
-inline bool contains(std::string const& v1, std::string const& v2) {
-  return v1.find(v2) != std::string::npos;
-}
-inline int to_int(std::string const& value) {
-  std::stringstream buffer;
-  buffer << value;
-  int result;
-  buffer >> result;
-  return result;
-}
+namespace common_lpnamer {}  // namespace common_lpnamer
