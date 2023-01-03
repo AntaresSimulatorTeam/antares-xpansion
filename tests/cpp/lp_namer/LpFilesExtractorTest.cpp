ad12ab5a6e9c0614a3b2cbf2baeea1b202eeb646
Abdoulbari ZAKIR <azakir@silicom.fr>
2023-01-03 11:34:54 +0100

    export area & interco checker test

--- tests/cpp/lp_namer/LpFilesExtractorTest.cpp
+++ tests/cpp/lp_namer/LpFilesExtractorTest.cpp
@@ -3,9 +3,17 @@
 #include "LoggerBuilder.h"
 #include "LpFilesExtractor.h"
 
-const std::filesystem::path empty_archive =
-    std::filesystem::path("data_test") / "tests_lpnamer" / "LpFilesExtractor" /
-    "empty_archive.zip";
+const auto LP_FILE_EXTRACTOR_TEST_DIR =
+    std::filesystem::path("data_test") / "tests_lpnamer" / "LpFilesExtractor";
+const std::filesystem::path EMPTY_ARCHIVE =
+    LP_FILE_EXTRACTOR_TEST_DIR / "empty_archive.zip";
+const std::filesystem::path TWO_AREAS_FILES_ARCHIVE =
+    LP_FILE_EXTRACTOR_TEST_DIR / "2_areas_files.zip";
+const std::filesystem::path THREE_INTERCOS_FILES_ARCHIVE =
+    LP_FILE_EXTRACTOR_TEST_DIR / "3_intercos_files.zip";
+const std::filesystem::path ONE_AREA_0_INTERCO =
+    LP_FILE_EXTRACTOR_TEST_DIR / "test_0_interco_1_area.zip";
+
 class LpFilesExtractorTest : public ::testing::Test {
  protected:
   ProblemGenerationLog::ProblemGenerationLoggerSharedPointer logger_ =
@@ -14,7 +22,7 @@ class LpFilesExtractorTest : public ::testing::Test {
 
 TEST_F(LpFilesExtractorTest, IfNoAreaFileIsInAntaresArchive) {
   auto lp_files_extractor = LpFilesExtractor(
-      empty_archive, std::filesystem::temp_directory_path(), logger_);
+      EMPTY_ARCHIVE, std::filesystem::temp_directory_path(), logger_);
 
   std::ostringstream expectedErrorString;
   expectedErrorString << "No area*.txt file found" << std::endl;
@@ -25,16 +33,43 @@ TEST_F(LpFilesExtractorTest, IfNoAreaFileIsInAntaresArchive) {
     EXPECT_EQ(e.what(), expectedErrorString.str());
   }
 }
-TEST_F(LpFilesExtractorTest, IfNoAreaFileIsInAntaresArchive) {
+TEST_F(LpFilesExtractorTest, IfMoreThanOneAreaFileFoundInAntaresArchive) {
   auto lp_files_extractor = LpFilesExtractor(
-      empty_archive, std::filesystem::temp_directory_path(), logger_);
+      TWO_AREAS_FILES_ARCHIVE, std::filesystem::temp_directory_path(), logger_);
 
   std::ostringstream expectedErrorString;
-  expectedErrorString << "No area*.txt file found" << std::endl;
+  expectedErrorString << "More than one area*.txt file found" << std::endl;
 
   try {
     lp_files_extractor.ExtractFiles();
   } catch (const LpFilesExtractor::ErrorWithAreaFile& e) {
+    EXPECT_EQ(e.what(), expectedErrorString.str());
+  }
+}
+TEST_F(LpFilesExtractorTest, IfNoIntercoFileIsInAntaresArchive) {
+  auto lp_files_extractor = LpFilesExtractor(
+      ONE_AREA_0_INTERCO, std::filesystem::temp_directory_path(), logger_);
+
+  std::ostringstream expectedErrorString;
+  expectedErrorString << "No interco*.txt file found" << std::endl;
+
+  try {
+    lp_files_extractor.ExtractFiles();
+  } catch (const LpFilesExtractor::ErrorWithIntercosFile& e) {
+    EXPECT_EQ(e.what(), expectedErrorString.str());
+  }
+}
+TEST_F(LpFilesExtractorTest, IfMoreThanOneIntercoFileFoundInAntaresArchive) {
+  auto lp_files_extractor =
+      LpFilesExtractor(THREE_INTERCOS_FILES_ARCHIVE,
+                       std::filesystem::temp_directory_path(), logger_);
+
+  std::ostringstream expectedErrorString;
+  expectedErrorString << "More than one interco*.txt file found" << std::endl;
+
+  try {
+    lp_files_extractor.ExtractFiles();
+  } catch (const LpFilesExtractor::ErrorWithIntercosFile& e) {
     EXPECT_EQ(e.what(), expectedErrorString.str());
   }
 }
