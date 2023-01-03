9773b48e435d89010f187ab303c9f0e5d82a6534
Abdoulbari ZAKIR <azakir@silicom.fr>
2023-01-03 10:30:09 +0100

    format

--- src/cpp/lpnamer/main/include/ProblemGenerationExeOptions.h
+++ src/cpp/lpnamer/main/include/ProblemGenerationExeOptions.h
@@ -2,6 +2,7 @@
 #define ANTARES_XPANSION_SRC_CPP_LPNAMER_MAIN_INCLUDE_PROBLEMGENERATIONEXEOPTIONS_H
 #include <filesystem>
 #include <vector>
+
 #include "OptionsParser.h"
 
 class ProblemGenerationExeOptions : public OptionsParser {
@@ -18,7 +19,9 @@ class ProblemGenerationExeOptions : public OptionsParser {
   ProblemGenerationExeOptions();
 
   virtual ~ProblemGenerationExeOptions() = default;
-  std::filesystem::path XpansionOutputDir() const { return xpansion_output_dir_; }
+  std::filesystem::path XpansionOutputDir() const {
+    return xpansion_output_dir_;
+  }
   std::string MasterFormulation() const { return master_formulation_; }
   std::string AdditionalConstraintsFilename() const {
     return additional_constraintFilename_l_;
