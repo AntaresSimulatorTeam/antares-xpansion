33a1404839b9b7ae6b9ae899d2cdb777616e749b
Abdoulbari ZAKIR <azakir@silicom.fr>
2023-01-17 16:19:49 +0100

    delete old benders run funcs

--- src/cpp/benders/benders_sequential/include/BendersSequentialMain.h
+++
@@ -1,9 +0,0 @@
-#ifndef ANTARES_XPANSION_SRC_CPP_BENDERS_BENDERS_SEQUENTIAL_INCLUDE_BENDERSSEQUENTIALMAIN_H
-#define ANTARES_XPANSION_SRC_CPP_BENDERS_BENDERS_SEQUENTIAL_INCLUDE_BENDERSSEQUENTIALMAIN_H
-#include <filesystem>
-#include <string>
-
-int BendersSequentialMain(int argc, char **argv);
-int BendersSequentialMain(int argc, char **argv,
-                          const std::filesystem::path &options_file);
-#endif  // ANTARES_XPANSION_SRC_CPP_BENDERS_BENDERS_SEQUENTIAL_INCLUDE_BENDERSSEQUENTIALMAIN_H
