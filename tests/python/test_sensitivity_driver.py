7b9aa841c12bbc68afbb45a27a983181932a8a43
Jason Marechal <jason.marechal+github@gmail.com>
2023-02-27 10:28:56 +0100

    Fix test

--- tests/python/test_sensitivity_driver.py
+++ tests/python/test_sensitivity_driver.py
@@ -1,9 +1,12 @@
-import pytest
+import os.path
+import shutil
 import sys
 from pathlib import Path
 from unittest.mock import patch
 
+import pytest
 from antares_xpansion.sensitivity_driver import SensitivityDriver
+
 from file_creation import _create_empty_file_from_list
 
 SUBPROCESS_RUN = "antares_xpansion.sensitivity_driver.subprocess.run"
@@ -22,12 +25,13 @@ class TestSensitivityDriver:
                 "mock",
                 "mock",
                 "a_path",
+                tmp_path,
             )
 
     def test_non_existing_input_file(self, tmp_path):
         simulation_path = tmp_path
         sensitivity_driver = SensitivityDriver("")
-        with pytest.raises(SensitivityDriver.SensitivityFilePathError):
+        with pytest.raises(SensitivityDriver.SensitivityOutputPathError):
             sensitivity_driver.launch(
                 simulation_path,
                 tmp_path / "i_dont_exist",
@@ -37,16 +41,19 @@ class TestSensitivityDriver:
                 "",
                 "",
                 "a_path",
+                tmp_path,
             )
 
     def test_sensitivity_cmd(self, tmp_path):
-        simulation_path = tmp_path
+        simulation_path = os.path.join(tmp_path, "output")
+        Path(simulation_path).mkdir()
         exe_path = "/Path/to/bin1"
         sensitivity_driver = SensitivityDriver(exe_path)
 
         input_files = ["json_in.json", "benders_out", "master", "my_basis", "structure"]
-        input_paths = list(map(lambda x: tmp_path / x, input_files))
-        _create_empty_file_from_list(tmp_path, input_files)
+        input_paths = list(map(lambda x: os.path.join(simulation_path, x), input_files))
+        _create_empty_file_from_list(simulation_path, input_files)
+        shutil.make_archive(simulation_path, "zip", simulation_path)
 
         json_out_path = "somefile.json"
         sensitivity_log_path = "a_path"
@@ -54,7 +61,7 @@ class TestSensitivityDriver:
         with patch(SUBPROCESS_RUN, autospec=True) as run_function:
             run_function.return_value.returncode = 0
             sensitivity_driver.launch(
-                simulation_path,
+                f"{simulation_path}.zip",
                 input_paths[0],
                 input_paths[1],
                 input_paths[2],
@@ -62,6 +69,7 @@ class TestSensitivityDriver:
                 input_paths[4],
                 json_out_path,
                 sensitivity_log_path,
+                tmp_path,
             )
             expected_cmd = [
                 exe_path,
@@ -85,13 +93,15 @@ class TestSensitivityDriver:
             )
 
     def test_sensitivity_exe_error(self, tmp_path):
-        simulation_path = tmp_path
+        simulation_path = os.path.join(tmp_path, "output")
+        Path(simulation_path).mkdir()
         exe_path = "/Path/to/bin1"
         sensitivity_driver = SensitivityDriver(exe_path)
 
         input_files = ["json_in.json", "benders_out", "master", "my_basis", "structure"]
-        input_paths = list(map(lambda x: tmp_path / x, input_files))
-        _create_empty_file_from_list(tmp_path, input_files)
+        input_paths = list(map(lambda x: os.path.join(simulation_path, x), input_files))
+        _create_empty_file_from_list(simulation_path, input_files)
+        shutil.make_archive(simulation_path, "zip", simulation_path)
 
         json_out_path = "somefile.json"
         sensitivity_log_path = "a_path"
@@ -100,7 +110,7 @@ class TestSensitivityDriver:
             run_function.return_value.returncode = 1
             with pytest.raises(SensitivityDriver.SensitivityExeError):
                 sensitivity_driver.launch(
-                    simulation_path,
+                    f"{simulation_path}.zip",
                     input_paths[0],
                     input_paths[1],
                     input_paths[2],
@@ -108,4 +118,5 @@ class TestSensitivityDriver:
                     input_paths[4],
                     json_out_path,
                     sensitivity_log_path,
+                    tmp_path,
                 )
