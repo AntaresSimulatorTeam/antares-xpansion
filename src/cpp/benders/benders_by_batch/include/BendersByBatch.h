b9d388aec665f241bcfa03904b9dd096c6c9fd10
Abdoulbari ZAKIR <azakir@silicom.fr>
2023-03-07 09:25:27 +0100

    forgotten file from previous commit

--- src/cpp/benders/benders_by_batch/include/BendersByBatch.h
+++ src/cpp/benders/benders_by_batch/include/BendersByBatch.h
@@ -22,18 +22,21 @@ class BendersByBatch : public BendersMpi {
   void BroadcastSingleSubpbCostsUnderApprox();
 
  private:
-  void GetSubproblemCut(SubProblemDataMap &subproblem_cut_package,
-                        const std::vector<std::string> &batch_sub_problems,
-                        double *sum) const;
+  void GetSubproblemCut(
+      SubProblemDataMap &subproblem_cut_package,
+      const std::vector<std::string> &batch_sub_problems,
+      double *batch_subproblems_costs_contribution_in_gap_per_proc);
   BatchCollection batch_collection_;
   void MasterLoop();
   int SolveBatches();
-  // void SeparationLoop();
+  int SeparationLoop();
+  void UpdateRemainingEpsilon();
   size_t number_of_batch_;
   unsigned current_batch_id_;
   int number_of_sub_problem_resolved_;
   double remaining_epsilon_;
   double cumulative_subproblems_timer_per_iter_;
+  bool misprice_;
 };
 
 #endif  // SRC_CPP_BENDERS_BENDERS_BY_BATCH_INCLUDE_BENDERSBYBATCH_H_
