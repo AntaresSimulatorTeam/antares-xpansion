1fb8db9716501182d41d7f1d8527e28f36ada306
Abdoulbari ZAKIR <azakir@silicom.fr>
2023-05-02 11:06:40 +0200

    print useful message

--- src/cpp/lpnamer/main/RunProblemGeneration.cpp
+++ src/cpp/lpnamer/main/RunProblemGeneration.cpp
@@ -146,6 +146,8 @@ void RunProblemGeneration(
   Version first_version_without_variables_files("8.6");
   auto rename_variables =
       antares_version < first_version_without_variables_files;
+  (*logger)(ProblemGenerationLog::LOGLEVEL::INFO)
+      << "rename variable: " << std::boolalpha << rename_variables << std::endl;
 
   if (use_zip_implementation) {
     std::shared_ptr<ArchiveReader> reader =
