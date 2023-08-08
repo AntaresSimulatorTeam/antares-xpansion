589316476d1ef46aea32da86bf7ea9ba690454c6
Abdoulbari ZAKIR <azakir@silicom.fr>
2023-08-08 14:44:06 +0200

    update

--- src/cpp/xpansion_interfaces/ILogger.h
+++ src/cpp/xpansion_interfaces/ILogger.h
@@ -63,22 +63,6 @@ struct LogData {
   double master_time;
   double subproblem_time;
   int cumulative_number_of_subproblem_resolved;
-  bool operator==(const LogData &lhs) const {
-    return lb == lhs.lb && best_ub == lhs.best_ub && ub == lhs.ub &&
-           it == lhs.it && best_it == lhs.best_it &&
-           subproblem_cost == lhs.subproblem_cost &&
-           invest_cost == lhs.invest_cost && x_in == lhs.x_in &&
-           x_out == lhs.x_out && x_cut == lhs.x_cut &&
-           min_invest == lhs.min_invest && max_invest == lhs.max_invest &&
-           optimality_gap == lhs.optimality_gap &&
-           relative_gap == lhs.relative_gap &&
-           max_iterations == lhs.max_iterations &&
-           benders_elapsed_time == lhs.benders_elapsed_time &&
-           master_time == lhs.master_time &&
-           subproblem_time == lhs.subproblem_time &&
-           cumulative_number_of_subproblem_resolved ==
-               lhs.cumulative_number_of_subproblem_resolved;
-  }
 };
 class ILogger {
  public:
