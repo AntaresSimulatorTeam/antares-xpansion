be8a786a125cb6013976ca3e3ed572c755ecc0db
Abdoulbari ZAKIR <azakir@silicom.fr>
2023-08-11 15:22:43 +0200

    fix syntaxe

--- tests/end_to_end/benders/test_bendersSequentialEndToEnd.py
+++ tests/end_to_end/benders/test_bendersSequentialEndToEnd.py
@@ -6,6 +6,6 @@ from test_bendersEndToEnd import run_solver
 
 @pytest.mark.optim
 @pytest.mark.benderssequential
-def test_001_sequential(install_dir tmp_path, xpress):
+def test_001_sequential(install_dir, tmp_path, xpress):
     run_solver(install_dir, 'BENDERS', tmp_path,
                False, xpress)
