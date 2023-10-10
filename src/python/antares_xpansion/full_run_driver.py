10f4c4bc009070454a6cd95b57229aa7bc7c4ee3
Jason Marechal <jason.marechal+github@gmail.com>
2023-10-10 15:40:30 +0200

    Revert "Fix missing deps"
    This reverts commit b7a453c767f3db6d7da196cd28ace9c1767b4229.

--- src/python/antares_xpansion/full_run_driver.py
+++ src/python/antares_xpansion/full_run_driver.py
@@ -1,13 +1,13 @@
-import os
+
+from typing import List
 import subprocess
 import sys
-from pathlib import Path
-from typing import List
-
 from antares_xpansion.benders_driver import BendersDriver
-from antares_xpansion.logger import step_logger
 from antares_xpansion.problem_generator_driver import ProblemGeneratorDriver
+from antares_xpansion.logger import step_logger
 from antares_xpansion.study_output_cleaner import StudyOutputCleaner
+import os
+from pathlib import Path
 
 
 class FullRunDriver:
@@ -66,7 +66,6 @@ class FullRunDriver:
 
         os.chdir(lp_path)
         self.logger.info(f"Current directory is now: {os.getcwd()}")
-        self.logger.info(f"Running {self.full_command()}")
         ret = subprocess.run(
             self.full_command(), shell=False, stdout=sys.stdout, stderr=sys.stderr,
             encoding='utf-8')
