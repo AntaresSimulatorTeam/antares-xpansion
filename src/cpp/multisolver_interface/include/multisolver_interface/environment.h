06ed31a28f29945fbc4d77de313e2a93d78a913f
abdoulbari zakir <32519851+a-zakir@users.noreply.github.com>
2023-11-10 14:58:02 +0100

    suggestion by @florian

--- src/cpp/multisolver_interface/include/multisolver_interface/environment.h
+++ src/cpp/multisolver_interface/include/multisolver_interface/environment.h
@@ -1,6 +1,6 @@
 
-// This this file is an adaptation for Antares-Xpansion needs
-//  see orginal at https://github.com/rte-france/or-tools
+// This this file is an adaptation of OR-Tools for Antares-Xpansion needs
+//  see orginal at https://github.com/google/or-tools
 //  Copyright 2010-2021 Google LLC
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
