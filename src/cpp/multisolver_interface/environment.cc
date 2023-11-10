0c9c969b6a44842fdd1f01d24e36faa0977151d7
abdoulbari zakir <32519851+a-zakir@users.noreply.github.com>
2023-11-10 14:53:10 +0100

    suggestion by @flomnes
    Co-authored-by: Florian Omnès <florian.omnes@rte-france.com>

--- src/cpp/multisolver_interface/environment.cc
+++ src/cpp/multisolver_interface/environment.cc
@@ -1,4 +1,4 @@
-// This this file is an adaptation for Antares-Xpansion needs
+// This this file is an adaptation of OR-Tools for Antares-Xpansion needs
 //  see orginal at https://github.com/rte-france/or-tools
 // Copyright 2010-2021 Google LLC
 // Licensed under the Apache License, Version 2.0 (the "License");
