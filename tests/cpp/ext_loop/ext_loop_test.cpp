de3a648aad2411c7b7dda0bdb403459c800d83a1
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-02-26 15:52:34 +0100

    ok

--- tests/cpp/ext_loop/ext_loop_test.cpp
+++ tests/cpp/ext_loop/ext_loop_test.cpp
@@ -12,6 +12,8 @@ TEST_F(OuterLoopCriterionTest, IsCriterionHigh) {
   PlainData::Variables variables = {
       {"PositiveUnsuppliedEnergy::1", "PositiveUnsuppliedEnergy::2", "var3"},
       {0.2, 0.3, 68}};
+  double criterion_value = 2.0;  // two vars named PositiveUnsuppliedEnergy with
+                                 // value > max_unsup_energy
 
   PlainData::SubProblemData subProblemData;
   subProblemData.variables = variables;
@@ -25,6 +27,7 @@ TEST_F(OuterLoopCriterionTest, IsCriterionHigh) {
 
   EXPECT_EQ(criterion.IsCriterionSatisfied(worker_master_data),
             CRITERION::HIGH);
+  EXPECT_EQ(criterion.CriterionValue(), criterion_value);
 }
 
 TEST_F(OuterLoopCriterionTest, IsCriterionLow) {
@@ -35,6 +38,8 @@ TEST_F(OuterLoopCriterionTest, IsCriterionLow) {
   PlainData::Variables variables = {
       {"PositiveUnsuppliedEnergy::1", "PositiveUnsuppliedEnergy::2", "var3"},
       {0.2, 0.3, 68}};
+  double criterion_value = 2.0;  // two vars named PositiveUnsuppliedEnergy with
+                                 // value > max_unsup_energy
 
   PlainData::SubProblemData subProblemData;
   subProblemData.variables = variables;
@@ -47,4 +52,31 @@ TEST_F(OuterLoopCriterionTest, IsCriterionLow) {
   OuterloopCriterionLossOfLoad criterion(options);
 
   EXPECT_EQ(criterion.IsCriterionSatisfied(worker_master_data), CRITERION::LOW);
+  EXPECT_EQ(criterion.CriterionValue(), criterion_value);
+}
+
+TEST_F(OuterLoopCriterionTest, IsMet) {
+  double threshold = 2.0;
+  double epsilon = 1e-1;
+  double max_unsup_energy = 0.1;
+  const ExternalLoopOptions options = {threshold, epsilon, max_unsup_energy};
+  PlainData::Variables variables = {
+      {"PositiveUnsuppliedEnergy::1", "PositiveUnsuppliedEnergy::2", "var3"},
+      {0.2, 0.3, 68}};
+  double criterion_value = 2.0;  // two vars named PositiveUnsuppliedEnergy with
+                                 // value > max_unsup_energy
+
+  PlainData::SubProblemData subProblemData;
+  subProblemData.variables = variables;
+  SubProblemDataMap cut_trace = {
+      std::make_pair(std::string("P1"), subProblemData)};
+
+  WorkerMasterData worker_master_data;
+  worker_master_data._cut_trace = cut_trace;
+
+  OuterloopCriterionLossOfLoad criterion(options);
+
+  EXPECT_EQ(criterion.IsCriterionSatisfied(worker_master_data),
+            CRITERION::IS_MET);
+  EXPECT_EQ(criterion.CriterionValue(), criterion_value);
 }
