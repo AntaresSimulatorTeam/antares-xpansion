1cb0246717274a0c3e14224c57db9da3fc354047
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-03-29 01:47:11 +0100

    sorry

--- src/cpp/benders/external_loop/OuterLoopInputDataReader.cpp
+++ src/cpp/benders/external_loop/OuterLoopInputDataReader.cpp
@@ -1,9 +1,17 @@
 #include "OuterLoopInputDataReader.h"
 
+/**
+ * prefix could be := PositiveUnsuppliedEnergy:: or something else necessarily
+ * /!\ body could be := Brittany or nothing
+ */
 OuterLoopPattern::OuterLoopPattern(const std::string &prefix,
                                    const std::string &body)
     : prefix_(prefix), body_(body) {}
 
+/**
+ * just do
+ * just cat ;)
+ */
 std::regex OuterLoopPattern::MakeRegex() const {
   auto pattern = "(^" + prefix_ + ").*" + body_;
   return std::regex(pattern);
