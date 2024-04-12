c33f5f8134958cd1649026431aa7073b0e67c7bc
abdoulbari zaher <32519851+a-zakir@users.noreply.github.com>
2024-04-12 12:25:39 +0200

    fix xpress detection spam (#771)
    In mpi parallel mode, xpress detection infos are shown multiple times (x
    nums of procs)
    this pr should fix this

--- src/cpp/lpnamer/helper/ProblemGenerationLogger.h
+++ src/cpp/lpnamer/helper/ProblemGenerationLogger.h
@@ -11,14 +11,19 @@
 #include <set>
 #include <string>
 
+#include "ILogger.h"
 #include "LogUtils.h"
 #include "LoggerUtils.h"
+
 namespace ProblemGenerationLog {
 
-class ProblemGenerationILogger {
+class ProblemGenerationILogger : public ILoggerXpansion {
  public:
-  virtual ~ProblemGenerationILogger() = default;
-  virtual void DisplayMessage(const std::string& message) = 0;
+  ~ProblemGenerationILogger() override = default;
+  void display_message(const std::string& message) override = 0;
+  void PrintIterationSeparatorBegin() override = 0;
+  void PrintIterationSeparatorEnd() override = 0;
+
   virtual std::ostream& GetOstreamObject() = 0;
   LogUtils::LOGGERTYPE Type() const { return type_; }
 
@@ -39,8 +44,10 @@ class ProblemGenerationFileLogger : public ProblemGenerationILogger {
   ~ProblemGenerationFileLogger() override { logFile_.close(); }
   explicit ProblemGenerationFileLogger(
       const std::filesystem::path& logFilePath);
-  void DisplayMessage(const std::string& message) override;
-  std::ostream& GetOstreamObject() override { return logFile_; }
+  void display_message(const std::string& message) override;
+  void PrintIterationSeparatorBegin() override;
+  void PrintIterationSeparatorEnd() override;
+  std::ostream& GetOstreamObject() override;
 };
 
 class ProblemGenerationOstreamLogger : public ProblemGenerationILogger {
@@ -50,15 +57,17 @@ class ProblemGenerationOstreamLogger : public ProblemGenerationILogger {
  public:
   ~ProblemGenerationOstreamLogger() override = default;
   explicit ProblemGenerationOstreamLogger(std::ostream& stream);
-  void DisplayMessage(const std::string& message) override;
-  std::ostream& GetOstreamObject() override { return stream_; }
+  void display_message(const std::string& message) override;
+  void PrintIterationSeparatorBegin() override;
+  void PrintIterationSeparatorEnd() override;
+  std::ostream& GetOstreamObject() override;
 };
 
 class ProblemGenerationLogger;
 using ProblemGenerationLoggerSharedPointer =
     std::shared_ptr<ProblemGenerationLogger>;
 
-class ProblemGenerationLogger {
+class ProblemGenerationLogger : public ILoggerXpansion {
  private:
   LogUtils::LOGLEVEL log_level_;
   std::string context_ = "Unknown Context";
@@ -69,9 +78,11 @@ class ProblemGenerationLogger {
   ~ProblemGenerationLogger() = default;
 
   void AddLogger(const ProblemGenerationILoggerSharedPointer& logger);
-  void DisplayMessage(const std::string& message) const;
-  void DisplayMessage(const std::string& message,
-                      const LogUtils::LOGLEVEL log_level) const;
+  void display_message(const std::string& message);
+  void display_message(const std::string& message,
+                       const LogUtils::LOGLEVEL log_level);
+  void PrintIterationSeparatorBegin() override;
+  void PrintIterationSeparatorEnd() override;
   void setLogLevel(const LogUtils::LOGLEVEL log_level);
   void setContext(const std::string& context) { context_ = context; }
 
