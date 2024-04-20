f23cd94283f43c52c0ffdedb4ac4c9b77580281c
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-04-21 01:10:09 +0200

    fix fullrun

--- tests/python/test_full_run_driver.py
+++ tests/python/test_full_run_driver.py
@@ -41,12 +41,14 @@ class TestFullRunDriver:
             SolversExe("benders.exe", "", ""), self.benders_driver_options_file)
         full_run_driver = FullRunDriver(self.full_run_exe,
                                         problem_generation, benders_driver)
-        full_run_driver.prepare_drivers(output_path, is_relaxed, "benders", json_file_path,
+        benders_method = "benders"
+        full_run_driver.prepare_drivers(output_path, is_relaxed, benders_method, json_file_path,
                                         benders_keep_mps=benders_keep_mps, benders_oversubscribe=benders_oversubscribe, benders_allow_run_as_root=benders_allow_run_as_root)
         xpansion_output_dir = output_path.parent / \
             (output_path.stem+"-Xpansion")
         expected_command = [self.full_run_exe, "--benders_options", self.benders_driver_options_file,
-                            "-s", str(json_file_path), "-a", str(output_path), "-f", "integer", "-e", self.pb_gen_data.additional_constraints]
+                            "-s", str(json_file_path), "-a", str(output_path), "-f", "integer", "-e",
+                            self.pb_gen_data.additional_constraints, "--solver", benders_method]
 
         command = full_run_driver.full_command()
         assert len(expected_command) == len(command)
