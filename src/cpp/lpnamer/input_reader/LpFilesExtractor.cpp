029c1d9dccf4ef80458011aa00a7ca32da1485ca
Jason Marechal <jason.marechal+github@gmail.com>
2024-05-16 09:49:21 +0200

    Better handling of mode with switch

--- src/cpp/lpnamer/input_reader/LpFilesExtractor.cpp
+++ src/cpp/lpnamer/input_reader/LpFilesExtractor.cpp
@@ -9,61 +9,81 @@ void LpFilesExtractor::ExtractFiles() const {
   std::vector<std::filesystem::path> vect_area_files;
   std::vector<std::filesystem::path> vect_interco_files;
 
-  // TODO: test unknown
-  if (mode_ == Mode::ARCHIVE) {
-    auto archive_reader = ArchiveReader(antares_archive_path_);
-    archive_reader.Open();
-    vect_area_files =
-        archive_reader.ExtractPattern("area*.txt", "", xpansion_output_dir_);
+  switch (mode_) {
+    case Mode::ARCHIVE: {
+      auto archive_reader = ArchiveReader(antares_archive_path_);
+      archive_reader.Open();
+      vect_area_files =
+          archive_reader.ExtractPattern("area*.txt", "", xpansion_output_dir_);
 
-    vect_interco_files =
-        archive_reader.ExtractPattern("interco*.txt", "", xpansion_output_dir_);
+      vect_interco_files = archive_reader.ExtractPattern("interco*.txt", "",
+                                                         xpansion_output_dir_);
 
-    archive_reader.ExtractPattern("ts-numbers*", "", xpansion_output_dir_);
-    archive_reader.Close();
-    archive_reader.Delete();
-  } else {
-    auto dit = std::filesystem::directory_iterator(simulation_dir_);
-    std::ranges::for_each(
-        dit, [&vect_area_files, &vect_interco_files](const auto& entry) {
-          if (entry.path().extension() == ".txt") {
-            if (entry.path().filename().string().starts_with("area")) {
-              vect_area_files.push_back(entry.path());
+      archive_reader.ExtractPattern("ts-numbers*", "", xpansion_output_dir_);
+      archive_reader.Close();
+      archive_reader.Delete();
+      break;
+    }
+    case Mode::ANTARES_API:
+      [[fallthrough]];
+    case Mode::FILE: {
+      auto dit = std::filesystem::directory_iterator(simulation_dir_);
+      std::ranges::for_each(
+          dit, [&vect_area_files, &vect_interco_files](const auto& entry) {
+            if (entry.path().extension() == ".txt") {
+              if (entry.path().filename().string().starts_with("area")) {
+                vect_area_files.push_back(entry.path());
+              }
+              if (entry.path().filename().string().starts_with("interco")) {
+                vect_interco_files.push_back(entry.path());
+              }
             }
-            if (entry.path().filename().string().starts_with("interco")) {
-              vect_interco_files.push_back(entry.path());
-            }
-          }
-        });
+          });
+    } break;
+    case Mode::UNKOWN:
+      throw LogUtils::XpansionError<std::runtime_error>(
+          "Mode is unknown", LOGLOCATION);
+    default:
+      throw LogUtils::XpansionError<std::runtime_error>(
+          "Mode is not supported:", LOGLOCATION);
   }
   if (auto num_areas_file = vect_area_files.size(); num_areas_file == 0) {
     std::ostringstream msg;
     auto log_location = LOGLOCATION;
     msg << "No area*.txt file found" << std::endl;
-    (*logger_)(LogUtils::LOGLEVEL::FATAL)
-        << log_location << msg.str();
+    (*logger_)(LogUtils::LOGLEVEL::FATAL) << log_location << msg.str();
     throw ErrorWithAreaFile(msg.str(), log_location);
   } else if (num_areas_file > 1) {
     std::ostringstream msg;
     auto log_location = LOGLOCATION;
     msg << "More than one area*.txt file found" << std::endl;
-    (*logger_)(LogUtils::LOGLEVEL::FATAL)
-        << log_location << msg.str();
+    (*logger_)(LogUtils::LOGLEVEL::FATAL) << log_location << msg.str();
     throw ErrorWithAreaFile(msg.str(), log_location);
   }
 
-  if (mode_ == Mode::ARCHIVE) {
-    std::filesystem::rename(vect_area_files[0],
-                            xpansion_output_dir_ / "area.txt");
-  } else {
-    try {
-      std::filesystem::copy(vect_area_files[0],
-                            xpansion_output_dir_ / "area.txt");
-    } catch (const std::filesystem::filesystem_error& e) {
-      auto log_location = LOGLOCATION;
-      (*logger_)(LogUtils::LOGLEVEL::FATAL) << log_location << e.what();
-      throw;
-    }
+  switch (mode_) {
+    case Mode::ANTARES_API:
+      [[fallthrough]];
+    case Mode::FILE:
+      try {
+        std::filesystem::copy(vect_area_files[0],
+                              xpansion_output_dir_ / "area.txt");
+      } catch (const std::filesystem::filesystem_error& e) {
+        auto log_location = LOGLOCATION;
+        (*logger_)(LogUtils::LOGLEVEL::FATAL) << log_location << e.what();
+        throw;
+      }
+      break;
+    case Mode::ARCHIVE:
+      std::filesystem::rename(vect_area_files[0],
+                              xpansion_output_dir_ / "area.txt");
+      break;
+    case Mode::UNKOWN:
+      throw LogUtils::XpansionError<std::runtime_error>(
+          "Mode is unknown", LOGLOCATION);
+    default:
+      throw LogUtils::XpansionError<std::runtime_error>(
+          "Mode is not supported:", LOGLOCATION);
   }
 
   if (auto num_intercos_file = vect_interco_files.size();
@@ -71,28 +91,37 @@ void LpFilesExtractor::ExtractFiles() const {
     std::ostringstream msg;
     msg << "No interco*.txt file found" << std::endl;
     auto log_location = LOGLOCATION;
-    (*logger_)(LogUtils::LOGLEVEL::FATAL)
-        << log_location << msg.str();
+    (*logger_)(LogUtils::LOGLEVEL::FATAL) << log_location << msg.str();
     throw ErrorWithIntercosFile(msg.str(), log_location);
   } else if (num_intercos_file > 1) {
     std::ostringstream msg;
     auto log_location = LOGLOCATION;
     msg << "More than one interco*.txt file found" << std::endl;
-    (*logger_)(LogUtils::LOGLEVEL::FATAL)
-        << log_location << msg.str();
+    (*logger_)(LogUtils::LOGLEVEL::FATAL) << log_location << msg.str();
     throw ErrorWithIntercosFile(msg.str(), log_location);
   }
-  if (mode_ == Mode::ARCHIVE) {
-    std::filesystem::rename(vect_interco_files[0],
-                            xpansion_output_dir_ / "interco.txt");
-  } else {
-    try {
-      std::filesystem::copy(vect_interco_files[0],
-                            xpansion_output_dir_ / "interco.txt");
-    } catch (const std::filesystem::filesystem_error& e) {
-      auto log_location = LOGLOCATION;
-      (*logger_)(LogUtils::LOGLEVEL::FATAL) << log_location << e.what();
-      throw;
-    }
+  switch (mode_) {
+    case Mode::ANTARES_API:
+      [[fallthrough]];
+    case Mode::FILE:
+      try {
+        std::filesystem::copy(vect_interco_files[0],
+                              xpansion_output_dir_ / "interco.txt");
+      } catch (const std::filesystem::filesystem_error& e) {
+        auto log_location = LOGLOCATION;
+        (*logger_)(LogUtils::LOGLEVEL::FATAL) << log_location << e.what();
+        throw;
+      }
+      break;
+    case Mode::ARCHIVE:
+      std::filesystem::rename(vect_interco_files[0],
+                              xpansion_output_dir_ / "interco.txt");
+      break;
+    case Mode::UNKOWN:
+      throw LogUtils::XpansionError<std::runtime_error>(
+          "Mode is unknown", LOGLOCATION);
+    default:
+      throw LogUtils::XpansionError<std::runtime_error>(
+          "Mode is not supported:", LOGLOCATION);
   }
 }
