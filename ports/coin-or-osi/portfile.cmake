1584d7fba010219491d8b517b586b062505d7879
Jason Marechal <jason.marechal+github@gmail.com>
2024-06-13 11:16:07 +0200

    Try fix win

--- ports/coin-or-osi/portfile.cmake
+++ ports/coin-or-osi/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
         SHA512 fb173c04e3920c863e4d29e202cbaf182ee21332dc05ce293ffc39ead1b124f86f9682cc12032d216a712ea9aa3d0c98176bcbcd1b217e93afc6ce5a1ced68ed
         HEAD_REF master
 )
-if(MSVC)
+if(MSVC OR WIN32)
     message(WARNING "BUILDING FOR MSVC WITH SHARED LIBS")
     vcpkg_cmake_configure(
             SOURCE_PATH "${SOURCE_PATH}"
