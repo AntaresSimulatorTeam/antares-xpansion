5f88a127ec35665a82e64d52f4164965e0cf789b
Jason Marechal <jason.marechal+github@gmail.com>
2024-06-24 14:51:18 +0200

    Cleanup

--- src/cpp/lpnamer/problem_modifier/XpansionProblemsFromAntaresProvider.cpp
+++ src/cpp/lpnamer/problem_modifier/XpansionProblemsFromAntaresProvider.cpp
@@ -20,12 +20,12 @@ XpansionProblemsFromAntaresProvider::provideProblems(
 {
   std::vector<std::shared_ptr<Problem>> xpansion_problems;
   xpansion_problems.reserve(
-      XpansionProblemsFromAntaresProvider::antares_hebdo_problems.weekCount());
+      antares_hebdo_problems.weekCount());
   for (const auto& [problem_id, hebdo_data] :
-       XpansionProblemsFromAntaresProvider::antares_hebdo_problems.weeklyProblems) {
+       antares_hebdo_problems.weeklyProblems) {
     xpansion_problems.push_back(
         AntaresProblemToXpansionProblemTranslator::translateToXpansionProblem(
-            XpansionProblemsFromAntaresProvider::antares_hebdo_problems,
+            antares_hebdo_problems,
             problem_id.year, problem_id.week, solver_name, solver_log_manager));
   }
   return xpansion_problems;
