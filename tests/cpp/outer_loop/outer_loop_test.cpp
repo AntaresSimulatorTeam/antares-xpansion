1f2fb67665393ac1476d7ca2ddb63eebe38a4d18
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-07-16 13:58:31 +0200

    update test

--- tests/cpp/outer_loop/outer_loop_test.cpp
+++ tests/cpp/outer_loop/outer_loop_test.cpp
@@ -33,7 +33,7 @@ const auto STUDY_PATH =
     std::filesystem::path("data_test") / "external_loop_test";
 const auto LP_DIR = STUDY_PATH / "lp";
 const auto OPTIONS_FILE = LP_DIR / "options.json";
-const auto OUTER_OPTIONS_FILE = LP_DIR / "adequacy_criterion.yml";
+const auto OUTER_OPTIONS_FILE = "adequacy_criterion.yml";
 
 class MasterUpdateBaseTest : public ::testing::TestWithParam<std::string> {
  public:
