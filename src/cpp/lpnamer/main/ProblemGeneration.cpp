3277f6a507f689abe881409f6bccfcab50d7d192
Jason Marechal <jason.marechal+github@gmail.com>
2024-08-23 10:08:43 +0200

    Add disk info

--- src/cpp/lpnamer/main/ProblemGeneration.cpp
+++ src/cpp/lpnamer/main/ProblemGeneration.cpp
@@ -85,6 +85,43 @@ void serialize(Archive& ar, Antares::Solver::LpsFromAntares& data, const unsigne
 }  // namespace boost::serialization
 
 namespace {
+std::uintmax_t disk_usage_percent(const std::filesystem::space_info& si,
+                                  bool is_privileged = false) noexcept
+{
+  if (constexpr std::uintmax_t X(-1);
+      si.capacity == 0 || si.free == 0 || si.available == 0 ||
+      si.capacity == X || si.free == X || si.available == X
+  )
+    return 100;
+
+  std::uintmax_t unused_space = si.free, capacity = si.capacity;
+  if (!is_privileged)
+  {
+    const std::uintmax_t privileged_only_space = si.free - si.available;
+    unused_space -= privileged_only_space;
+    capacity -= privileged_only_space;
+  }
+  const std::uintmax_t used_space{capacity - unused_space};
+  return 100 * used_space / capacity;
+}
+
+void print_disk_space_info(auto const& dirs, int width = 14)
+{
+  (std::cout << std::left).imbue(std::locale("en_US.UTF-8"));
+
+  for (const auto s : {"Capacity", "Free", "Available", "Use%", "Dir"})
+    std::cout << "│ " << std::setw(width) << s << ' ';
+
+  for (std::cout << '\n'; auto const& dir : dirs)
+  {
+    std::error_code ec;
+    const std::filesystem::space_info si = std::filesystem::space(dir, ec);
+    for (auto x : {si.capacity, si.free, si.available, disk_usage_percent(si)})
+      std::cout << "│ " << std::setw(width) << static_cast<std::intmax_t>(x / 1024.f /1024.f) << ' ';
+    std::cout << "│ " << dir << '\n';
+  }
+}
+
 struct self_mem {
   long double vm_usage = 0.0;
   long resident_set = 0.0;
@@ -110,8 +147,13 @@ self_mem process_mem_usage()
 void memory() {
   auto [dispo, total] = Memory::MemoryUsageGo();
   auto [vm, rss] = process_mem_usage();
-  std::cout << "Memory usage: " << dispo << "/" << total << std::endl;
-  std::cout << "VM: " << vm << "Mb; RSS: " << rss << std::endl;
+  std::cout << "====================================\n";
+  std::cout << "Memory usage: " << dispo << "/" << total << "\n";
+  std::cout << "VM: " << vm << "Mb; RSS: " << rss << "\n";
+  std::cout << "------------------------------------\n";
+  const auto dirs = {"/scratch", "/tmp"};
+  print_disk_space_info(dirs);
+  std::cout << "====================================" << std::endl;
 }
 }
 
@@ -159,14 +201,12 @@ std::filesystem::path ProblemGeneration::performAntaresSimulation() {
 //  return "/home/marechaljas/Téléchargements/study_1_integer/output/20240715-1416eco";
 //#endif
 {
-  auto [dispo, total] = Memory::MemoryUsageGo();
   std::cout << "Memory usage before simulation:\n ";
   memory();
 }
   auto results = Antares::API::PerformSimulation(options_.StudyPath());
 
   {
-    auto [dispo, total] = Memory::MemoryUsageGo();
     std::cout << "Memory usage after simulation:\n";
     memory();
   }  //  //Add parallel
