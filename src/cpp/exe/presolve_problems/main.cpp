e316eae1dbff659eea3f7b19d1f718e7e4a34777
Thomas Bittar <thomas.bittar@rte-france.com>
2024-08-29 17:51:49 +0200

    Remove useless files

--- src/cpp/exe/presolve_problems/main.cpp
+++ src/cpp/exe/presolve_problems/main.cpp
@@ -4,6 +4,7 @@
 #include <common.h>
 
 #include <filesystem>
+#include <typeinfo>
 #include <unordered_set>
 
 #include "multisolver_interface/../../SolverXpress.h"
@@ -36,58 +37,92 @@ int main(int argc, char **argv) {
                                 XPRS_OUTPUTLOG_FULL_OUTPUT);
 
   // Read full problem MPS
-  std::filesystem::path fullMpsPath(argv[1]);
-  LoadXpress::XPRSreadprob(xprsProb, fullMpsPath.c_str(), "");
+  std::filesystem::path lpDir(argv[1]);
 
   // Parse structure and get candidates id
-  std::filesystem::path structureFilePath = "structure.txt";
-  CouplingMap pbAndVarNameToVarId = build_input(structureFilePath);
-  std::vector<int> candidatesId;
-  for (const auto &[var_name, id] :
-       pbAndVarNameToVarId[fullMpsPath.replace_extension()]) {
-    candidatesId.emplace_back(id);
+  std::filesystem::path structureFilePath = lpDir / "structure.txt";
+  CouplingMap couplings = build_input(structureFilePath);
+  std::map<std::string, std::vector<int>> pbNameTocandidatesId;
+  for (const auto &[pbName, varNameAndCandidateId] : couplings) {
+    for (const auto &[varName, id] : varNameAndCandidateId) {
+      pbNameTocandidatesId[pbName].emplace_back(id);
+    }
   }
+  // Rename structure, so that structure with presolved pbs can be named
+  // structure.txt and benders can run without modification
+  std::filesystem::rename(structureFilePath,
+                          structureFilePath.parent_path() /
+                              (structureFilePath.stem().string() + "-full" +
+                               structureFilePath.extension().string()));
 
+  CouplingMap presolvedCouplings;
+  std::string presolvedPrefix = "presolved-";
   // Configure presolve and solve with 0 iteration
-  LoadXpress::XPRSloadsecurevecs(xprsProb, 0, candidatesId.size(), nullptr,
-                                 candidatesId.data());
-  LoadXpress::XPRSsetintcontrol(xprsProb, XPRS_LPITERLIMIT, 0);
-  LoadXpress::XPRSlpoptimize(xprsProb, "");
+  for (const auto &[pbName, candidates] : pbNameTocandidatesId) {
+    if (pbName == "master") {
+      // Copy couplings for master
+      for (auto &[varName, id] : couplings[pbName]) {
+        presolvedCouplings[pbName][varName] = id;
+      }
+    } else {
+      // Do the actual presolve work for subproblems
+      std::filesystem::path fullMpsPath = lpDir / pbName;
+      LoadXpress::XPRSreadprob(xprsProb, fullMpsPath.c_str(), "");
+      LoadXpress::XPRSloadsecurevecs(xprsProb, 0, candidates.size(), nullptr,
+                                     candidates.data());
+      LoadXpress::XPRSsetintcontrol(xprsProb, XPRS_LPITERLIMIT, 0);
+      LoadXpress::XPRSlpoptimize(xprsProb, "");
 
-  // Get candidates id in presolved problem
-  int nbCols(0);
-  int nbRows(0);
-  LoadXpress::XPRSgetintattrib(xprsProb, XPRS_COLS, &nbCols);
-  LoadXpress::XPRSgetintattrib(xprsProb, XPRS_ROWS, &nbRows);
-  std::vector<int> colMap(nbCols);
-  std::vector<int> rowMap(nbRows);
-  LoadXpress::XPRSgetpresolvemap(xprsProb, rowMap.data(), colMap.data());
+      // Write presolved problem MPS
+      std::filesystem::path presolvedFilename =
+          presolvedPrefix + fullMpsPath.filename().string();
+      std::filesystem::path presolvedPath =
+          fullMpsPath.parent_path() / presolvedFilename;
+      LoadXpress::XPRSwriteprob(xprsProb, presolvedPath.c_str(), "");
 
-  std::unordered_set<int> initCandidatesIdSet(candidatesId.begin(),
-                                              candidatesId.end());
+      // Get candidates id in presolved problem
+      int nbCols(0);
+      int nbRows(0);
+      LoadXpress::XPRSgetintattrib(xprsProb, XPRS_COLS, &nbCols);
+      LoadXpress::XPRSgetintattrib(xprsProb, XPRS_ROWS, &nbRows);
+      std::vector<int> colMap(nbCols);
+      std::vector<int> rowMap(nbRows);
+      LoadXpress::XPRSgetpresolvemap(xprsProb, rowMap.data(), colMap.data());
 
-  // Use an unordered_map to store the indices of found values
-  std::unordered_map<int, int> initIdToPresolvedId;
+      std::unordered_set<int> initCandidatesIdSet(
+          pbNameTocandidatesId[pbName].begin(),
+          pbNameTocandidatesId[pbName].end());
+      // Use an unordered_map to store the indices of found values
+      std::unordered_map<int, int> initIdToPresolvedId;
 
-  // Iterate through the vector
-  for (int i = 0; i < colMap.size(); ++i) {
-    if (initCandidatesIdSet.find(colMap[i]) != initCandidatesIdSet.end()) {
-      
-      initIdToPresolvedId[colMap[i]] = i;
-      initCandidatesIdSet.erase(colMap[i]);
-      if (initCandidatesIdSet.empty()) {
-        break;
+      for (int i = 0; i < colMap.size(); ++i) {
+        if (initCandidatesIdSet.find(colMap[i]) != initCandidatesIdSet.end()) {
+          initIdToPresolvedId[colMap[i]] = i;
+          initCandidatesIdSet.erase(colMap[i]);
+          if (initCandidatesIdSet.empty()) {
+            break;
+          }
+        }
+      }
+      for (auto &[varName, id] : couplings[pbName]) {
+        presolvedCouplings[presolvedFilename.string()][varName] =
+            initIdToPresolvedId[id];
       }
     }
   }
 
-  // Write presolved problem MPS
-  std::string presolvedPrefix = "presolved-";
-  std::filesystem::path presolvedFilename =
-      presolvedPrefix + fullMpsPath.filename().string();
-  std::filesystem::path presolvedPath =
-      fullMpsPath.parent_path() / presolvedFilename;
-  LoadXpress::XPRSwriteprob(xprsProb, presolvedPath.c_str(), "");
+  // Write structure for presolved problem
+  std::ofstream coupling_file(structureFilePath);
+
+  for (auto const &[pbName, candidatesNameAndColId] : presolvedCouplings) {
+    for (auto const &[candidateName, presolvedColId] : candidatesNameAndColId) {
+      coupling_file << std::setw(50) << pbName;
+      coupling_file << std::setw(50) << candidateName;
+      coupling_file << std::setw(10) << presolvedColId;
+      coupling_file << std::endl;
+    }
+  }
+  coupling_file.close();
 
   return 0;
 }
