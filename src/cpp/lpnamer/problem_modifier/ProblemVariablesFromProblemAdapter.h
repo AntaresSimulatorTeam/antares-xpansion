7dcfb8cc5679a0a4ef63acf0235b54b6850e9647
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-09-12 10:23:43 +0200

    fix warnings on ActiveLink

--- src/cpp/lpnamer/problem_modifier/ProblemVariablesFromProblemAdapter.h
+++ src/cpp/lpnamer/problem_modifier/ProblemVariablesFromProblemAdapter.h
@@ -7,7 +7,7 @@ class ProblemVariablesFromProblemAdapter
     : public IProblemVariablesProviderPort {
  public:
   ProblemVariablesFromProblemAdapter(
-      std::shared_ptr<Problem> problem, std::vector<struct ActiveLink> vector_1,
+      std::shared_ptr<Problem> problem, std::vector<ActiveLink> vector_1,
       std::shared_ptr<ProblemGenerationLog::ProblemGenerationLogger>
           shared_ptr_1);
   ProblemVariables Provide() override;
@@ -19,6 +19,6 @@ class ProblemVariablesFromProblemAdapter
       std::map<colId, ColumnsToChange>& p_indirect_cost_columns) const;
 
   std::shared_ptr<Problem> problem_;
-  const std::vector<struct ActiveLink> active_links_;
+  const std::vector<ActiveLink> active_links_;
   std::shared_ptr<ProblemGenerationLog::ProblemGenerationLogger> logger_;
 };
