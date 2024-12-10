c36b1354b05982972a3220c009c739cc7b91d880
Thomas Bittar <thomas.bittar@rte-france.com>
2024-12-10 18:27:31 +0100

    Not erasing sub pb mps file in merge

--- src/cpp/benders/merge_mps/MergeMPS.cpp
+++ src/cpp/benders/merge_mps/MergeMPS.cpp
@@ -33,9 +33,10 @@ void MergeMPS::launch() {
 
     if (kvp.first != _options.MASTER_NAME) {
       solver_l->read_prob_mps(problem_name);
-      std::filesystem::remove(problem_name);
+      // std::filesystem::remove(problem_name);
       int mps_ncols(solver_l->get_ncols());
 
+      // -------- Change sub pb objective to take weights into account -------
       DblVector o(mps_ncols);
       IntVector sequence(mps_ncols);
       for (int i(0); i < mps_ncols; ++i) {
@@ -47,6 +48,9 @@ void MergeMPS::launch() {
         c *= weigth;
       }
       solver_l->chg_obj(sequence, o);
+      // -------- End change sub pb objective to take weights into account -----
+
+      
     } else {
       solver_l->read_prob_mps(problem_name);
     }
