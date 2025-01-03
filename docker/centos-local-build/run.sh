1bbf405f7a236b82f8f486b0be06ff755cd048af
Jason Marechal <jason.marechal+github@gmail.com>
2025-01-03 10:39:23 +0100

    Update run.sh now accepting parameters instead of hard coded path

--- docker/centos-local-build/run.sh
+++ docker/centos-local-build/run.sh
@@ -1,6 +1,10 @@
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <sources> <caches>"
+    exit 1
+fi
 docker build -t xpansion/centos7 .
 docker run \
---mount type=bind,src=/home/marechaljas/CLionProjects/antares-xpansion,dst=/mnt/sources \
---mount type=bind,src=/home/marechaljas/CLionProjects/docker_caches,dst=/mnt/caches \
+--mount type=bind,src=$1,dst=/mnt/sources \
+--mount type=bind,src=$2,dst=/mnt/caches \
 xpansion/centos7 \
 /mnt/sources /mnt/caches
