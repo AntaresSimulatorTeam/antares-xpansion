89ed2690c8aaafc4fe1fdbd9043a40f559fbea45
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2025-01-13 11:16:32 +0100

    test

--- src/cpp/benders/benders_mpi/BendersMPI.cpp
+++ src/cpp/benders/benders_mpi/BendersMPI.cpp
@@ -158,9 +158,10 @@ void BendersMpi::GatherCuts(const SubProblemDataMap &subproblem_data_map,
 
   // only rank_0 receive non-emtpy gathered_subproblem_map
   master_build_cuts(gathered_subproblem_map);
-
-  ComputeSubproblemsContributionToCriteria(subproblem_data_map);
-  if (_world.rank() == rank_0) {
+  if (!criterion_computation_.IsEmpty()) {
+    ComputeSubproblemsContributionToCriteria(subproblem_data_map);
+  }
+  if (_world.rank() == rank_0 && !criterion_computation_.IsEmpty()) {
     criteria_vector_for_each_iteration_.push_back(
       _data.criteria_current_iteration_data.criteria);
     UpdateMaxCriterionArea();
