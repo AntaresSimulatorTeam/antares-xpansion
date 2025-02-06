4fc9c52ed95c07718d70b8adc2c31f2542a27ae4
Jason Marechal <jason.marechal+github@gmail.com>
2025-02-06 16:58:21 +0100

    Use vector of ref to map content instead of copying

--- src/cpp/lpnamer/problem_modifier/XpansionProblemsFromAntaresProvider.cpp
+++ src/cpp/lpnamer/problem_modifier/XpansionProblemsFromAntaresProvider.cpp
@@ -24,7 +24,7 @@ std::vector<std::shared_ptr<Problem>> XpansionProblemsFromAntaresProvider::provi
 {
     std::vector<std::shared_ptr<Problem>> xpansion_problems;
     xpansion_problems.reserve(antares_hebdo_problems.weekCount());
-    std::vector<std::pair<Antares::Solver::WeeklyProblemId, Antares::Solver::WeeklyDataFromAntares>> weekly_problems_vector;
+    std::vector<std::pair<const Antares::Solver::WeeklyProblemId&, const Antares::Solver::WeeklyDataFromAntares&>> weekly_problems_vector;
     weekly_problems_vector.reserve(antares_hebdo_problems.weeklyProblems.size());
     for (const auto& [problem_id, hebdo_data] : antares_hebdo_problems.weeklyProblems) {
         weekly_problems_vector.emplace_back(problem_id, hebdo_data);
