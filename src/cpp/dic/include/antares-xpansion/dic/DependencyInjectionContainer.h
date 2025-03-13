f3bd2a5919ec508da42c8c7426d96a4490ce0168
Jason Marechal <jason.marechal+github@gmail.com>
2025-03-13 14:38:52 +0100

    Simple DependencyInjectionContainer

--- src/cpp/dic/include/antares-xpansion/dic/DependencyInjectionContainer.h
+++ src/cpp/dic/include/antares-xpansion/dic/DependencyInjectionContainer.h
@@ -1,29 +1,44 @@
 #pragma once
 #include <string>
 #include <unordered_map>
-#include <any>
+#include <memory>
+#include <boost/any/unique_any.hpp>
 
 namespace Xpansion {
 class DependencyInjectionContainer {
   DependencyInjectionContainer() = default;
+public:
   DependencyInjectionContainer(DependencyInjectionContainer const &) = delete;
   void operator=(DependencyInjectionContainer const &) = delete;
   DependencyInjectionContainer(DependencyInjectionContainer &&) = delete;
   DependencyInjectionContainer &operator=(DependencyInjectionContainer &&) = delete;
-public:
+
 static auto &Instance() {
   static DependencyInjectionContainer instance;
   return instance;
 }
   template<class T>
-  void  Register(const std::string &key, const T &value) {
+  void  Register(const std::string &key, const T& value) {
     container[key] = value;
   }
-  template<class T>
-  auto get(const std::string &key) -> T {
-    return std::any_cast<T>(container[key]);
-  }
 
-  std::unordered_map<std::string, std::any> container;
+    template<class T>
+void  Register(const std::string &key, T&& value) {
+    container.insert( {key, std::forward<T>(value)} );
+}
+
+    template<class T, class... Args>
+        void Make(const std::string &key, Args &&... args)
+{
+    auto tmp = std::make_unique<T>(std::forward<Args>(args)...);
+        container.insert({key, std::move(tmp)});
+}
+
+    template<class T>
+  auto get(const std::string &key) -> T& {
+    return boost::any_cast<T&>(container[key]);
+}
+
+  std::unordered_map<std::string, boost::anys::unique_any> container;
 };
 } // Xpansion
--- tests/cpp/DIC/dependency_injection_container.cpp
+++ tests/cpp/DIC/dependency_injection_container.cpp
@@ -8,3 +8,19 @@ TEST(dic, registeration_type)
     DependencyInjectionContainer::Instance().Register<int>("int", 5);
     EXPECT_EQ(DependencyInjectionContainer::Instance().get<int>("int"), 5);
 }
+
+class Foo { public: virtual int foo(){return 42;} };
+class Bar: public Foo { public: int foo() override {return 43;} };
+
+TEST(dic, make_object)
+{
+    DependencyInjectionContainer::Instance().Make<Bar>("foo");
+    EXPECT_EQ(DependencyInjectionContainer::Instance().get<std::unique_ptr<Bar>>("foo")->foo(), 43);
+}
+
+TEST(dic, register_object)
+{
+    auto b = std::make_unique<Bar>();
+    DependencyInjectionContainer::Instance().Register("foo", std::move(b));
+    EXPECT_EQ(DependencyInjectionContainer::Instance().get<std::unique_ptr<Bar>>("foo")->foo(), 43);
+}
