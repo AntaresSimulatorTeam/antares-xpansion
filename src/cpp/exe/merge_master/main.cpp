d40d5e1f8237e22c31c3e28a7ddb0dfad24dce36
Nicolas Bessin <nicolasbessin49@gmail.com>
2025-04-02 11:33:59 +0200

    Copy paste of the merge_mps code to create a new merge_master_mps executable

---
+++ src/cpp/exe/merge_master/main.cpp
@@ -0,0 +1,44 @@
+// projet_benders.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include <filesystem>
+
+#include "antares-xpansion/benders/benders_core/SimulationOptions.h"
+#include "antares-xpansion/benders/benders_core/Worker.h"
+#include "antares-xpansion/benders/factories/WriterFactories.h"
+#include "antares-xpansion/benders/logger/User.h"
+#include "antares-xpansion/benders/merge_master_mps/MergeMasterMPS.h"
+#include "antares-xpansion/benders/output/JsonWriter.h"
+#include "antares-xpansion/helpers/solver_utils.h"
+
+//@suggest: create and move to standardlp.cpp
+// Initialize static member
+size_t StandardLp::appendCNT = 0;
+
+int main(int argc, char** argv)
+{
+    usage(argc);
+    SimulationOptions options(argv[1]);
+    options.print(std::cout);
+
+    Logger logger = std::make_shared<xpansion::logger::User>(std::cout);
+
+    logger->display_message("starting merge_mps");
+
+    std::shared_ptr<Output::OutputWriter> writer = build_json_writer(std::filesystem::path(
+                                                                       options.JSON_FILE),
+                                                                     false);
+    try
+    {
+        MergeMPS merge_mps(options.get_base_options(), logger, writer);
+        merge_mps.launch();
+    }
+    catch (std::exception& ex)
+    {
+        std::string error = "Exception raised and program stopped : " + std::string(ex.what());
+        logger->display_message(error);
+        exit(1);
+    }
+
+    return 0;
+}
