9ec927b7829f554fbd2c3cc17934fd92f72b858a
Jason Marechal <jason.marechal+github@gmail.com>
2025-04-11 17:05:05 +0200

    Fix warnings

--- src/cpp/benders/benders_by_batch/include/antares-xpansion/benders/benders_by_batch/BendersByBatch.h
+++ src/cpp/benders/benders_by_batch/include/antares-xpansion/benders/benders_by_batch/BendersByBatch.h
@@ -41,7 +41,6 @@ private:
     double Gap() const;
     size_t number_of_batch_;
     unsigned current_batch_id_;
-    int number_of_sub_problem_resolved_;
     double remaining_epsilon_;
     double cumulative_subproblems_timer_per_iter_;
     bool misprice_;
