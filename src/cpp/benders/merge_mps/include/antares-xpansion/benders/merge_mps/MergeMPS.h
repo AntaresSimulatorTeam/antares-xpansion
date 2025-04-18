33a99ba615d9d5ca45ae86da5c185b21bb076ee6
Ian Menezes <ian.menezes@rte-france.com>
2025-04-18 17:44:43 +0200

    Created MergePathwayMPS class and exec

--- src/cpp/benders/merge_mps/include/antares-xpansion/benders/merge_mps/MergeMPS.h
+++ src/cpp/benders/merge_mps/include/antares-xpansion/benders/merge_mps/MergeMPS.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <optional>
+
 #include "antares-xpansion/benders/benders_core/common.h"
 #include "antares-xpansion/benders/factories/WriterFactories.h"
 #include "antares-xpansion/helpers/solver_utils.h"
@@ -52,3 +54,51 @@ private:
 };
 
 using MergeMPS = MergeMasterSubproblemMPS;
+
+class MergeMasterMasterMPS: public AbstractMergeMPS
+{
+public:
+    struct PathwayConstraints
+    {
+        double min_investment;
+        double max_investment;
+
+        double min_decommissioning;
+        double max_decommissioning;
+    };
+
+    struct PathwayNode
+    {
+        PathwayNode(const std::string&& node, const Json::Value& data);
+
+        std::string name;
+        std::filesystem::path path{};
+
+        std::optional<std::string> parent{std::nullopt};
+        double weight{1.};
+
+        std::map<std::string, PathwayConstraints> variables{};
+    };
+
+    using PathwayTree = std::vector<PathwayNode>;
+
+    MergeMasterMasterMPS(MergeMPSOptions options,
+                         Logger logger,
+                         std::shared_ptr<Output::OutputWriter> writer,
+                         const std::filesystem::path& tree_filename);
+
+private:
+    [[nodiscard]] CouplingMap build_coupling_map() const override;
+    [[nodiscard]] double get_objective_weight(const int nb_subproblems,
+                                              const std::string& name) const override;
+    void add_coupling_constraints() override;
+
+    PathwayTree tree_;
+
+    // TODO All this part : find better option
+    // TODO find a way of static init
+    std::string add_suffix(const std::string& s) const;
+    const std::string delimiter_{"/"};
+};
+
+using MergePathwayMPS = MergeMasterMasterMPS;
---
+++ src/cpp/exe/merge_master/main.cpp
@@ -0,0 +1,38 @@
+#include <filesystem>
+
+#include "antares-xpansion/benders/benders_core/SimulationOptions.h"
+#include "antares-xpansion/benders/factories/WriterFactories.h"
+#include "antares-xpansion/benders/logger/User.h"
+#include "antares-xpansion/benders/merge_mps/MergeMPS.h"
+#include "antares-xpansion/benders/merge_mps/StandardLp.h"
+
+// Initialize static member
+size_t StandardLp::appendCNT = 0;
+
+int main(int argc, char** argv)
+{
+    usage(argc);
+    SimulationOptions options(argv[1]);
+    options.print(std::cout);
+
+    Logger logger = std::make_shared<xpansion::logger::User>(std::cout);
+
+    logger->display_message("starting merge_master_mps");
+
+    std::shared_ptr<Output::OutputWriter> writer = build_json_writer(std::filesystem::path(
+                                                                       options.JSON_FILE),
+                                                                     false);
+    try
+    {
+        MergePathwayMPS merge_mps(options.get_base_options(), logger, writer, argv[2]);
+        merge_mps.launch();
+    }
+    catch (std::exception& ex)
+    {
+        std::string error = "Exception raised and program stopped : " + std::string(ex.what());
+        logger->display_message(error);
+        exit(1);
+    }
+
+    return 0;
+}
