40d9628020ff71806293dfed8f8b22cfb9edb670
Jason Maréchal <45510813+JasonMarechal25@users.noreply.github.com>
2025-04-23 15:02:07 +0200

    Revert "Handle Simulator 9.2.1-rc2 (#1041)"
    This reverts commit 03fdd2610fd4d9e22140986d38e20e133a016995.

--- src/python/antares_xpansion/antares_driver.py
+++ src/python/antares_xpansion/antares_driver.py
@@ -29,8 +29,9 @@ class AntaresDriver:
         self.antares_n_cpu = 1  # default
         self.zip_option = "-z"
         self.logger = step_logger(__name__, __class__.__name__)
-        # TODO update antares version which comes with named problems
+        #TODO update antares version which comes with named problems
         self.FIRST_VERSION_WITH_NAMED_PROBLEMS = "8.7"
+
 
     def launch(self, antares_study_path, antares_n_cpu: int, use_xpress: bool = False) -> bool:
         self._set_antares_n_cpu(antares_n_cpu)
@@ -64,7 +65,6 @@ class AntaresDriver:
         self.logger.info("Launching antares")
 
         start_time = datetime.now()
-        self.logger.info(self._get_antares_cmd())
         returned_l = subprocess.run(self._get_antares_cmd(), shell=False,
                                     stdout=subprocess.DEVNULL,
                                     stderr=subprocess.DEVNULL)
@@ -105,16 +105,14 @@ class AntaresDriver:
             self.simulation_name = list_of_dirs[-1]
 
     def _get_antares_cmd(self):
-        cmd = [str(self.antares_exe_path), self.data_dir, self.ANTARES_N_CPU_OPTION, str(self.antares_n_cpu),
-               self.zip_option]
+        cmd = [str(self.antares_exe_path), self.data_dir, self.ANTARES_N_CPU_OPTION, str(self.antares_n_cpu), self.zip_option]
         if self.use_xpress:
-            cmd.extend(["--linear-solver=xpress"])
+            cmd.extend(["--solver=xpress"])
         else:
-            cmd.extend(["--linear-solver=sirius"])
+            cmd.extend(["--solver=sirius"])
         simulator_version = version.parse(__antares_simulator_version__)
         simulator_version_with_named_mps = version.parse(self.FIRST_VERSION_WITH_NAMED_PROBLEMS)
-        if (simulator_version.major > simulator_version_with_named_mps.major) or (
-                simulator_version.major >= simulator_version_with_named_mps.major and simulator_version.minor >= simulator_version_with_named_mps.minor):
+        if (simulator_version.major > simulator_version_with_named_mps.major) or (simulator_version.major >= simulator_version_with_named_mps.major and simulator_version.minor >= simulator_version_with_named_mps.minor):
             cmd.append("--named-mps-problems")
 
         return cmd
