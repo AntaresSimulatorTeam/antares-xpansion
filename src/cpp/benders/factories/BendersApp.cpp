e375b00a38698d408f57d2b410ed57677f9144cc
Jason Marechal <jason.marechal+github@gmail.com>
2025-05-05 10:16:01 +0200

    Fix outerloop

--- src/cpp/benders/factories/BendersApp.cpp
+++ src/cpp/benders/factories/BendersApp.cpp
@@ -173,6 +173,13 @@ int BendersApp::RunExternalLoop()
         benders_ = std::move(environment.benders);
         criterion_input_holder_ = environment.criterion_input_data;
         method_ = environment.method;
+        if (pworld_->rank() == 0)
+        {
+            if (!isCriterionListEmpty())
+            {
+                AddCriterionOutputs();
+            }
+        }
         double tau = 0.5;
         const auto& outer_loop_inputs = std::get<Benders::Criterion::OuterLoopCriterionInputData>(
           criterion_input_holder_);
