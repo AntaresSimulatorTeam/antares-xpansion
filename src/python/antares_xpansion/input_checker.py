98a75472582a585623cb834558911a5b6198a359
Nicolas Bessin <nicolasbessin49@gmail.com>
2025-06-24 09:55:42 +0200

    Changed the candidate input checker to allow a candidate to have explicitly given max investment of 0.

--- src/python/antares_xpansion/input_checker.py
+++ src/python/antares_xpansion/input_checker.py
@@ -253,28 +253,29 @@ def _check_candidate_name_and_link(ini_file):
 
 def _check_candidate_exclusive_attributes(ini_file):
     # check exclusion between max-investment and (max-units, unit-size) attributes
+    # This still allows the maximum investment to be zero for a given candidate (needed in the trajectory workflow)
     for each_section in ini_file.sections():
         max_invest = (
             ini_file.getfloat(each_section, "max-investment")
             if ini_file.has_option(each_section, "max-investment")
-            else 0
+            else None
         )
         unit_size = (
             ini_file.getfloat(each_section, "unit-size")
             if ini_file.has_option(each_section, "unit-size")
-            else 0
+            else None
         )
         max_units = (
             ini_file.getfloat(each_section, "max-units")
             if ini_file.has_option(each_section, "max-units")
-            else 0
+            else None
         )
-        if max_invest != 0:
-            if max_units != 0 or unit_size != 0:
+        if max_invest is not None:
+            if max_units is not None or unit_size is not None:
                 logger.error(
                     f"Illegal values in section {each_section}: cannot assign non-null values simultaneously to max-investment and (unit-size or max_units)")
                 raise MaxUnitsAndMaxInvestmentNonNullSimultaneously
-        elif max_units == 0 or unit_size == 0:
+        elif max_units is None or unit_size is None:
             logger.error(
                 f"Illegal values in section {each_section}: need to assign non-null values to max-investment or (unit-size and max_units)")
             raise MaxUnitsAndMaxInvestmentAreNullSimultaneously
