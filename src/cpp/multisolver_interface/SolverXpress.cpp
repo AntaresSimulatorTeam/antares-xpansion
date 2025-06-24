fa668908228d476131c32ab4e315f684b46e6bc0
Jason Marechal <jason.marechal+github@gmail.com>
2025-06-24 11:50:38 +0200

    Trim

--- src/cpp/multisolver_interface/SolverXpress.cpp
+++ src/cpp/multisolver_interface/SolverXpress.cpp
@@ -384,7 +384,7 @@ std::vector<std::string> SolverXpress::get_names(int type, int n_elements)
     zero_status_check(XPRSgetnames(_xprs, type, names_in_one_string.data(), 0, n_elements - 1),
                       "get " + TYPETONAME.at(type) + " names.",
                       LOGLOCATION);
-    names_in_one_string = StringManip::trim_in_place(names_in_one_string);
+    names_in_one_string = StringManip::trim(names_in_one_string);
     auto res = StringManip::split(names_in_one_string, '\0');
     return res;
 }
