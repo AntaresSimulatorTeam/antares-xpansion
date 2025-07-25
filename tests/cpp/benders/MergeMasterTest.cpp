1158e0dcea0dc0b5fdb757d2095b078da4e37114
Jason Maréchal <45510813+JasonMarechal25@users.noreply.github.com>
2025-07-25 10:08:49 +0200

    Add coverage for Merge MPS (#1067)

---
+++ tests/cpp/benders/MergeMasterTest.cpp
@@ -0,0 +1,45 @@
+#include "antares-xpansion/benders/merge_master_mps/MergeMasterMPS.h"
+#include "gtest/gtest.h"
+
+TEST(MergeMasterMPS, ParseVariableType)
+{
+    EXPECT_EQ(MergeMasterTrajectoryMPS::parse_variable_type("x"),
+              MergeMasterTrajectoryMPS::CAPACITY);
+    EXPECT_EQ(MergeMasterTrajectoryMPS::parse_variable_type("dx_plus"),
+              MergeMasterTrajectoryMPS::DX_PLUS);
+    EXPECT_EQ(MergeMasterTrajectoryMPS::parse_variable_type("dx_minus"),
+              MergeMasterTrajectoryMPS::DX_MINUS);
+}
+
+TEST(MergeMasterMPS, ParseConstraintType)
+{
+    EXPECT_EQ(MergeMasterTrajectoryMPS::parse_constraint_type("="), 'E');
+    EXPECT_EQ(MergeMasterTrajectoryMPS::parse_constraint_type(">"), 'G');
+    EXPECT_EQ(MergeMasterTrajectoryMPS::parse_constraint_type("<"), 'L');
+}
+
+TEST(MergeMasterMPS, VariablePositionsGetSet)
+{
+    MergeMasterTrajectoryMPS::VariablePositions positions;
+    positions.set(MergeMasterTrajectoryMPS::CAPACITY, 0);
+    positions.set(MergeMasterTrajectoryMPS::DX_PLUS, 1);
+    positions.set(MergeMasterTrajectoryMPS::DX_MINUS, 2);
+
+    EXPECT_EQ(positions.get(MergeMasterTrajectoryMPS::CAPACITY), 0);
+    EXPECT_EQ(positions.get(MergeMasterTrajectoryMPS::DX_PLUS), 1);
+    EXPECT_EQ(positions.get(MergeMasterTrajectoryMPS::DX_MINUS), 2);
+}
+
+TEST(MergeMasterMPS, CandidateCostsGet)
+{
+    Json::Value data;
+    data[MasterStructureKeys::KEY_OPERATION_COST] = 100.0;
+    data[MasterStructureKeys::KEY_INVESTMENT_COST] = 200.0;
+    data[MasterStructureKeys::KEY_RETIREMENT_COST] = 50.0;
+
+    MergeMasterTrajectoryMPS::CandidateCosts costs(data);
+
+    EXPECT_DOUBLE_EQ(costs.get(MergeMasterTrajectoryMPS::CAPACITY), 100.0);
+    EXPECT_DOUBLE_EQ(costs.get(MergeMasterTrajectoryMPS::DX_PLUS), 200.0);
+    EXPECT_DOUBLE_EQ(costs.get(MergeMasterTrajectoryMPS::DX_MINUS), 50.0);
+}
