62599fc2d4ee20cd44c72387925d73b3ffe2fcdc
h-fournier <hugo.fournier@scalian.com>
2025-08-07 10:42:56 +0200

    Remove wrong modification

--- src/cpp/multisolver_interface/COIN_common_functions.cpp
+++ src/cpp/multisolver_interface/COIN_common_functions.cpp
@@ -94,16 +94,9 @@ void fill_rhs_from_bounds(const double* rowLower,
         {
             if (rowUpper[i] < COIN_DBL_MAX)
             {
-                if (rowLower[i] == rowUpper[i])
-                {
-                    rhs[i - first] = rowLower[i];
-                }
-                else
-                {
-                    auto error = LOGLOCATION + "ERROR : Row " + std::to_string(i)
-                                 + " has two different RHS, both right and left.";
-                    throw GenericSolverException(error);
-                }
+                auto error = LOGLOCATION + "ERROR : Row " + std::to_string(i)
+                             + " has two different RHS, both right and left.";
+                throw GenericSolverException(error);
             }
             else
             {
