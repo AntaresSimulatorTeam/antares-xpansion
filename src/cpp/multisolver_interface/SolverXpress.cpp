cef63eb9cdd63b3790a80ac0d76edac5335d4575
Jason Marechal <jason.marechal+github@gmail.com>
2025-09-05 14:41:18 +0200

    Remove log

--- src/cpp/multisolver_interface/SolverXpress.cpp
+++ src/cpp/multisolver_interface/SolverXpress.cpp
@@ -148,7 +148,6 @@ void SolverXpress::init()
 
 void SolverXpress::free()
 {
-    std::cout << "Freeing XPRESS problem" << std::endl;
     int status = XPRSdestroyprob(_xprs);
     _xprs = NULL;
     if (status)
