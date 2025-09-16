9480231763f31eb6673e64b853b66dc164284068
Jason Marechal <jason.marechal+github@gmail.com>
2025-09-16 13:12:05 +0200

    Rename var

--- src/cpp/lpnamer/problem_modifier/AntaresProblemToXpansionProblemTranslator.cpp
+++ src/cpp/lpnamer/problem_modifier/AntaresProblemToXpansionProblemTranslator.cpp
@@ -6,6 +6,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <regex>
 
 #include "antares-xpansion/multisolver_interface/SolverFactory.h"
 #include "antares-xpansion/xpansion_interfaces/LogUtils.h"
@@ -34,6 +35,23 @@ std::shared_ptr<Problem> AntaresProblemToXpansionProblemTranslator::translateToX
     std::vector<int> tmp(constant.VariablesCount, 0);
     std::vector<char> coltypes(constant.VariablesCount, 'C');
 
+    // Renommer les variables pour chaque heure de la semaine
+    std::vector<std::string> renamed_variables;
+    renamed_variables.reserve(constant.VariablesMeaning.size());
+    for (size_t i = 0; i < constant.VariablesMeaning.size(); ++i)
+    {
+        renamed_variables.push_back(replace_hour_in_name(constant.VariablesMeaning[i], i % 168));
+    }
+
+    // Renommer les contraintes pour chaque heure de la semaine
+    std::vector<std::string> renamed_constraints;
+    renamed_constraints.reserve(constant.ConstraintsMeaning.size());
+    for (size_t i = 0; i < constant.ConstraintsMeaning.size(); ++i)
+    {
+        renamed_constraints.push_back(
+          replace_hour_in_name(constant.ConstraintsMeaning[i], i % 168));
+    }
+
     problem->add_cols(constant.VariablesCount,
                       0,
                       hebdo.LinearCost.data(),
@@ -42,7 +60,7 @@ std::shared_ptr<Problem> AntaresProblemToXpansionProblemTranslator::translateToX
                       {},
                       hebdo.Xmin.data(),
                       hebdo.Xmax.data(),
-                      constant.VariablesMeaning);
+                      renamed_variables);
 
     std::span signs(hebdo.Direction.data(), hebdo.Direction.size());
     problem->add_rows(constant.ConstraintesCount,
@@ -53,7 +71,7 @@ std::shared_ptr<Problem> AntaresProblemToXpansionProblemTranslator::translateToX
                       reinterpret_cast<const int*>(constant.Mdeb.data()),
                       reinterpret_cast<const int*>(constant.ColumnIndexes.data()),
                       constant.ConstraintsMatrixCoeff.data(),
-                      constant.ConstraintsMeaning);
+                      renamed_constraints);
     // On peut ajouter la partie qui renomme les variables ici si on stocke les
     // données du type de variables dans ConstantDataFromAntares, i.e. en
     // définissant une autre implémentation de IProblemVariablesProviderPort
@@ -89,3 +107,7 @@ std::vector<char> AntaresProblemToXpansionProblemTranslator::convertSignToLEG(
                            });
     return LEG_vector;
 }
+
+// Fonction utilitaire pour remplacer hour<...> par hour<valeur>
+static std::string replace_hour_in_name(const std::string& name, int hour)
+{
