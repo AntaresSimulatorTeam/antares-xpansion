2a5fe34cbc1a640379cd4820e42083e61e28fb21
JasonMarechal25 <JasonMarechal25@users.noreply.github.com>
2025-10-06 09:49:26 +0000

    deploy: e6db180bf24776e44368a012415776784653123a

--- search/all_13.js
+++ search/all_13.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['h_20help_20tt_0',['&lt;tt&gt;-h, --help&lt;/tt&gt;',['../md_docs_2user-guide_2get-started_2launching-optimization.html#autotoc_md169',1,'']]],
-  ['handling_20different_20platforms_1',['Handling Different Platforms',['../md_docs_2developer-guide_2install__from__sources_26-Xpress.html#autotoc_md109',1,'']]],
-  ['have_20mpi_20related_20issues_20please_20use_20the_20other_20ones_2',['⚠️ Single-file versions in Ubuntu are known to have MPI-related issues. Please use the other ones.',['../md_docs_2changelog_2CHANGELOG.html#autotoc_md25',1,'']]],
+  ['h_20help_20tt_0',['&lt;tt&gt;-h, --help&lt;/tt&gt;',['../md_docs_2user-guide_2get-started_2launching-optimization.html#autotoc_md170',1,'']]],
+  ['handling_20different_20platforms_1',['Handling Different Platforms',['../md_docs_2developer-guide_2install__from__sources_26-Xpress.html#autotoc_md110',1,'']]],
+  ['have_20mpi_20related_20issues_20please_20use_20the_20other_20ones_2',['⚠️ Single-file versions in Ubuntu are known to have MPI-related issues. Please use the other ones.',['../md_docs_2changelog_2CHANGELOG.html#autotoc_md26',1,'']]],
   ['headersmanager_3',['HeadersManager',['../structHeadersManager.html',1,'']]],
   ['headersmanagerexternalloop_4',['HeadersManagerExternalLoop',['../structHeadersManagerExternalLoop.html',1,'']]],
-  ['help_20tt_5',['&lt;tt&gt;-h, --help&lt;/tt&gt;',['../md_docs_2user-guide_2get-started_2launching-optimization.html#autotoc_md169',1,'']]],
-  ['how_20the_20code_20loads_20xpress_6',['How the Code Loads Xpress',['../md_docs_2developer-guide_2install__from__sources_26-Xpress.html#autotoc_md106',1,'']]]
+  ['help_20tt_5',['&lt;tt&gt;-h, --help&lt;/tt&gt;',['../md_docs_2user-guide_2get-started_2launching-optimization.html#autotoc_md170',1,'']]],
+  ['how_20the_20code_20loads_20xpress_6',['How the Code Loads Xpress',['../md_docs_2developer-guide_2install__from__sources_26-Xpress.html#autotoc_md107',1,'']]]
 ];
