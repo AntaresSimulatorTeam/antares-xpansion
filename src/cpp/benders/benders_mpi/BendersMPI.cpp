db3c844319d1879dbecdd2b5089f7d0524c38c2a
Jason Marechal <jason.marechal+github@gmail.com>
2025-11-20 12:48:22 +0100

    fix: Missing Xpress loading log in benders

--- src/cpp/benders/benders_mpi/BendersMPI.cpp
+++ src/cpp/benders/benders_mpi/BendersMPI.cpp
@@ -10,7 +10,7 @@
 #include "antares-xpansion/helpers/Timer.h"
 
 BendersMpi::BendersMpi(const BendersBaseOptions& options,
-                       Logger logger,
+                       std::shared_ptr<ILogger> logger,
                        std::shared_ptr<Output::OutputWriter> writer,
                        mpi::environment& env,
                        mpi::communicator& world,
