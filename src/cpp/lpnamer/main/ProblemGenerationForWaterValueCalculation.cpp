3a18d96af8c1d6343a5a467cf0ef4fb62d64282d
EUROGICIEL\pierre-augustin.val <pierre-augustin.vallin@scalian.com>
2025-12-09 09:18:33 +0100

    fix: bad clone of problem

--- src/cpp/lpnamer/main/ProblemGenerationForWaterValueCalculation.cpp
+++ src/cpp/lpnamer/main/ProblemGenerationForWaterValueCalculation.cpp
@@ -181,8 +181,7 @@ ProblemGenerationForWaterValueCalculation::cleanProblemsForBellmanCalculations(
           {
               // copy of the problem needed if gridCollection contains multiple
               // gridDefinitions, and for multistock
-              std::shared_ptr<Problem> problem = std::make_shared<Problem>(
-                            *(pb.second->clone()));
+              std::shared_ptr<Problem> problem = std::make_shared<Problem>(*(pb.second->clone()));
               std::string pbName = "problem-" + std::to_string(pbId.year) + "-"
                                    + std::to_string(pbId.week) + "--optim-nb-1";
               logger->display_message("Modifying problem: " + pbName);
