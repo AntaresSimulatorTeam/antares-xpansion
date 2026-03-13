a505da1cb1229e0fbd4d0fe6de2d8e9bef95f0e1
Jason Marechal <jason.marechal@pm.me>
2026-03-13 17:21:38 +0100

    step 5

---
+++ src/cpp/benders/benders_core/BendersCore.cpp
@@ -0,0 +1,144 @@
+#include "antares-xpansion/benders/benders_core/BendersCore.h"
+
+#include <memory>
+
+#include "antares-xpansion/benders/benders_core/BendersProblemFromFile.h"
+#include "antares-xpansion/benders/benders_core/strategies/MPISubproblemSolver.h"
+#include "antares-xpansion/benders/benders_core/strategies/SingleLoopStrategy.h"
+#include "antares-xpansion/helpers/Timer.h"
+
+BendersCore::BendersCore(BendersBaseOptions options,
+                         Logger logger,
+                         std::shared_ptr<Output::OutputWriter> writer,
+                         std::shared_ptr<MathLoggerDriver> mathLoggerDriver):
+    BendersBase(std::move(options), std::move(logger), std::move(writer), mathLoggerDriver),
+    solver_strategy_(std::make_unique<MPISubproblemSolver>()),
+    loop_strategy_(std::make_unique<SingleLoopStrategy>())
+{
+}
+
+BendersCore::BendersCore(BendersBaseOptions options,
+                         Logger logger,
+                         std::shared_ptr<Output::OutputWriter> writer,
+                         std::shared_ptr<MathLoggerDriver> mathLoggerDriver,
+                         SubproblemSolverPtr solver_strategy,
+                         LoopStrategyPtr loop_strategy):
+    BendersBase(std::move(options), std::move(logger), std::move(writer), mathLoggerDriver),
+    solver_strategy_(std::move(solver_strategy)),
+    loop_strategy_(std::move(loop_strategy))
+{
+}
+
+void BendersCore::launch()
+{
+    _logger->display_message("Building input");
+    _logger->display_message("Constructing workers...");
+
+    InitializeProblems();
+    _logger->display_message("Running solver...");
+    try
+    {
+        Run();
+        _logger->display_message(BendersName() + " solver terminated.");
+    }
+    catch (const std::exception& ex)
+    {
+        std::string error = "Exception raised : " + std::string(ex.what());
+        _logger->display_message(error);
+    }
+
+    post_run_actions();
+    free();
+}
+
+std::string BendersCore::BendersName() const
+{
+    return "BendersCore";
+}
+
+void BendersCore::InitializeProblems()
+{
+    MatchProblemToId();
+    std::shared_ptr<IBendersProblemProvider>
+      benders_problem_provider = std::make_shared<BendersProblemFromFile>(get_master_path());
+    reset_master<WorkerMaster>(master_variable_map_,
+                               get_solver_name(),
+                               get_log_level(),
+                               _data.nsubproblem,
+                               solver_log_manager_,
+                               IsResumeMode(),
+                               _logger,
+                               Options().PROBLEMS_FORMAT,
+                               benders_problem_provider.get(),
+                               Options().MASTER_SOLUTION_TOLERANCE,
+                               Options().CUT_COEFFICIENT_TOLERANCE);
+    for (const auto& problem: coupling_map_)
+    {
+        const auto subProblemFilePath = GetSubproblemPath(problem.first);
+
+        AddSubproblem(problem);
+        AddSubproblemName(problem.first);
+    }
+}
+
+void BendersCore::Run()
+{
+    init_data();
+    ChecksResumeMode();
+    if (is_trace())
+    {
+        OpenCsvFile();
+    }
+
+    HandleInitialMasterRelaxation();
+
+    while (!_data.stop)
+    {
+        Timer timer_master;
+        ++_data.it;
+
+        if (SwitchToIntegerMaster(_data.is_in_initial_relaxation))
+        {
+            _logger->LogAtSwitchToInteger();
+            ActivateIntegrityConstraints();
+            ResetDataPostRelaxation();
+        }
+
+        _logger->log_at_initialization(_data.it + GetNumIterationsBeforeRestart());
+        _logger->display_message("\tSolving master...");
+        get_master_value();
+        _logger->log_master_solving_duration(_data.timer_master);
+
+        ComputeXCut();
+        _logger->log_iteration_candidates(bendersDataToLogData(_data));
+
+        _logger->display_message("\tSolving subproblems...");
+        solver_strategy_->solve_subproblems(*this);
+        _logger->LogSubproblemsSolvingWalltime(_data.subproblems_walltime);
+
+        compute_ub();
+        update_best_ub();
+
+        _logger->log_at_iteration_end(bendersDataToLogData(_data));
+
+        UpdateTrace();
+
+        _data.timer_master = timer_master.elapsed();
+        _data.iteration_time = -_data.benders_time;
+        _data.benders_time = GetBendersTime();
+        _data.iteration_time += _data.benders_time;
+        _data.stop = ShouldBendersStop();
+        SaveCurrentBendersData();
+    }
+    CloseCsvFile();
+    EndWritingInOutputFile();
+    write_basis();
+}
+
+void BendersCore::post_run_actions() const
+{
+    if (_data.best_it != 0)
+    {
+        SaveSolutionInOutputFile();
+    }
+}
---
+++ src/cpp/benders/benders_core/include/antares-xpansion/benders/benders_core/BendersCore.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "antares-xpansion/benders/benders_core/BendersBase.h"
+#include "antares-xpansion/benders/benders_core/strategies/ILoopStrategy.h"
+#include "antares-xpansion/benders/benders_core/strategies/ISubproblemSolver.h"
+
+class BendersCore: public BendersBase
+{
+public:
+    BendersCore(BendersBaseOptions options,
+                Logger logger,
+                std::shared_ptr<Output::OutputWriter> writer,
+                std::shared_ptr<MathLoggerDriver> mathLoggerDriver);
+
+    BendersCore(BendersBaseOptions options,
+                Logger logger,
+                std::shared_ptr<Output::OutputWriter> writer,
+                std::shared_ptr<MathLoggerDriver> mathLoggerDriver,
+                SubproblemSolverPtr solver_strategy,
+                LoopStrategyPtr loop_strategy);
+
+    void launch() override;
+    std::string BendersName() const override;
+    void InitializeProblems() override;
+
+protected:
+    void Run() override;
+    void post_run_actions() const override;
+
+private:
+    SubproblemSolverPtr solver_strategy_;
+    LoopStrategyPtr loop_strategy_;
+};
