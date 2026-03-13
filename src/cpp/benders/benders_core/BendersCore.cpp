8c76d67aef1ceec0d8f3e02f8163af5e2bf24958
Jason Marechal <jason.marechal@pm.me>
2026-03-13 21:26:38 +0100

    Fix test

--- src/cpp/benders/benders_core/BendersCore.cpp
+++ src/cpp/benders/benders_core/BendersCore.cpp
@@ -113,7 +113,7 @@ void BendersCore::Run()
         _logger->log_iteration_candidates(bendersDataToLogData(_data));
 
         _logger->display_message("\tSolving subproblems...");
-        solver_strategy_->solve_subproblems(*this);
+        BuildCut();
         _logger->LogSubproblemsSolvingWalltime(_data.subproblems_walltime);
 
         compute_ub();
