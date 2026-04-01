89944446c760f42a7465938d712bc25100ed52fc
Juliette-Gerbaux <juliette.gerbaux@rte-france.com>
2026-04-01 09:54:38 +0200

    Fix GetSubCutTolerance

--- src/cpp/benders/benders_core/BendersBase.cpp
+++ src/cpp/benders/benders_core/BendersBase.cpp
@@ -1497,11 +1497,11 @@ void BendersBase::SetPlugin(std::shared_ptr<BendersPlugin> benders_plugin)
 std::map<int, double> BendersBase::GetSubCutTolerance() const
 {
     std::map<int, double> subproblem_cut_coefficient_tolerance{};
-    for (const auto& subproblem_name: GetSubProblemNames())
+    for (const auto& subproblem: _problem_to_id)
     {
-        subproblem_cut_coefficient_tolerance[ProblemToId(
-          subproblem_name)] = Options().CUT_COEFFICIENT_TOLERANCE
-                              * SubproblemWeight(_data.nsubproblem, subproblem_name);
+        std::cout << subproblem.first << std::endl;
+        subproblem_cut_coefficient_tolerance[subproblem.second] = Options().CUT_COEFFICIENT_TOLERANCE
+                              * SubproblemWeight(_data.nsubproblem, subproblem.first);
     }
     return subproblem_cut_coefficient_tolerance;
 }
