8210e99b162aca84caeb3ad0e9b2c3beb539d90d
Juliette-Gerbaux <juliette.gerbaux@rte-france.com>
2026-04-01 10:28:22 +0200

    Fix computation of cut tolerance

--- src/cpp/benders/benders_core/WorkerMaster.cpp
+++ src/cpp/benders/benders_core/WorkerMaster.cpp
@@ -568,10 +568,18 @@ void WorkerMaster::setToZeroIfWithinTolerance(std::vector<double>& values,
     double cut_coefficient_tolerance = 0.0;
     for (int sb_id: subproblem_ids)
     {
-        cut_coefficient_tolerance += _subproblem_tolerance.find(sb_id)->second;
+        auto it = _subproblem_tolerance.find(sb_id);
+        if (it == _subproblem_tolerance.end()) {
+            throw std::runtime_error("sb_id absent de _subproblem_tolerance");
+        }
+        cut_coefficient_tolerance += it->second;
     }
 
     const double tolerance = cut_coefficient_tolerance;
+
+    if (tolerance>=1){
+        throw std::runtime_error("Cut tolerance greater than 1");
+    }
 
     std::transform(values.begin() + first,
                    values.begin() + last,
