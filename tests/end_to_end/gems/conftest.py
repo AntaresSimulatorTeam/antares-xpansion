64f607cbbc8617539ac267c3e5f0f1ee31beae71
payetvin <payetvin_externe@rte-france.com>
2026-05-05 16:21:44 +0200

    simplify conftest

--- tests/end_to_end/gems/conftest.py
+++ tests/end_to_end/gems/conftest.py
@@ -1,8 +1,4 @@
 import pytest
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent / "src" / "python"))
 
 from tests import build_config_reader
 
