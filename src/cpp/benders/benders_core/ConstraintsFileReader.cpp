f2452f29325982672b7e87dd4c66ff539f081a92
Hedi Bouchehda <h.bouchehdaa@gmail.com>
2026-06-15 15:01:04 +0200

    removing uncessary pointer checks

--- src/cpp/benders/benders_core/ConstraintsFileReader.cpp
+++ src/cpp/benders/benders_core/ConstraintsFileReader.cpp
@@ -13,26 +13,13 @@ ConstraintsFileReader::ConstraintsFileReader(const std::filesystem::path constra
                                            SOLVER_TYPE::CONTINUOUS,
                                            solver_log_manager);
 
-    if (solver_ != nullptr)
-    {
-        solver_->set_threads(1);
-        solver_->set_output_log_level(log_level);
-    }
-    else
-    {
-        std::cout << "solver is null " << std::endl;
-    }
-
+    solver_->set_threads(1);
+    solver_->set_output_log_level(log_level);
+ 
     benders_problem_provider_ = std::make_shared<BendersProblemFromFile>(constraint_file_path);
     solver_IO_.configure(solver_name, format);
-    if (benders_problem_provider_ != nullptr)
-    {
-        benders_problem_provider_->provide_problem(solver_IO_, solver_);
-    }
-    else
-    {
-        std::cout << "benders_problem_provider_ is null " << std::endl;
-    }
+    benders_problem_provider_->provide_problem(solver_IO_, solver_);
+
 }
 
 int ConstraintsFileReader::get_row_index(const std::string& name)
