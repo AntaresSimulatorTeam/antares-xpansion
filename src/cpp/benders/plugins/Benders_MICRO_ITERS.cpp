34766cfa87a0ebffc68391195d586c8c3f0354a2
ze-hedi <h.bouchehdaa@gmail.com>
2026-06-19 20:01:45 +0200

    Feature/benders micro iterations 3 (#1252)
    new interface for cpp plugin

--- src/cpp/benders/plugins/Benders_MICRO_ITERS.cpp
+++ src/cpp/benders/plugins/Benders_MICRO_ITERS.cpp
@@ -6,8 +6,10 @@
 
 #include <cassert>
 #include <chrono>
+#include <exception>
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
 #include <string_view>
 
 #include <boost/tokenizer.hpp>
@@ -216,7 +218,7 @@ void Benders_MICRO_ITERS::OnBendersStart(const SubproblemsMapPtr& subproblem_map
     BuildSubproblemConstraintsManagerMap(subproblem_map, options, solver_log_manager);
     build_variables_to_follow_indices_vector();
 
-    onBendersStartPlugin_(sub_pb_ids_,
+    onBendersStartPlugin_(sub_names_,
                           _world->rank(),
                           options.INPUTROOT,
                           options.OUTPUTROOT,
@@ -247,8 +249,12 @@ void Benders_MICRO_ITERS::OnBendersIterationEnd()
     {
         for (auto& [sub_name, constraint_reader_name]: subproblem_constraint_map_)
         {
-            auto constraint_reader = constraints_map_[constraint_reader_name];
-            constraint_reader->delete_added_rows();
+            auto it = constraints_map_.find(constraint_reader_name);
+            if (it == constraints_map_.end())
+            {
+                continue;
+            }
+            it->second->delete_added_rows();
         }
     }
 }
@@ -267,8 +273,14 @@ void Benders_MICRO_ITERS::build_variables_to_follow_indices_vector()
 {
     for (auto& [sub_name, constraint_reader_name]: subproblem_constraint_map_)
     {
+        auto it = constraints_map_.find(constraint_reader_name);
+        if (it == constraints_map_.end())
+        {
+            // This subproblem is handled by another MPI rank
+            continue;
+        }
         variables_to_follow_indices_per_sub_[sub_name] = std::vector<int>();
-        auto constraint_reader = constraints_map_[constraint_reader_name];
+        auto constraint_reader = it->second;
         for (auto& variable: variables_to_follow_)
         {
             int variable_index = constraint_reader->get_variable_index_in_solution(variable);
@@ -339,22 +351,9 @@ void Benders_MICRO_ITERS::OnBendersSubResolutionEnd(std::string sub_name, int nu
     }
 }
 
-void Benders_MICRO_ITERS::SetSubProblemIDs(const char** subs_ids, int n_subs)
+void Benders_MICRO_ITERS::SetSubProblemIDs(std::vector<std::string>& sub_names)
 {
-    sub_ids_storage_.clear();
-    sub_ids_storage_.reserve(n_subs);
-    for (int i = 0; i < n_subs; i++)
-    {
-        sub_ids_storage_.push_back(subs_ids[i]);
-    }
-
-    sub_ids_ptrs_.resize(n_subs);
-    for (int i = 0; i < n_subs; i++)
-    {
-        sub_ids_ptrs_[i] = sub_ids_storage_[i].c_str();
-    }
-
-    sub_pb_ids_ = SubProblemIds{sub_ids_ptrs_.data(), n_subs};
+    sub_names_ = sub_names;
 }
 
 void Benders_MICRO_ITERS::BuildSubproblemConstraintsManagerMap(
