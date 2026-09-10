3f07716e4caf5602aeac8652986f577db7c49c01
Hedi Bouchehda <h.bouchehdaa@gmail.com>
2026-09-10 09:28:33 +0200

    as we integrated the plugin on the benders sequential, we fix its setup in the unit test file

--- tests/cpp/benders/benders_sequential_test.cpp
+++ tests/cpp/benders/benders_sequential_test.cpp
@@ -5,6 +5,7 @@
 #include "RandomDirGenerator.h"
 #include "antares-xpansion/benders/benders_core/CouplingMapGenerator.h"
 #include "antares-xpansion/benders/benders_sequential/BendersSequential.h"
+#include "antares-xpansion/benders/plugins/NoOperationPlugin.h"
 #include "antares-xpansion/benders/output/JsonWriter.h"
 #include "antares-xpansion/helpers/ArchiveWriter.h"
 #include "antares-xpansion/multisolver_interface/environment.h"
@@ -31,6 +32,7 @@ public:
                                      std::shared_ptr<MathLoggerDriver> mathLoggerDriver):
         BendersSequential(options, logger, writer, mathLoggerDriver)
     {
+        SetPlugin(std::make_shared<NoOperationPlugin>());
     }
 
     void init_data() override
